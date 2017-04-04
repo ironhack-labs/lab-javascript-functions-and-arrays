46a192d2facf8760ed1a08a9b7348ff09a1c8501
Hery Martin <scorpionxii@gmail.com>
2017-04-04 16:58:59 +0200

    Update

--- starter-code/functions-and-arrays.js
+++ starter-code/functions-and-arrays.js
@@ -1,31 +1,34 @@
 // Find the maximum
-function maxOfTwoNumbers (first, second) {
-
+function maxOfTwoNumbers(first, second) {
+    if (first <= second)
+        return second;
+    else
+        return first;
 }
 
 var largest = maxOfTwoNumbers(2, 6);
 console.log(largest);
 
 // Finding Longest Word
-function findLongestWord (words) {
-
+function findLongestWord(words) {
+    var longitude = 0;
+    for (var i = 0; i < words.length; i++)
+        if (longitude < words[i].length)
+            longitude = words[i].length;
+    return longitude;
 }
 
-var words = [
-  "mystery",
-  "brother",
-  "aviator",
-  "crocodile",
-  "pearl",
-  "orchard",
-  "crackpot"
-];
+
+var words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 var longest = findLongestWord(words);
 console.log(longest);
 
 // Calculating a Sum
 function sumArray (array) {
-
+    var suma = 0;
+    for(var i=0; i<array.length; i++)
+        suma += array[i];
+    return suma;
 }
 
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
@@ -34,7 +37,7 @@ console.log(total);
 
 // Calculate the Average
 function averageNumbers (array) {
-
+    return sumArray(array)/array.length;
 }
 
 var numbers = [2, 6, 9, 10, 7, 4, 1, 9];
@@ -43,42 +46,27 @@ console.log(average);
 
 // Array of Strings
 function averageWordLength (array) {
-
+    var result = 0;
+    for (var i=0; i<array.length; i++)
+        result += array[i].length;
+    return result/array.length;
 }
 
-var words = [
-  "seat",
-  "correspond",
-  "linen",
-  "motif",
-  "hole",
-  "smell",
-  "smart",
-  "chaos",
-  "fuel",
-  "palace"
-];
+
+var words = ["seat", "correspond", "linen", "motif", "hole", "smell", "smart", "chaos", "fuel", "palace"];
 var averageLength = averageWordLength(words);
 console.log(averageLength);
 
 // Unique Arrays
-function uniquifyArray (array) {
-
+function uniquifyArray(array) {
+  for (var i=0; i<array.length; i++)
+    for(var j=i+1; j<array.length; j++)
+      if (array[i]==array[j])
+        array.splice(j,1);
+  return array;
 }
 
-var words = [
-  "crab",
-  "poison",
-  "contagious",
-  "simple",
-  "bring",
-  "sharp",
-  "playground",
-  "poison",
-  "communion",
-  "simple",
-  "bring"
-];
+var words = ["crab", "poison", "contagious", "simple", "bring", "sharp", "playground", "poison", "communion", "simple", "bring"];
 var uniqued = uniquifyArray(words);
 console.log(uniqued);
 
@@ -87,16 +75,7 @@ function doesWordExist (wordsArray, word) {
 
 }
 
-var words = [
-  "machine",
-  "subset",
-  "trouble",
-  "starting",
-  "matter",
-  "eating",
-  "truth",
-  "disobedience"
-];
+var words = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
 var hasMatter = doesWordExist(words, "matter");
 console.log(hasMatter);
@@ -109,19 +88,7 @@ function howManyTimes (words, word) {
 
 }
 
-var words = [
-  "machine",
-  "matter",
-  "subset",
-  "trouble",
-  "starting",
-  "matter",
-  "eating",
-  "matter",
-  "truth",
-  "disobedience",
-  "matter"
-];
+var words = ["machine", "matter", "subset", "trouble", "starting", "matter", "eating", "matter", "truth", "disobedience", "matter"];
 
 var howManyMatter = howManyTimes(words, "matter");
 console.log(howManyMatter);
@@ -135,8 +102,8 @@ function greatestProduct (matrix) {
 }
 
 var matrix = [
-  [08,02,22,97,38,15,0,40,0,75,04,05,07,78,52,12,50,77,91,08],
-  [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,0],
+  [08,02,22,97,38,15, 0,40, 0,75,04,05,07,78,52,12,50,77,91,08],
+  [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62, 0],
   [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65],
   [52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91],
   [22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
@@ -144,10 +111,10 @@ var matrix = [
   [32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70],
   [67,26,20,68,02,62,12,20,95,63,94,39,63,08,40,91,66,49,94,21],
   [24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72],
-  [21,36,23,09,75,0,76,44,20,45,35,14,0,61,33,97,34,31,33,95],
+  [21,36,23,09,75, 0,76,44,20,45,35,14, 0,61,33,97,34,31,33,95],
   [78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14,09,53,56,92],
-  [16,39,05,42,96,35,31,47,55,58,88,24,0,17,54,24,36,29,85,57],
-  [86,56,0,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58],
+  [16,39,05,42,96,35,31,47,55,58,88,24, 0,17,54,24,36,29,85,57],
+  [86,56, 0,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58],
   [19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40],
   [04,52,08,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66],
   [88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69],
