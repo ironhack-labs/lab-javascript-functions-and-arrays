79b4d3cada824cd67e38b34ed4115411d4e548c6
Lars Hansen <lars1702@gmail.com>
2018-05-22 16:04:40 +0200

    done?

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -11,8 +11,6 @@ var words = [
   'crackpot'
 ];
 
-// Calculating a Sum
-
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // Calculate the Average
@@ -98,3 +96,29 @@ var matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+function maxOfTwoNumbers (a, b) {
+  if (a>b) {
+    return a;
+  }
+  else {
+    return b;
+  }
+};
+
+
+
+function findLongestWord (longWord) {
+  for (var i=1; i<words.length; i++) {
+    if(longWord.length < words[i].length){
+      var longWord = words[i];
+    }
+  }
+  return longWord;
+}
+
+/*
+if  continue
+
+else add word
+*/
