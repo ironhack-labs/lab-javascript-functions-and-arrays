9e0eb460ec4a7aeabe8a804279185301bc4fb6b2
Hannes Reinsch <hannes.reinsch99@gmail.com>
2018-05-28 09:31:19 +0200

    BER-Hannes_Reinsch

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,15 +1,16 @@
 // Find the maximum
-function maxOfTwoNumbers(num1, num2){
-  if(num1 > num2){
+function maxOfTwoNumbers(num1,num2){
+  if(num1>num2){
     return num1;
-  }else if(num1 < num2){
+  }
+  else if(num1<num2){
     return num2;
-  }else{
-   return num1
+  }
+  else{
+    console.log("Both numbers are equal");
+    return num1;
   }
 }
-
-
 // Finding Longest Word
 var words = [
   'mystery',
@@ -21,26 +22,56 @@ var words = [
   'crackpot'
 ];
 
-var lgth = 0;
-var longest;
-
 function findLongestWord(words){
-  for(var i=0; i < words.length; i++){
-    if(words[i].length > lgth){
-        var lgth = words[i].length;
-        longest = words[i];
-    }      
-  } 
+  var longest=[];
+  if(words.length<=0){
+    return undefined;
+  }
+  if(words.length===1){
+    return words[0];
+  }
+  
+  for(var i=0; i<words.length; i++){
+    if(words[i].length>longest.length){
+      longest[0]=words[i];
+      longest.length=words[i].length;
+    }
+    else{
+      continue;
+    }
+  }
+    return longest;
+}
+// Calculating a Sum
+var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumArray(numbers){
+  if (numbers.length===0){
+    return 0;
+  }
+  var sum=0;
+  for(var i=0; i<numbers.length; i++){
+    sum+=numbers[i];
+  }
+  return sum;
 }
 
 
-// Calculating a Sum
-
-var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // Calculate the Average
-
 var numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(numbersAvg){
+  var average=0;
+  if (numbersAvg.length===0){
+    return undefined;
+  }
+  for (var i=0; i<numbersAvg.length; i++){
+    average=average+numbersAvg[i];
+  }
+  average=(average/numbersAvg.length);
+  return average;
+}
+
+
 
 // Array of Strings
 var wordsArr = [
@@ -55,6 +86,17 @@ var wordsArr = [
   'fuel',
   'palace'
 ];
+function averageWordLength(wordsArr){
+  var sum=0;
+
+  if(wordsArr.length===0){
+    return undefined;
+  }
+  for(var i=0; i<wordsArr.length; i++){
+    sum+=wordsArr[i].length;
+  }
+  return (sum/wordsArr.length);
+}
 
 // Unique Arrays
 var wordsUnique = [
@@ -70,6 +112,23 @@ var wordsUnique = [
   'simple',
   'bring'
 ];
+function uniquifyArray (wordsUnique){
+  var uniqueA=[];
+  if(wordsUnique.length===0){
+    return undefined;
+  }
+  for(var j=0; j<wordsUnique.length; j++){
+    var newArr=[];
+    for(var i=j; i<wordsUnique.length-1; i++){
+      newArr.push(wordsUnique[i+1]);
+    }
+    if(newArr.indexOf(wordsUnique[j])===-1){
+      uniqueA.push(wordsUnique[j]);
+    }
+  }
+  return uniqueA;
+}
+
 
 // Finding Elements
 var wordsFind = [
@@ -82,7 +141,18 @@ var wordsFind = [
   'truth',
   'disobedience'
 ];
-
+function doesWordExist(wordsFind, key){
+  var count=0;
+  for(var i=0; i<wordsFind.length; i++){
+    if(key-wordsFind[i]===0){
+      count++;
+    }
+  }
+  if((count>1) &&(count===1)){
+    return true;
+  }
+    return false;
+}
 // Counting Repetion
 var wordsCount = [
   'machine',
@@ -97,27 +167,319 @@ var wordsCount = [
   'disobedience',
   'matter'
 ];
+
+function howManyTimes(wordsCount, key){
+  if(wordsCount.length===0){
+    return false;
+  }
+  var count=0;
+  for(var i=0; i<wordsCount.length; i++){
+    if(wordsCount.indexOf(key)>-1){
+      count=count+1;
+    }
+  }
+  if(count<1){
+    return 0;
+  }
+  else{
+    return count;
+  }
+}
+
 // Bonus Quest
 
 var matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+  [
+    49,
+    49,
+    99,
+    40,
+    17,
+    81,
+    18,
+    57,
+    60,
+    87,
+    17,
+    40,
+    98,
+    43,
+    69,
+    48,
+    4,
+    56,
+    62,
+    0
+  ],
+  [
+    81,
+    49,
+    31,
+    73,
+    55,
+    79,
+    14,
+    29,
+    93,
+    71,
+    40,
+    67,
+    53,
+    88,
+    30,
+    3,
+    49,
+    13,
+    36,
+    65
+  ],
   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+  [
+    22,
+    31,
+    16,
+    71,
+    51,
+    67,
+    63,
+    89,
+    41,
+    92,
+    36,
+    54,
+    22,
+    40,
+    40,
+    28,
+    66,
+    33,
+    13,
+    80
+  ],
+  [
+    24,
+    47,
+    32,
+    60,
+    99,
+    3,
+    45,
+    2,
+    44,
+    75,
+    33,
+    53,
+    78,
+    36,
+    84,
+    20,
+    35,
+    17,
+    12,
+    50
+  ],
+  [
+    32,
+    98,
+    81,
+    28,
+    64,
+    23,
+    67,
+    10,
+    26,
+    38,
+    40,
+    67,
+    59,
+    54,
+    70,
+    66,
+    18,
+    38,
+    64,
+    70
+  ],
+  [
+    67,
+    26,
+    20,
+    68,
+    2,
+    62,
+    12,
+    20,
+    95,
+    63,
+    94,
+    39,
+    63,
+    8,
+    40,
+    91,
+    66,
+    49,
+    94,
+    21
+  ],
+  [
+    24,
+    55,
+    58,
+    5,
+    66,
+    73,
+    99,
+    26,
+    97,
+    17,
+    78,
+    78,
+    96,
+    83,
+    14,
+    88,
+    34,
+    89,
+    63,
+    72
+  ],
   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+  [
+    16,
+    39,
+    5,
+    42,
+    96,
+    35,
+    31,
+    47,
+    55,
+    58,
+    88,
+    24,
+    0,
+    17,
+    54,
+    24,
+    36,
+    29,
+    85,
+    57
+  ],
   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+  [
+    19,
+    80,
+    81,
+    68,
+    5,
+    94,
+    47,
+    69,
+    28,
+    73,
+    92,
+    13,
+    86,
+    52,
+    17,
+    77,
+    4,
+    89,
+    55,
+    40
+  ],
   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+  [
+    88,
+    36,
+    68,
+    87,
+    57,
+    62,
+    20,
+    72,
+    3,
+    46,
+    33,
+    67,
+    46,
+    55,
+    12,
+    32,
+    63,
+    93,
+    53,
+    69
+  ],
+  [
+    4,
+    42,
+    16,
+    73,
+    38,
+    25,
+    39,
+    11,
+    24,
+    94,
+    72,
+    18,
+    8,
+    46,
+    29,
+    32,
+    40,
+    62,
+    76,
+    36
+  ],
+  [
+    20,
+    69,
+    36,
+    41,
+    72,
+    30,
+    23,
+    88,
+    34,
+    62,
+    99,
+    69,
+    82,
+    67,
+    59,
+    85,
+    74,
+    4,
+    36,
+    16
+  ],
+  [
+    20,
+    73,
+    35,
+    29,
+    78,
+    31,
+    90,
+    1,
+    74,
+    31,
+    49,
+    71,
+    48,
+    86,
+    81,
+    16,
+    23,
+    57,
+    5,
+    54
+  ],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
