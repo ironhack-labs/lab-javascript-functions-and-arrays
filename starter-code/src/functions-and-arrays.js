106700bbe46c755cc9992c11f184682a69621728
CPBCPB <christinebeck2010@gmail.com>
2018-06-12 15:33:06 -0400

    will finish later

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,4 +1,124 @@
+// notes from review var words = [
+//   'mystery',
+//   'brother',
+//   'aviator',
+//   'crocodile',
+//   'pearl',
+//   'orchard',
+//   'mysteriously',
+//   'crackpot'
+// ];
+
+
+// function findLongestWord(wordsArray){
+//     var longestWord = "";
+//     wordsArray.forEach(function(eachWord){
+//       if(eachWord.length > longestWord.length){
+//           longestWord = eachWord;
+//       }
+//   });
+//   return(longestWord);
+// }
+
+// //   console.log(   findLongestWord(words)   );
+
+
+
+// var numbers = [5,5,5,5,5,7,7,7,7,7];
+
+
+// function calculateSum(arrayOfNumbers){
+//     var sum = 0;
+//     arrayOfNumbers.forEach(function(oneParticularNumber){
+//       sum += oneParticularNumber
+//     });
+//       return sum;
+// }
+
+// //   console.log(   calculateSum(numbers)    );
+
+// function calculateAverage(numberArray){
+//   return   (calculateSum(numberArray) / numberArray.length)
+// }
+
+// // console.log(  calculateAverage(numbers)          )
+
+
+
+// var wordsWithDuplicates = [
+//   'crab',
+//   'crab', 
+//   'crab',
+//   'poison',
+//   'contagious',
+//   'simple',
+//   'bring',
+//   'sharp',
+//   'simple',
+//   'simple',
+//   'playground',
+//   'poison',
+//   'communion',
+//   'simple',
+//   'simple',
+//   'simple',
+//   'bring'
+// ];
+
+// function uniquifyArray(theArray){
+
+//   var newArray = [];
+
+//   theArray.forEach(function(eachWord){
+
+//       if(newArray.indexOf(eachWord) === -1){
+//           newArray.push(eachWord)
+//       }
+// // .includes is newer method that accomplishes the same thing with cleaner syntax
+
+//       // if(!newArray.includes(eachWord)){
+//       //     newArray.push(eachWord)
+//       // }
+//   });
+//   return newArray
+// }
+
+// // console.log(uniquifyArray(wordsWithDuplicates));
+
+
+// function doesItExist(arrayOfWords, wordToSearchFor){
+//    var doesIt = false;
+//    arrayOfWords.forEach(function(eachWord){
+//       if(eachWord === wordToSearchFor){doesIt = true}
+//    });
+//   return doesIt
+// }
+// //  console.log(  doesItExist(wordsWithDuplicates, 'kentucky')   )
+
+
+// function numberOfTimes(theArray, theWord){
+//   var count = 0;
+//  theArray.forEach(function(eachWord){
+//       if(eachWord === theWord){
+//           count++;
+//       }
+//    });
+//   return count;
+// }
+
+// console.log( numberOfTimes(wordsWithDuplicates, 'rain')  );
+
+
 // Find the maximum
+
+function maxOfTwoNumbers(number1, number2){
+  if (number1 > number2){ return(number1)}
+  else if (number2 > number1){
+    return(number2)
+  }
+}
+
+console.log(maxOfTwoNumbers(15,20))
 
 // Finding Longest Word
 var words = [
@@ -10,10 +130,29 @@ var words = [
   'orchard',
   'crackpot'
 ];
+function findingLongestWord(array){
+  var longestWord =""
+  array.forEach(function(aWord){
+    if (aWord.length>longestWord.length){longestWord=aWord};
+  })
+  console.log(longestWord)
+}
+findingLongestWord(words)
 
 // Calculating a Sum
 
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumArray(array){
+var runningSum=0;
+array.forEach(function(numb){
+  runningSum = runningSum + numb
+  });
+  console.log(runningSum)
+}
+sumArray(numbers)
 
 // Calculate the Average
 
