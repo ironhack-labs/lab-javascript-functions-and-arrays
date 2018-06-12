28741e97716d0d5e7b7019a0662bde692d180f8e
Sandra Najera <snajera@ucm.es>
2018-06-12 16:51:42 +0200

    Bonus

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -188,3 +188,28 @@ var matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+function greatestProduct(matrix){
+  var greatestProduct = 0;
+  for (var i = 0; i < matrix.length - 4; i++) {
+    for (var j = 0; j < matrix[i].length - 4; j++) {
+      var horizontalProduct = 1;
+      for (var k = j; k < (j + 4); k++) {
+        horizontalProduct *= matrix[i][k];
+      }
+
+      if (horizontalProduct > greatestProduct) {
+        greatestProduct = horizontalProduct;
+      }
+
+      var verticalProduct = 1;
+      for (var k = i; k < (i + 4); k++) {
+        verticalProduct *= matrix[k][j];
+      }
+
+      if (verticalProduct > greatestProduct) {
+        greatestProduct = verticalProduct;
+      }
+    }
+  }
+  return greatestProduct
+}
