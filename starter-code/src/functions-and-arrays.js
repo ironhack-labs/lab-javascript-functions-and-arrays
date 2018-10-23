d1e87478a9845f2073c345fc8cd76481908950ab
Javier <jarrindaulivi4@gmail.com>
2018-10-23 15:29:24 -0400

    done

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,8 +1,40 @@
+
+
 // Find the maximum
 
+
+
+
+
+function maxOfTwoNumbers(numOne, numTwo){
+  if(numOne > numTwo ){
+    return numOne;
+  }
+  else if (numOne < numTwo){
+    return numTwo;
+  }
+  else {
+    return numOne;
+  }
+}
 // Finding Longest Word
-var words = [
-  'mystery',
+function findLongestWord(words){
+  let bigger = "";
+    if (words.length < 1){
+      return undefined;
+    }
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > bigger.length) {
+        bigger = words[i];
+      }
+  };
+    return bigger;
+}
+
+
+findLongestWord(words);
+ var words = [
+ 'mystery',
   'brother',
   'aviator',
   'crocodile',
@@ -12,12 +44,38 @@ var words = [
 ];
 
 // Calculating a Sum
+let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumArray(numbers){
+  let addIt = 0;
+  if (numbers.length < 1){
+    return 0;
+  }
+  for(var i=0; i< numbers.length; i++) { 
+    addIt += numbers[i];
+}
+  return addIt;
+}
 
+sumArray();
+
+console.log(sumArray());
 // Calculate the Average
-
 var numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(numbersAvg){
+  let addIt = 0;
+  if (numbersAvg.length < 1){
+    return undefined;
+  }
+  for(var i=0; i< numbersAvg.length; i++) {
+    addIt += numbersAvg[i];
+  }
+  return addIt/numbersAvg.length;
+}
+// avgArray();
+// console.log(avgArray());
+
+
 
 // Array of Strings
 var wordsArr = [
