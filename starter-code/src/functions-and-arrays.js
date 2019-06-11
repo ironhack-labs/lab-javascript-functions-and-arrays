b5eeecca4fcd88a2793abbe0bb3a17f2c89fb57d
Michael Roth <michael.franklin.roth@gmail.com>
2019-06-11 14:08:04 -0400

    javascript lab with Sams Diana and Michael Roth

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,4 +1,8 @@
 // Find the maximum
+function maxOfTwoNumbers(first, second){
+  if(first > second){return first}
+  else {return second}
+}
 
 // Finding Longest Word
 var words = [
@@ -11,11 +15,40 @@ var words = [
   'crackpot'
 ];
 
+function findLongestWord(wordArray){
+  let longestWord;
+  if(wordArray.length !== 0){
+    longestWord="";
+    wordArray.forEach((word)=>{
+    if(word.length>longestWord.length){
+      longestWord = word;
+    }
+  });
+  }
+  return longestWord;
+}
+
 // Calculating a Sum
+function sumArray(array){
+  let sum = 0;
+  array.forEach((num)=>{
+    sum += num;
+  });
+  return sum;
+}
 
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // Calculate the Average
+function averageNumbers(numArray){
+  let average;
+  if(numArray.length !== 0) {
+    let sum = sumArray(numArray);
+    let amountOfNumbers = numArray.length;
+    average = sum / amountOfNumbers;
+  }
+  return average;
+}
 
 var numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
@@ -33,6 +66,19 @@ var wordsArr = [
   'palace'
 ];
 
+function averageWordLength(wordArray){
+  let averageWordLength;
+  if(wordArray.length !== 0) {
+    averageWordLength = 0;
+    wordLengthArray=[];
+    wordArray.forEach((words)=>{
+      wordLengthArray.push(words.length);
+    });
+    averageWordLength = averageNumbers(wordLengthArray);
+  }
+  return averageWordLength;
+}
+
 // Unique Arrays
 var wordsUnique = [
   'crab',
@@ -47,6 +93,18 @@ var wordsUnique = [
   'simple',
   'bring'
 ];
+function uniquifyArray(wordArray){
+  let uniqueArray;
+  if(wordArray.length !== 0) {
+    uniqueArray=[];
+    wordArray.forEach((words)=>{
+      if(uniqueArray.indexOf(words) === -1) {
+        uniqueArray.push(words);
+      }
+    });
+  }
+  return uniqueArray;
+}
 
 // Finding Elements
 var wordsFind = [
@@ -60,7 +118,30 @@ var wordsFind = [
   'disobedience'
 ];
 
+function doesWordExist(wordArray, wordSearch){
+  let wordExist = false;
+  wordArray.forEach((words)=>{
+    if(wordSearch == words) {
+      wordExist = true;
+    }
+  });
+  return wordExist;
+}
+
+
+// `IN` STATEMENT DOESN'T WORK FOR US
+// function doesWordExist(wordArray, wordSearch){
+//   let wordExist = false;
+//   if(wordSearch in wordArray === true){
+//     wordExist = true;
+//   }
+//   return wordExist;
+// }
+
+// doesWordExist(wordsFind, 'machine');
+
 // Counting Repetion
+
 var wordsCount = [
   'machine',
   'matter',
@@ -74,27 +155,19 @@ var wordsCount = [
   'disobedience',
   'matter'
 ];
-// Bonus Quest
 
-var matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+function howManyTimes(wordArray, selectedWord) {
+  let wordCount;
+  if(wordArray.length === 0) {
+    wordCount=false;
+  } else {
+    wordCount = 0;
+    wordArray.forEach((words)=>{
+      if(words === selectedWord) {
+        wordCount++;
+      }
+    });
+  }
+  return wordCount;
+}
+// Bonus Quest
