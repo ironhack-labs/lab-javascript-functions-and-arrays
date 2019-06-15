bff08c01171c7e39914f5c666a0150a1ab2993f4
confidenti-al <steve@confidenti.al>
2019-06-15 15:03:38 -0400

    Reviewed in detail on Saturday, rewrote all functions and code, commented every step as I went through it

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,12 +1,20 @@
 // Find the maximum
+// function maxOfTwoNumbers(first, second) {
+//   if (first > second) {
+//     return first;
+//   }
+//   else {
+//     return second;
+//   };
+// };
+
 function maxOfTwoNumbers(first, second) {
-  if (first > second) {
-    return first;
+  if (first > second) {                     // see if the first num is bigger than the num
+    return first;                           // it's being compared to, and return the first
+  } else {                                  // num if bigger, otherwise
+    return second;                          // return the second num
   }
-  else {
-    return second;
-  };
-};
+}
 
 // Finding Longest Word
 var words = [
@@ -19,47 +27,65 @@ var words = [
   'crackpot'
 ];
 
+// function findLongestWord(arrayOfWords) {
+//   let longestWord = arrayOfWords[0];
+//   arrayOfWords.forEach((eachWord) => {
+//     if(eachWord.length > longestWord.length){
+//       longestWord = eachWord;
+//     };
+//   })
+//   return longestWord;
+// };
+
 function findLongestWord(arrayOfWords) {
-  let longestWord = arrayOfWords[0];
-  arrayOfWords.forEach((eachWord) => {
-    if(eachWord.length > longestWord.length){
-      longestWord = eachWord;
-    };
+  let longestWord = arrayOfWords[0]; // sets the initial value to the first word in the array
+                                     // so when we start the loop, the first word to check is
+                                     // at position [0]. Will be updated when we reset longestWord
+                                     // using the loop - could use for with [i] or foreach.
+  arrayOfWords.forEach((eachWord)=>{
+    if(eachWord.length > longestWord.length){ // uses > bc it will return the first if 2+ are
+      longestWord = eachWord;                 // same length automatically.
+    }
   })
   return longestWord;
-};
-
+}
 
 // Calculating a Sum
-
-function sumArray(arrayOfNumbers){
-  let total = 0;
-  arrayOfNumbers.forEach((eachNumber) => {
-    total += eachNumber;
-  })
-  return total;
-};
-
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-// Calculate the Average
-function averageNumbers(arrayOfNums) {
-  if(arrayOfNums.length === 0){
-    return undefined;
-  };
-  return sumArray(arrayOfNums)/arrayOfNums.length;
-};
+// function sumArray(arrayOfNumbers){
+//   let total = 0;
+//   arrayOfNumbers.forEach((eachNumber) => {
+//     total += eachNumber;
+//   })
+//   return total;
+// };
 
+function sumArray(arrayOfNumbers){
+  let total = 0;                          // initialize total as 0, then use forEach to loop
+  arrayOfNumbers.forEach((eachNumber)=>{  // through all the elements in the array, storing 
+    total += eachNumber;                  // the previous total in the variable, and adding
+  })                                      // the next element's value to the previously stored
+  return total;                           // total to get the sum.
+}
+
+// Calculate the Average
 var numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageWordLength(theArray) {
-  let newArray = [];
-  theArray.forEach((eachWord) => {
-    newArray.push(eachWord.length)
-  })
-  return averageNumbers(newArray);
-};
+// function averageNumbers(arrayOfNums) {
+//   if(arrayOfNums.length === 0){
+//     return undefined;
+//   };
+//   return sumArray(arrayOfNums)/arrayOfNums.length;
+// };
 
+function averageNumbers(arrayOfNums){
+  if (arrayOfNums.length === 0){                      // check to make sure the array is not
+    return undefined;                                 // empty; if empty return undefined.
+  };                                                  // use previous sumArray function to
+    return sumArray(arrayOfNums)/arrayOfNums.length;  // take total and divide my length of
+}                                                     // array of Nums to get average.    
+                                                      
 // Array of Strings
 var wordsArr = [
   'seat',
@@ -74,18 +100,21 @@ var wordsArr = [
   'palace'
 ];
 
-function uniquifyArray(arrayOfWords) {
-  if(arrayOfWords.length === 0) {
-    return undefined;
-  };
-  let newArray = [];
-  arrayOfWords.forEach((eachWord) => {
-    if(newArray.indexOf(eachWord) === -1) {
-      newArray.push(eachWord);
-    };
-  })
-  return newArray;
-};
+// function averageWordLength(theArray) {
+//   let newArray = [];
+//   theArray.forEach((eachWord) => {
+//     newArray.push(eachWord.length)
+//   })
+//   return averageNumbers(newArray);
+// };
+
+function averageWordLength(arrayOfWords) {
+  let wordsArray = [];                      // initialize the temp word length array to null;
+  arrayOfWords.forEach((eachWord)=>{        // use forEach to loop through all words in array,
+    wordsArray.push(eachWord.length)        // get the length of each word and store in the
+  })                                        // temp word length array, then return the previous
+  return averageNumbers(wordsArray);        // averageNumbers function on that array
+}
 
 // Unique Arrays
 var wordsUnique = [
@@ -102,6 +131,32 @@ var wordsUnique = [
   'bring'
 ];
 
+// function uniquifyArray(arrayOfWords) {
+//   if(arrayOfWords.length === 0) {
+//     return undefined;
+//   };
+//   let newArray = [];
+//   arrayOfWords.forEach((eachWord) => {
+//     if(newArray.indexOf(eachWord) === -1) {
+//       newArray.push(eachWord);
+//     };
+//   })
+//   return newArray;
+// };
+
+function uniquifyArray(arrayOfWords){
+  if(arrayOfWords.length === 0){            // check to make sure word array isn't empty;
+    return undefined;                       // if empty, return undefined;
+  };
+  let newArray = [];                        // initialize temp array holder to empty;
+  arrayOfWords.forEach((eachWord)=>{        // loop through each word in the array
+    if(newArray.indexOf(eachWord) === -1){  // if the indexOf method returns -1, means word
+      newArray.push(eachWord);              // does not exist in the temp array holder, so
+    }                                       // then put it into the temp array holder.
+  })                                        // >> indexOf :: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+  return newArray;                          // return the unique words as the temp array holder
+}
+
 // Finding Elements
 var wordsFind = [
   'machine',
@@ -114,17 +169,28 @@ var wordsFind = [
   'disobedience'
 ];
 
-function doesWordExist(theWords, searchTerm) {
-  let result = false;
-  theWords.forEach((singleWord) => {
-    if(singleWord === searchTerm) {
-     result = true;
-    };
-  })
-  return result;
-};
+// function doesWordExist(theWords, searchTerm) {
+//   let result = false;
+//   theWords.forEach((singleWord) => {
+//     if(singleWord === searchTerm) {
+//      result = true;
+//     };
+//   })
+//   return result;
+// };
 
-// Counting Repetion
+function doesWordExist(wordsArrayToSeach, eachWordToSeach){ // pass in two args (array, search term)
+  let result = false;                                       // initialize result state to false;
+  wordsArrayToSeach.forEach((eachWord)=>{                   // loop through array of words to search,
+                                                            // pass in each word from the words array
+    if(eachWord === eachWordToSeach) {                      // if word equals word we're looking for,
+      result = true;                                        // set result to true;
+    }
+  })
+  return result;                                            // return result state;
+}
+
+// Counting Repetition
 var wordsCount = [
   'machine',
   'matter',
@@ -139,18 +205,31 @@ var wordsCount = [
   'matter'
 ];
 
-function howManyTimes(theWords, searchTerm) {
-  if(theWords.length === 0) {
-    return false;
+// function howManyTimes(theWords, searchTerm) {
+//   if(theWords.length === 0) {
+//     return false;
+//   };
+//   let total = 0;
+//   theWords.forEach((singleWord) => {
+//     if(singleWord === searchTerm) {
+//      total++;
+//     };
+//   })
+//   return total;
+// };
+
+function howManyTimes(arrayOfWords, searchTerm){  // pass in two args, array of words, search term
+  if(arrayOfWords.length === 0){                  // check to make sure array of words isn't empty;
+    return false;                                 // if empty; return state false -> can't search array anymore
   };
-  let total = 0;
-  theWords.forEach((singleWord) => {
-    if(singleWord === searchTerm) {
-     total++;
+  let count = 0;                                  // initialize count to 0, will add to count as we find occurrences
+  arrayOfWords.forEach((eachWord)=>{              // forEach loop through the array of words, passing in each word
+    if(eachWord === searchTerm){                  // if each word is found each to the search term,
+      count++;                                    // increase the count for each word by one;
     };
   })
-  return total;
-};
+  return count;                                   // return count value to the array of words counter
+}
 
 // Bonus Quest
 
@@ -177,23 +256,47 @@ var matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct(theMatrix) {
-  let greatest = 0;
-  theMatrix.forEach((eachRow) => {
-    for(let i=0; i < eachRow.length - 4; i++) {
-      let product = eachRow[i]*eachRow[i+1]*eachRow[i+2]*eachRow[i+3];
-      if(product > greatest) {
-        greatest = product;
-      };
-    };
+// function greatestProduct(theMatrix) {
+//   let greatest = 0;
+//   theMatrix.forEach((eachRow) => {
+//     for(let i=0; i < eachRow.length - 4; i++) {
+//       let product = eachRow[i]*eachRow[i+1]*eachRow[i+2]*eachRow[i+3];
+//       if(product > greatest) {
+//         greatest = product;
+//       };
+//     };
+//   })
+//   for(let i=0; i< theMatrix.length - 4; i++) {
+//     theMatrix[i].forEach((eachNumber, index) => {
+//       let prod = theMatrix[i][index] *  theMatrix[i+1][index] * theMatrix[i+2][index] * theMatrix[i+3][index];
+//          if(prod > greatest) {
+//         greatest = prod;
+//       };
+//     })
+//   };
+//   return greatest;
+// }
+
+function greatestProduct(theMatrix) {           // declare the function, pass in the matrix array of row arrays
+  let greatest = 0;                             // set the returned variable greatest to zero to start
+                                                // first do rows, comparing each number to the next 3 in row unless
+                                                // you're at the end of the row array where there are less than 3
+                                                // to add to the previous
+  theMatrix.forEach((eachRow)=>{
+    for(let i = 0; i < eachRow.length - 4; i++){  // bc of length-4, will stop when then are less than 4 to multiply
+      let product = eachRow[i]*eachRow[i+1]*eachRow[i+2]*eachRow[i+3]; // multiply each successive set of 4 values
+      if (product > greatest){                  // if the product is bigger than the existing greatest value,
+        greatest = product;                     // update greatest to product value
+      }
+    }
   })
-  for(let i=0; i< theMatrix.length - 4; i++) {
-    theMatrix[i].forEach((eachNumber, index) => {
-      let prod = theMatrix[i][index] *  theMatrix[i+1][index] * theMatrix[i+2][index] * theMatrix[i+3][index];
-         if(prod > greatest) {
-        greatest = prod;
-      };
-    })
-  };
-  return greatest;
+    for(let i = 0; i < theMatrix.length - 4; i++){ // loop through the index values of the columns -4
+      theMatrix[i].forEach((index)=>{  // pass in the index value for each column's position
+        let prod = theMatrix[i][index]*theMatrix[i+1][index]*theMatrix[i+2][index]*theMatrix[i+3][index]; // multiply 4 numbers
+        if(prod > greatest){           // if the product is bigger than the previously stored greatest value (including rows),
+          greatest = prod;             // update greatest value
+        }
+      })
+      return greatest;                 // return the greatest value whether rows or columns
+    }
 }
