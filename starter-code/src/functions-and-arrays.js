ca2e446f5fc95725eddd17d8eb0c6168f29b2276
Jennifer V Phan <jennyphan181295@gmail.com>
2019-06-25 16:22:38 +0200

    done

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,100 +1,229 @@
 // Find the maximum
+function largerNum(num1, num2) {
+    if (num1 > num2) {
+        console.log(`${num1} is larger than ${num2}`);
+    } else if (num1 < num2) {
+        console.log(`${num2} is larger than ${num1}`);
+    } else {
+        console.log("They are equal!");
+    }
+}
+
 
 // Finding Longest Word
 var words = [
-  'mystery',
-  'brother',
-  'aviator',
-  'crocodile',
-  'pearl',
-  'orchard',
-  'crackpot'
+    'mystery',
+    'brother',
+    'aviator',
+    'crocodile',
+    'pearl',
+    'orchard',
+    'crackpot'
 ];
+var longestLength = 0;
+var longestWord = "";
+
+function longest(anyArray) {
+    for (var i = 0; i < anyArray.length; i++) {
+        if (anyArray[i].length > longestLength) {
+            longestLength = anyArray[i].length;
+            longestWord = anyArray[i];
+        }
+    }
+    console.log(longestWord);
+}
 
 // Calculating a Sum
 
 var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+var sum = numbers.reduce((total, amount) => total + amount);
+console.log(sum);
+
+// var sum = 0;
+// function sumArray(oneArray) {
+//     for (var i = 0; i < oneArray.length; i++) {
+//         sum += oneArray[i];
+//     }
+//     console.log(sum);
+// }
 
 // Calculate the Average
 
 var numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+var sum = numbersAvg.reduce((total, amount) => total + amount);
+console.log(sum / (numbersAvg.length));
 
 // Array of Strings
 var wordsArr = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace'
+    'seat',
+    'correspond',
+    'linen',
+    'motif',
+    'hole',
+    'smell',
+    'smart',
+    'chaos',
+    'fuel',
+    'palace'
 ];
+var lengths = [];
+
+function averageWordLength(anotherArray) {
+    for (var i = 0; i < anotherArray.length; i++) {
+        lengths.push((anotherArray[i].length));
+    }
+    var sumLengths = lengths.reduce((total, amount) => total + amount);
+    console.log(sumLengths / (wordsArr.length));
+}
+
+averageWordLength(wordsArr);
+
 
 // Unique Arrays
 var wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
+    'crab',
+    'poison',
+    'contagious',
+    'simple',
+    'bring',
+    'sharp',
+    'playground',
+    'poison',
+    'communion',
+    'simple',
+    'bring'
 ];
+
+function uniquifyArray(anotherArray) {
+    var uniqueArray = [];
+    for (var i = 0; i < anotherArray.length; i++) {
+        if (uniqueArray.indexOf(anotherArray[i]) === -1) {
+            uniqueArray.push(anotherArray[i]);
+        }
+    }
+    console.log(uniqueArray)
+}
+uniquifyArray(wordsUnique);
+
 
 // Finding Elements
 var wordsFind = [
-  'machine',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'truth',
-  'disobedience'
+    'machine',
+    'subset',
+    'trouble',
+    'starting',
+    'matter',
+    'eating',
+    'truth',
+    'disobedience'
 ];
+
+function doesWordExist(anArray, aWord) {
+    for (var i = 0; i < anArray.length; i++) {
+        if (anArray[i] === aWord) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 // Counting Repetion
 var wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+    'machine',
+    'matter',
+    'subset',
+    'trouble',
+    'starting',
+    'matter',
+    'eating',
+    'matter',
+    'truth',
+    'disobedience',
+    'matter'
 ];
+
+function howManyTimes(anArr, anyWord) {
+    var counter = 0;
+    for (var i = 0; i < anArr.length; i++) {
+        if (anArr[i] === anyWord) {
+            counter += 1;
+        }
+    }
+    console.log(`The word ${anyWord} appears ${counter} time(s).`);
+}
+
+// function timesAppear(oneArr) {
+//     oneArr.forEach(
+//         function(word) {
+//             var counter = 0;
+//             for (var i = 0; i < oneArr.length; i++) {
+//                 if (oneArr[i] === word) {
+//                     counter += 1;
+//                 }
+//             }
+//             console.log(`The ${word} appears ${counter} time(s).`);
+//         }
+//     )
+// }
+// timesAppear(wordsCount);
+
+
 // Bonus Quest
 
 var matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+    [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+    [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+    [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+    [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+    [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+    [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+    [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+    [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+    [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+    [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+    [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+    [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+    [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+    [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+    [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+    [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+    [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+    [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+    [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+    [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+function greatestProduct(anyObj) {
+    var horizontalMult = [];
+    for (var i = 0; i < anyObj.length; i++) {
+        for (var j = 0; j < anyObj[i].length - 3; j++) {
+            var x = anyObj[i][j];
+            var y = anyObj[i][j + 1];
+            var z = anyObj[i][j + 2];
+            var t = anyObj[i][j + 3];
+            horizontalMult.push(x * y * z * t);
+        }
+    }
+    var horValue = Math.max.apply(null, horizontalMult);
+
+    var verticalMult = [];
+    for (var n = 0; n < anyObj.length - 3; n++) {
+        for (var m = 0; m < anyObj[n].length; m++) {
+            var a = anyObj[n][m];
+            var b = anyObj[n + 1][m];
+            var c = anyObj[n + 2][m];
+            var d = anyObj[n + 3][m];
+            verticalMult.push(a * b * c * d);
+        }
+    }
+    var verValue = Math.max.apply(null, verticalMult);
+
+    if (horValue > verValue) {
+        console.log(horValue);
+    } else {
+        console.log(verValue);
+
+    }
+}
+greatestProduct(matrix);
