9cc1ca8b9ddcb30806ab432908b9f2aa9d8a2ecd
William Brown <hce@WmHB.local>
2019-10-15 18:18:49 +0200

    Finished all challenges + bonus

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,4 +1,17 @@
+/* eslint-disable prettier/prettier */
 // Find the maximum
+
+function maxOfTwoNumbers(num1, num2) {
+  let maxNum;
+  [num1, num2].forEach(function(num) {
+    if (!maxNum || num > maxNum) {
+      maxNum = num;
+    }
+    return maxNum;
+  });
+  return maxNum;
+}
+console.log(maxOfTwoNumbers(-51, 10));
 
 // Finding Longest Word
 const words = [
@@ -8,16 +21,51 @@ const words = [
   'crocodile',
   'pearl',
   'orchard',
-  'crackpot'
+  'crackpot',
 ];
 
+function findLongestWord(words) {
+  if (!words.length) {
+    return null;
+  }
+  let longestWord = '';
+  words.forEach(function(word) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+  return longestWord;
+}
+
+console.log(findLongestWord(words));
 // Calculating a Sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumArray(numbers) {
+  let sum = 0;
+  numbers.forEach(function(number) {
+    sum += number;
+  });
+  return sum;
+}
+
+console.log(sumArray(numbers));
+
 // Calculate the Average
 
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers(numbers) {
+  if (!numbers.length) {
+    return null;
+  }
+  let sum = 0;
+  numbers.forEach(function(number) {
+    sum += number;
+  });
+  return sum / numbers.length;
+}
 
 // Array of Strings
 const wordsArr = [
@@ -30,8 +78,19 @@ const wordsArr = [
   'smart',
   'chaos',
   'fuel',
-  'palace'
+  'palace',
 ];
+
+function averageWordLength(words) {
+  if (!words.length) {
+    return null;
+  }
+  let characters = 0;
+  words.forEach(function(word) {
+    characters += word.length;
+  });
+  return characters / words.length;
+}
 
 // Unique Arrays
 const wordsUnique = [
@@ -45,8 +104,21 @@ const wordsUnique = [
   'poison',
   'communion',
   'simple',
-  'bring'
+  'bring',
 ];
+
+function uniquifyArray(words) {
+  if (!words.length) {
+    return [];
+  }
+  let uniqueArray = [];
+  words.forEach(function(word) {
+    if (!uniqueArray.includes(word)) {
+      uniqueArray.push(word);
+    }
+  });
+  return uniqueArray;
+}
 
 // Finding Elements
 const wordsFind = [
@@ -57,8 +129,17 @@ const wordsFind = [
   'matter',
   'eating',
   'truth',
-  'disobedience'
+  'disobedience',
 ];
+
+function doesWordExist(words, keyword) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === keyword) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // Counting Repetition
 const wordsCount = [
@@ -72,30 +153,70 @@ const wordsCount = [
   'matter',
   'truth',
   'disobedience',
-  'matter'
+  'matter',
 ];
+
+function howManyTimes(words, keyword) {
+  let occurences = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === keyword) {
+      occurences += 1;
+    }
+  }
+  return occurences;
+}
 
 // Bonus
 
 const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+  [8,2,22,97,38,15,0,40,0,75,4,5,7,78,52,12,50,77,91,8],
+[49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,4,56,62,0],
+[81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,3,49,13,36,65],
+[52,70,95,23,4,60,11,42,69,24,68,56,1,32,56,71,37,2,36,91],
+[22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
+[24,47,32,60,99,3,45,2,44,75,33,53,78,36,84,20,35,17,12,50],
+[32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70],
+[67,26,20,68,2,62,12,20,95,63,94,39,63,8,40,91,66,49,94,21],
+[24,55,58,5,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72],
+[21,36,23,9,75,0,76,44,20,45,35,14,0,61,33,97,34,31,33,95],
+[78,17,53,28,22,75,31,67,15,94,3,80,4,62,16,14,9,53,56,92],
+[16,39,5,42,96,35,31,47,55,58,88,24,0,17,54,24,36,29,85,57],
+[86,56,0,48,35,71,89,7,5,44,44,37,44,60,21,58,51,54,17,58],
+[19,80,81,68,5,94,47,69,28,73,92,13,86,52,17,77,4,89,55,40],
+[4,52,8,83,97,35,99,16,7,97,57,32,16,26,26,79,33,27,98,66],
+[88,36,68,87,57,62,20,72,3,46,33,67,46,55,12,32,63,93,53,69],
+[4,42,16,73,38,25,39,11,24,94,72,18,8,46,29,32,40,62,76,36],
+[20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,4,36,16],
+[20,73,35,29,78,31,90,1,74,31,49,71,48,86,81,16,23,57,5,54],
+[1,70,54,71,83,51,54,69,16,92,33,48,61,43,52,1,89,19,67,48]];
+
+function greatestProduct(matrix) {
+  let largestProduct = 0;
+  let product = 0;
+  // find largest horizontal product
+  matrix.forEach(function(array) {
+    array.forEach(function(num, index) {
+      if (index < array.length - 3) {
+        product =
+          num * array[index + 1] * array[index + 2] * array[index + 3];
+        if (product > largestProduct) {
+          largestProduct = product;
+        }
+      }
+    });
+  });
+  // find largest vertical product
+  matrix.forEach(function(array, arrayIndex) {
+    array.forEach(function (num, numIndex) {
+      if (arrayIndex < matrix.length - 3) {
+        product = num * matrix[arrayIndex + 1][numIndex] * matrix[arrayIndex + 2][numIndex] * matrix[arrayIndex + 3][numIndex];
+        if (product > largestProduct) {
+          largestProduct = product;
+        }
+      }
+    })
+  });
+  return largestProduct;
+}
+
+console.log(greatestProduct(matrix));
