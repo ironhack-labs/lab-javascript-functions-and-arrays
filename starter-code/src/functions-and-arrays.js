3c53769311d47c663156bc206ab0d8c3b8af9aad
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-28 15:25:32 -0400

    done-but still tester showing 2 more failures which needs to be done

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -222,3 +222,46 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+// //if we turn all array elements into 2, it will give 16.
+// // Uncomment for-loop to turn all array elements with number 2(or any other number).
+function switchArr(arr, toNumber){
+for(let i=0;i<arr.length;i++){
+  for(let j=0;j<arr[i].length;j++){
+    arr[i][j]=toNumber;
+  }
+}
+return arr;
+}
+// switchArr(matrix, 2); //switches whole arr el. with 2.
+
+
+function get(arr, y, x) {
+
+  if (0 <= y && y < arr.length && 0 <= x && x < arr[y].length) {
+    return arr[y][x];
+  }
+  return 0;
+}
+
+function greatestProduct(arr, k) {
+
+  let max = 0;
+  let len = arr.length;
+  for (let y = 0; y < len; y++) {
+    for (let x = 0; x < len; x++) {
+
+      let p1 = 1, p2 = 1, p3 = 1, p4 = 1;
+
+      for (let i = 0; i < k; i++) {
+        p1*= get(arr, y, x + i);
+        p2*= get(arr, y + i, x);
+        p3*= get(arr, y + i, x + i);
+        p4*= get(arr, y + i, x - i);
+      }
+      max = Math.max(p1, p2, p3, p4, max);
+    }
+  }
+  return max;
+}
+greatestProduct(matrix, 4);
