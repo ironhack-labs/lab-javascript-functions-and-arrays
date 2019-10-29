69e8e16e22cf370dae29ba5c401687cb87a699b8
esrfree <esrfree@LAPTOP-6IF23GBG.localdomain>
2019-10-29 16:27:40 -0400

    Iteration #8: Bonus - Horizontal, Vertical and Diagonal adjacents

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -137,3 +137,40 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+function greatestProduct( matrix, n ) {
+  let greatest = [];
+
+  //Horizontal adjacents
+  for ( let a = 0; a < matrix.length; a++ ) {
+    for ( let i = 0; i <= matrix[a].length - n; i++) {
+      let prod = 1;
+      for (let j = i; j < i + n; j++) {
+        prod *= matrix[a][j];
+      }
+      greatest.push(prod);
+    }
+  }
+
+  // Vertical adjacents
+  for ( let a = 0; a < matrix.length; a++ ) {
+    for ( let i = 0; i <= matrix.length - n; i++ ) {
+      let prod = 1;
+      for ( let j = i; j < i + n; j++ ) {
+        prod *= matrix[j][a];
+      }
+      greatest.push(prod);
+    }
+  }
+
+  // Diagonal adjacents
+  for ( let a = 0; a <= matrix.length - n; a++ ) {
+    for ( let i = 0; i <= matrix[a].length - n; i++ ) {
+      let prod = 1;
+      for ( let j = 0; j < n; j++ ) {
+        prod *= matrix[a + j][i + j];
+      }
+      greatest.push(prod);
+    }
+  }
+  return Math.max(...greatest);
+}
