0b97ba139ada311bb0da8e0a9aa5e0e37f04d121
Veronica <veronica1506@gmail.com>
2019-10-28 21:54:23 -0400

    Function and Arrays Iteration 8-Bonus

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -36,7 +36,6 @@ function sumArray(arr) {
     numbers.forEach(num => sum += num)
   }
   return sum
-
 }
 
 // Iteration #4: Calculate the average
@@ -177,3 +176,20 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+function greatestProduct(matrix) {
+  let arrMultiplication = [];
+  let largestNum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      arrMultiplication.push(matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3])
+    if (arrMultiplication[i] > largestNum) {
+      largestNum = arrMultiplication[i]
+    }
+
+    }
+    
+  }
+  console.log(largestNum)
+
+}
