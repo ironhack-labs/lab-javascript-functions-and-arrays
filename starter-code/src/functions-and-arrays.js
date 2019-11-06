00960b51fd4012a24a413e23345ce449a4a1e2ab
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-06 23:58:43 +0100

    sum array

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -40,6 +40,21 @@ function findLongestWord(words){
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+
+function sumArray(num){
+  
+  sumTotal = 0;
+
+  num.forEach(function(e){
+  
+    sumTotal = e + sumTotal;
+  
+  });
+  
+  return sumTotal;
+
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
