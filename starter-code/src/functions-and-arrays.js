d995bd5a4c8870501ade2cca8b46de2ad1f9b114
pelinski <teresapelinski@gmail.com>
2019-11-07 12:06:37 +0100

    finished iteration 4

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -50,6 +50,13 @@ function sumArray(numbers) {
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(numbers) {
+  if (numbers.length = 0) { 
+    return null;
+  }
+  return sumArray(numbers)/numbers.length; 
+}
+
 // Level 2: Array of strings
 const wordsArr = [
   'seat',
