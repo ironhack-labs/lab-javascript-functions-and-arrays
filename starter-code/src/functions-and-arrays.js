d0a221ab03f6c5a2ebb1f3940c4c888dad597fce
Miguel <alonsogalalrdomiguel@gmail.com>
2019-11-07 22:58:35 +0100

    modificado hasta itinerario 4

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,15 +1,54 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(a,b){
+  if(a > b){
+    return a 
+  }else{
+    return b
+  }
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(w){
+  let longestword = new String();
+  for(let i = 0; i < w.length; i++){
+    if (w[i].length > longestword.length){
+      
+      longestword = w[i];
+    }
+  }  
+  if(longestword == ""){
+    longestword = null
+  }
+  return longestword;
+}
+
+  findLongestWord(words)
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumArray(n){
+  let sum = 0
+  for (let i = 0; i < n.length; i++){
+    sum = sum + n[i]
+  }
+  return sum;
+}
+sumArray(numbers)
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(s){
+  let sum = 0;
+  for (let i = 0; i < s.length; i++){
+    sum = sum + s[i]
+  }
+  sum = sum / s.length;
+  return sum;
+ }
+ averageNumbers(numbersAvg)
 
 // Level 2: Array of strings
 const wordsArr = [
