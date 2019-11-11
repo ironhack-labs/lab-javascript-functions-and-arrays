36e62620f1d2fc75f94c2665dc662dd83f52495b
Javi L <jlopezro@gmail.com>
2019-11-11 16:32:47 +0100

    Identation

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -2,11 +2,11 @@
 
 function maxOfTwoNumbers(num1, num2) {
 
-if (num1 > num2) {
-  return num1;
-} else {
-  return num2;
-};
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  };
 
 };
 
