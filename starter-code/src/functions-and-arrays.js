fb2a042bd3d45bc5b2cc67b87d1bbb35685df5e5
RFeldman84 <rachel.feldman84@gmail.com>
2019-11-12 01:39:33 -0500

    redo of initial commit for re-learning

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,13 +1,21 @@
-//Find the maximum
+// +++++++++++++++++Find the maximum+++++++++++++++++++++++++++
+ function maxOfTwoNumbers(num1, num2){
+    return (num1<num2) ? num2: num1;
+ }
 
-function maxOfTwoNumbers (num1, num2){
-  if (num1 > num2) {
-    return num1;
-  }else return num2;
-}
-//console.log (maxOfTwoNumbers(5,20)) => 20
+ 
+// if (num1 < num2){
+//     return num2;
+//   } else {
+//     return num1;
+//   }
+//  }
 
-// Finding Longest Word
+maxOfTwoNumbers(5,20) //=> 20
+
+
+// +++++++++++++++ Finding Longest Word++++++++++++++++
+
 var words = [
   'mystery',
   'brother',
@@ -18,56 +26,103 @@ var words = [
   'crackpot'
 ];
 
-function findLongestWord(Lwords){
-  if(Lwords.length ===0){
+// answer 2.0 
+
+function findLongestWord (array){
+  if (array.length === 0) {
+    return undefined;
+  }
+  let longest_word = " ";
+  array.forEach(element => { 
+   if(element.length > longest_word.length) {
+       longest_word = element;
+   } 
+  });
+console.log(longest_word) 
+// //console.log(longest_word.length)    
+return longest_word;
+}
+findLongestWord (words);
+
+/*
+//old answer 1.0 --- didnt peek!
+function findLongestWord(Lword) {
+  if (Lword.length === 0) {
     return undefined;
   }
   let longest = "";
-  Lwords.forEach((eachSingleWord)=>{
-    if(eachSingleWord.length > longest.length){
+  Lword.forEach(eachSingleWord => {
+    if (eachSingleWord.length > longest.length) {
       longest = eachSingleWord;
     }
   })
   return longest;
 }
-//console.log(findLongestWord()) => crocodile
+console.log(findLongestWord(words)) //=> crocodile
+*/
+
+// use >= get next longest same length
+ // const findLongestWord1 = (array) => {
+// }
+ //no function words.foreach targets, inside function placeholder targets the array 
 
 
-// Calculating a Sum 
 
-var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
-function sumArray (sumNums){
+ // ++++++++Calculating a Sum ++++++
+ 
+ var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+ 
+ function sumArray(sumNums) {
   let sum = 0;
-  sumNums.forEach((addNum)=>{
+  sumNums.forEach(addNum => {
     sum += addNum;
   })
+  console.log(sum)
   return sum;
 }
-//console.log(sumArray()) => 87
+sumArray(numbers) //=> 87
 
-// Calculate the Average
 
-var numbers = [2, 6, 9, 10, 7, 4, 1, 9];
+// //****go back to*** Redo  with ++RUDUCE++  reDOce hehehe*******
+// Calculating a sum is as simple as iterating over an array and adding each of the elements together.
 
-function averageNumbers(avgArray){
+// Semantically[reduce](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce) is the best method to use for this, but you can use any loop we've discussed so far.
+
+// Create a `sumArray` function that takes an array of numbers as a parameter, and calculate the sum of all its numbers:
+
+
+
+
+// ++++++++++Calculate the Average++++++++++++++++++++++
+
+
+ numbers = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(avgArray) {
   let sumNum = 0;
-  avgArray.forEach((addNum)=>{
+  avgArray.forEach(addNum => {
     sumNum += addNum;
-    return sumNum ;  //sum 48
-  })
-  let avgNum = sumNum / numbers.length;
+    return sumNum; //sum 48
+  });
+   avgNum = sumNum / numbers.length;
   return avgNum;
 }
-//console.log (averageNumbers())  //avg 6
-//console.log (numbers.length) // 8
+console.log (averageNumbers(numbers))  //avg 6
+console.log (numbers.length) // 8
 
+
+// ++++?? go back to** try using callback with sumArray funtion???++++
+// **Algorithm**
+// 1. Find the sum as we did in the first exercise
+// 2. Take the sum from step 1, and divide it by the number of elements in the list.
+
+
+
+// ++++++++++++++++++ Array of Strings++++++++++++++++++++
 
 /*
-// Array of Strings
 var wordsArr = [
   'seat',
-  'correspond'
+  'correspond',
   'linen',
   'motif',
   'hole',
@@ -78,7 +133,17 @@ var wordsArr = [
   'palace'
 ];
 
-// Unique Arrays
+*/
+//findLongestWord(wordsArr)
+
+
+
+
+
+
+// +++++++++++++++++++++++++++Unique Arrays++++++++++++++
+
+/*
 var wordsUnique = [
   'crab',
   'poison',
@@ -92,8 +157,10 @@ var wordsUnique = [
   'simple',
   'bring'
 ];
+*/
+//+++++++++++++++++Finding Elements++++++++++++++++++++++
 
-// Finding Elements
+/*
 var wordsFind = [
   'machine',
   'subset',
@@ -105,7 +172,11 @@ var wordsFind = [
   'disobedience'
 ];
 
-// Counting Repetion
+*/
+
+//+++++++++++++++++++++Counting Repetion++++++++++++++++++
+
+/*
 var wordsCount = [
   'machine',
   'matter',
@@ -119,52 +190,45 @@ var wordsCount = [
   'disobedience',
   'matter'
 ];
-// Bonus Quest
-
-var matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
-
-
-
-
-
-
-
-
-// IN CLASS OVERVEIW
-//Submission Upon completion, run the following commands
-//$ git add .    $ git commit -m 'done'   $ git push origin master
-//Navigate to your repo and create a Pull Request -from your master branch to the original repository master branch.  In the Pull request name, add your name and last names separated by a dash '-'
-
-//Pass the tests
-
-//You have to write your code on the src/functions-and-arrays.js file. Following the instructions, you should go step by step passing all the tests.
-
-//Do not rush to go through all of them at once, take your time to read carefully about what the iteration is asking you, and solve the errors one by one.
-
-//When coding with tests, is super important to read and understand the errors we are having for each test, this way we will know what it expect from your code.
-
-//DeliverablesAll our work will be located in the functions-and-arrays.js file, so that will be enough. Anyway, you have to push the whole repo to Github.
-
 
 */
+
+
+// ## Bonus Quest
+
+// In the 20×20 grid below; What is the greatest product of four adjacent numbers in the same direction (up, down, left, right)?
+
+// Write a function `greatestProduct` to find the answer!
+
+/*
+var matrix = [
+  [08,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,08],
+  [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00],
+  [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65],
+  [52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91],
+  [22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
+  [24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50],
+  [32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70],
+  [67,26,20,68,02,62,12,20,95,63,94,39,63,08,40,91,66,49,94,21],
+  [24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72],
+  [21,36,23,09,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95],
+  [78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14,09,53,56,92],
+  [16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57],
+  [86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58],
+  [19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40],
+  [04,52,08,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66],
+  [88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69],
+  [04,42,16,73,38,25,39,11,24,94,72,18,08,46,29,32,40,62,76,36],
+  [20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16],
+  [20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
+  [01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48],
+];
+```
+*/
+
+
+
+
+
+
+
