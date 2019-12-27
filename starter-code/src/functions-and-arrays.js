f1a508e5364e9bb97e7e2c450d6dfd6a2689e3b1
Javier Varela <jvarela@strands.com>
2019-12-27 16:08:58 +0100

    iteration 1 find the maximum

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -91,3 +91,14 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+
+//Iteration #1: Find the maximum
+
+function maxOfTwoNumbers(a, b){
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
