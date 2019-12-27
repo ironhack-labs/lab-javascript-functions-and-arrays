ff8ffe8ff5342455ee26ae6c6a60826499c89669
Gisela García José <gisgarjos@hotmail.com>
2019-12-27 23:39:26 +0100

    done

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -110,6 +110,17 @@ const wordsFind = [
   'disobedience'
 ];
 
+function doesWordExist(array, search) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == search) {
+      return true;
+    }
+  }
+    return false;
+  }
+
+doesWordExist(wordsFind, "Arrays");
+
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -125,6 +136,16 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+function howManyTimes(array, search) {
+  let result = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == search) {
+      result++;
+    }
+  }
+  return result;
+}
+howManyTimes(countRepetition, "matter");
 
 // Iteration #8: Bonus
 
@@ -150,3 +171,5 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+function greatestProduct ()
