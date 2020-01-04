cb09ce991708cc7a981a2977ac32df5276741a37
Marc Serra <mserrahidalgo@gmail.com>
2020-01-04 15:42:40 +0100

    finish with bonus

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,93 +1,226 @@
 // Iteration #1: Find the maximum
+// Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest.
+
+function maxOfTwoNumbers(n1, n2){
+  max = n1
+  if (n2 > n1){
+    max = n2
+  }
+  return max
+}
 
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+// Declare a function named findLongestWord that takes as an argument an array of words and
+// returns the longest one. If there are 2 with the same length, it should return the first
+// occurrence.
+
+function findLongestWord(arrWor){
+  if (arrWor.length > 0){
+    let biggest = "";
+    arrWor.forEach(element => {
+      if (element.length > biggest.length){
+        biggest = element;
+      }
+    });    
+    return biggest
+  } else{
+    return null
+  }
+}
 
 // Iteration #3: Calculate the sum
+// Calculating a sum is as simple as iterating over an array and adding each of the elements together.
+// Declare a function named sumArray that takes as an argument an array of numbers, and returns the
+// sum of all of the numbers in the array.
 
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumArray(arrNum){
+  let sum = 0;
+  arrNum.forEach(element => {
+    sum += element
+  });
+  return sum
+}
 
 // Iteration #4: Calculate the average
+// Calculating an average is an extremely common task. Let's practice it a bit.
+// Find the sum as we did in the first exercise
+// Take the sum from step 1, and divide it by the number of elements in the list.
+
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+// Declare a function named averageNumbers that expects an array of numbers and returns the average of the numbers:
+function averageNumbers(arrNum){
+  if (arrNum.length > 0){
+    return sumArray(arrNum)/arrNum.length
+  } else{
+    return null
+  }
+}
 
 // Level 2: Array of strings
-const wordsArr = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace'
-];
+// Declare a function named averageWordLength that receives as a single argument an array of words
+// and returns the average length of the words:
+
+function averageWordLength(arrWor){
+  if(arrWor.length > 0){
+    let sum = 0;
+    arrWor.forEach(element => {
+      sum += element.length
+    });
+    return sum/arrWor.length
+  } else{
+    return null
+  }
+}
+
 
 // Iteration #5: Unique arrays
-const wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
-];
+// Take the following array, remove the duplicates, and return a new array. You're more
+// than likely going to want to check out the indexOf Array method.
+// Do this in the form of a function uniquifyArray that receives an array of words as a argument.
+
+function uniquifyArray(arrWor){
+  let newArr = [];
+  arrWor.forEach(element => {
+    if (newArr.indexOf(element) == -1){
+      newArr.push(element);
+    };
+  });
+  return newArr
+}
 
 // Iteration #6: Find elements
-const wordsFind = [
-  'machine',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'truth',
-  'disobedience'
-];
+// Let's create a simple array search.
+// Declare a function named doesWordExist that will take in an array of words as one argument,
+// and a word to search for as the other. Return true if it exists, otherwise, return false.
+// Don't use indexOf for this one.
+
+function doesWordExist(arrWor, word){
+  for (let i = 0; i < arrWor.length; i++) {
+    if(arrWor[i] == word.toString()){
+      return true
+    };    
+  }
+  return false
+}
 
 // Iteration #7: Count repetition
-const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
-];
+// Declare a function named howManyTimes that will take in an array of words as the first argument,
+// and a word to search for as the second argument. The function will return the number of times
+// that word appears in the array.
+function howManyTimes(arrWor, word){
+  let count = 0;
+  arrWor.forEach(element => {
+    if(element == word){
+      count += 1
+    }
+  });
+  return count
+}
 
 // Iteration #8: Bonus
+// What is the greatest product of four adjacent numbers? We consider adjacent any four numbers
+// that are next to each other in horizontal, vertical or diagonal.
+// For example, if we have a 5x5 Matrix like:
+/*
+[ 1,  2, 3, 4, 5]
+[ 1, 20, 3, 4, 5]
+[ 1, 20, 3, 4, 5]
+[ 1, 20, 3, 4, 5]
+[ 1,  4, 3, 4, 5]
+*/
+// The greatest product will be the 20x20x20x4 = 32000;
+//Declare a function named greatestProduct to find it in the 20×20 grid below!
 
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+function greatestProduct(grid){
+  allProducts = [];
+  objectMatrix = {}
+  // Make a object with coordinates:
+  /*
+  - 0  1  2
+  0 00 01 02
+  1 10 11 10
+  2 20 21 22
+  */
+  for (let i = 0; i < grid.length; i++) {
+    line = grid[i]
+    for (let x = 0; x < line.length; x++) {
+      number = grid[i][x]
+      var key = `${i+1}${x+1}`
+      objectMatrix[key] = number
+    }
+  }
+  // For every number, do the calcs to all directions. If is outbound of the matrix, multiply 1.
+  const keys = Object.keys(objectMatrix)
+  keys.forEach(k => {
+    k = parseFloat(k)
+    let hR = 1;
+    let hL = 1;
+    let vU = 1;
+    let vD = 1;
+    let dUR = 1;
+    let dUL = 1;
+    let dDR = 1;
+    let dDL = 1;
+    for (let y = 0; y < 4; y++) {
+      // Horizontal Right
+      let valHR = objectMatrix[(k+y).toString()]
+      if ( valHR == undefined){
+        valHR = 1;
+      };
+      hR *= valHR
+      // Horizontal Left
+      let valHL = objectMatrix[(k-y).toString()]
+      if ( valHL == undefined){
+        valHL = 1;
+      };
+      hL *= valHL
+      // Vertical Up
+      let valvU = objectMatrix[(k-(y*10)).toString()]
+      if ( valvU == undefined){
+        valvU = 1;
+      };
+      vU *= valvU
+      // Vertical Down
+      let valvD = objectMatrix[(k+(y*10)).toString()]
+      if ( valvD == undefined){
+        valvD = 1;
+      };
+      vD *= valvD
+      // Diagonal Up Right
+      let valdUR = objectMatrix[(k-(y*10)+y).toString()]
+      if ( valdUR == undefined){
+        valdUR = 1;
+      };
+      dUR *= valdUR
+      // Diagonal Up Left
+      let valdUL = objectMatrix[(k-(y*10)-y).toString()]
+      if ( valdUL == undefined){
+        valdUL = 1;
+      };
+      dUL *= valdUL
+      // Diagonal Down Right
+      let valdDR = objectMatrix[(k+(y*10)+y).toString()]
+      if ( valdDR == undefined){
+        valdDR = 1;
+      };
+      dDR *= valdDR
+      // Diagonal Down Left
+      let valDDL = objectMatrix[(k+(y*10)-y).toString()]
+      if ( valDDL == undefined){
+        valDDL = 1;
+      };
+      dDL *= valDDL
+    };
+    // Finally push all results to final array and get the maximum number
+    allProducts.push(hR);
+    allProducts.push(hL);
+    allProducts.push(vU);
+    allProducts.push(vD);
+    allProducts.push(dUR);
+    allProducts.push(dUL);
+    allProducts.push(dDR);
+    allProducts.push(dDL);
+
+  });
+  return Math.max(...allProducts);
+}
