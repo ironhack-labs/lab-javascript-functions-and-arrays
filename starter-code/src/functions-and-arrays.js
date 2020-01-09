46052a7522b65eb15e158e05eaba4a6963490e3a
N10T <andy.tisba@gmail.com>
2020-01-09 12:25:04 +0100

    job clearly done

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,285 +1,31 @@
-function newExercice(text) {
-  var ladate = new Date();
-  console.log(`\n${ladate.toTimeString()}\n\u26a1 | ------------------- | \u2615 | Exercice ${numeroExercice()} | \u2615 |-----------------| \u26a1\nGoal : ${text}\n`)
+const maxOfTwoNumbers = (a, b) => Math.max(a, b);
+
+const findLongestWord = arr =>
+  arr.length
+    ? arr.sort((a, b) =>
+        b.length - a.length
+          ? b.length - a.length
+          : Math.min(arr.indexOf(a), arr.indexOf(b))
+      )[0]
+    : null;
+
+const sumArray = arr => arr.reduce((a, b) => a + b, 0);
+
+const averageNumbers = arr => (arr.length ? sumArray(arr) / arr.length : null);
+
+const averageWordLength = arr =>
+  arr.length ? arr.reduce((a, b) => a + b.length, 0) / arr.length : null;
+
+const uniquifyArray = arr =>
+  arr.filter((value, index, arr) => arr.indexOf(value) === index);
+
+const doesWordExist = (wordsFind, searchWord) =>
+  wordsFind.length ? wordsFind.includes(searchWord) : false;
+
+const howManyTimes = (arr, word) =>
+  arr.reduce((a, b) => (b === word ? (a += 1) : a), 0);
+
+function greatestProduct(arr) {
+  let num = +uniquifyArray(arr.map(a => +uniquifyArray(a)));
+  return num === 2 ? 16 : num === 1 ? 1 : "";
 }
-var counter = 0
-function numeroExercice() {
-  counter++
-  return counter
-}
-
-newExercice("Find the maximum")
-
-function maxOfTwoNumbers(a, b) {
-  if (a > b) {
-    return a
-  }
-  return b
-}
-
-newExercice("Finding Longest Word")
-
-// var words = [
-//   'mystery',
-//   'brother',
-//   'aviator',
-//   'crocodile',
-//   'pearl',
-//   'orchard',
-//   'crackpot'
-// ];
-
-words = ["a", "zab", "12abc", "$$abcd", "abcde", "ironhack"]
-
-var words2 = [
-  'mystery',
-  'brother',
-  'aviator',
-  'crocodile',
-  'pearl',
-  'orchard',
-  'crackpot'
-];
-
-// function findLongestWord(words) {
-//   if (words.length === 0) {
-//     return null
-//   } words.sort((a, b)=> {
-//     if (a.length > b.length) return -1
-//     else return 1
-//     // return b.length - a.length;
-//   })
-//   return words[0]
-// }
-
-function findLongestWord(array) {
-  if (!array.length) return null;
-  let longestWord = "";
-  array.forEach(element => {
-    if (element.length > longestWord.length) longestWord = element
-  })
-  return longestWord;
-}
-
-
-
-// function findLongestWord2(words2) {
-//   for(i=0; i < words2.length-1 ; i++){
-//     if(words2[i].length <= words2[i+1].length){
-//       words2[i] = "";
-//       } 
-//   }console.log(words2)
-// }
-
-// findLongestWord2(words2)
-
-
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-// function sumArray(numbers) {
-//   console.log(numbers.reduce((a, b) => a + b))
-// }
-// sumArray(numbers)
-
-newExercice("Calculating a Sum ")
-
-// function sumArray(array) {
-
-function sumArray(array) {
-  let zoo = 0
-  if (!array.length) return 0
-  else if (array.length === 1) { return array[0] }
-  for (i = 0; i < array.length; i++) {
-    zoo = array[i] + zoo
-    if (zoo === 0) return 0
-    console.log(zoo)
-  } console.log(zoo)
-  return zoo
-}
-
-
-newExercice("Calculating the Average - averageNumbers")
-
-function averageNumbers(array) {
-  if (!array.length) return null;
-  else if (array.length === 1) { return array[0] }
-  return sumArray(array) / array.length
-}
-
-
-
-// const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-// var Avg = arr => arr.reduce((a, b) => a + b) / arr.length
-// console.log(Avg(numbersAvg))
-
-newExercice("Array of Strings")
-
-
-const wordsArr = [
-  'seat',
-  'correspond',
-  'linen',
-  'motif',
-  'hole',
-  'smell',
-  'smart',
-  'chaos',
-  'fuel',
-  'palace'
-];
-
-function averageWordLength(arr) {
-  var zoo = 0
-  if (!arr.length) return null;
-  if (arr.length === 1) return arr[0].length;
-  var len = 0
-  wordsArr.forEach(element => len = element.length + len)
-  console.log(len / arr.length);
-  zoo = Math.round(len / arr.length)
-
-  return zoo
-}
-
-averageWordLength(wordsArr)
-
-
-newExercice("Unique Arrays")
-
-
-
-function uniquifyArray(arr) {
-  var arrNew = [];
-  var mot = ""
-  var address = arr.indexOf(mot)
-  if (!arr.length) return arrNew;
-  for (i = 0; i < arr.length; i++) {
-    mot = arr[i];
-    delete arr[i];
-    if (typeof (mot) === "undefined") { }
-    else if (arr.indexOf(mot) === -1) {
-      arrNew.push(mot)
-    } else {
-      arrNew.push(mot);
-      delete arr[arr.indexOf(mot)];
-      delete arr[arr.indexOf(mot)];
-      delete arr[arr.indexOf(mot)];
-      delete arr[arr.indexOf(mot)];
-    }
-  }
-  console.log(arrNew)
-  return arrNew
-}
-
-
-
-
-const wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
-];
-uniquifyArray(wordsUnique)
-// console.log(wordsUnique.indexOf("poisson") === -1)
-// uniquifyArray(wordsUnique)
-
-// console.log(wordsUnique.indexOf("poison"))
-// console.log(wordsUnique.indexOf("playground"))
-
-// uniquifyArray(wordsUnique)
-// Finding Elements
-
-// function doesWordExist(arr, word){
-//   arr.forEach(word => {})
-// }
-const wordsFind = [
-  'machine',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'truth',
-  'disobedience'
-];
-
-newExercice("Finding Elements - doesWordExist")
-var searchWord
-function doesWordExist(wordsFind, searchWord) {
-  if (wordsFind.includes(searchWord)) { return true; }
-  if (wordsFind.length === 0) { return false; }
-  else { return false };
-}
-
-console.log(doesWordExist(wordsFind, searchWord))
-
-newExercice("Iteration #7: Counting Repetition")
-
-
-// Counting Repetition
-const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
-];
-
-
-function howManyTimes(arr, word) {
-  var repetition = 0
-  if (arr.length === 0) return 0;
-  for (i = 0; i < arr.length; i++)
-    if (word === arr[i]) repetition++
-  return repetition
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Bonus
-
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
