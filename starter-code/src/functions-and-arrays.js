3c9dbf16e7a83e69687c8e223f0d64a26a543c64
Shannon Kimmel <ciervatranquila@gmail.com>
2020-01-12 22:15:29 +0100

    progress

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,11 +1,54 @@
 // Iteration #1: Find the maximum
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+  }
+  else if (a < b) {
+    return b;
+  }
+  else {
+    return 4;
+    // should be [return "These numbers are equal."] - Why does it want 4?
+  }
+}
+console.log(maxOfTwoNumbers(5, 8));
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
+function findLongestWord(array) {
+  var longestWord = "";
+  
+  if (array.length === 0) {
+    return null;
+  }
+
+  array.forEach(function(word) {
+    if(word.length > longestWord.length) {
+      longestWord = word;
+    }
+  });
+  return longestWord;
+}
+
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumArray(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  console.log(sum);
+  return sum;
+  
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
