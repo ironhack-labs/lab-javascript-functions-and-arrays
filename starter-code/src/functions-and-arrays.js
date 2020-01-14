8d35f3dff07d36fc18f592aa6d89fc1d2a10c834
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-01-14 17:36:52 +0100

    Abel Exercise

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,15 +1,68 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers (num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  else if (num1 < num2) {
+    return num2;
+  }
+  else {
+    return "The numbers are equal.";
+  }
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function longestWord(words) {
+  var sortedWord = words.sort(function (longest, shortest) {
+    return shortest.length - longest.length
+  })
+  return sortedWord[0]
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumArray(numbers) {
+  var sumNum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sumNum += numbers[i]
+  }
+  return sumNum
+}
+
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function calculateSum(num) {
+  var sumNum = 0;
+  for (var i = 0; i < num.length; i++) {
+    sumNum += num[i];
+  }
+  var avgNum = sumNum / num.length
+  return avgNum;
+}
+
+
+
+
+
+
 
 // Level 2: Array of strings
 const wordsArr = [
@@ -24,6 +77,17 @@ const wordsArr = [
   'fuel',
   'palace'
 ];
+
+
+
+
+
+
+
+
+
+
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
