c1b03e04a5b464f7259020c538a2a726d8606524
Caio Andreolli <caioandreolli@gmail.com>
2020-01-22 01:09:16 -0300

    Lab feito até #4

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,17 +1,86 @@
 // Iteration #1: Find the maximum
+// Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest.
+
+function maxOfTwoNumbers(num1, num2){
+  if(num1>num2){
+    console.log('#1 THE MAXIMUM IS', num1);
+    return num1;
+  }
+  console.log('#1 THE MAXIMUM IS', num2);
+  return num2;
+}
+
+maxOfTwoNumbers(11,101);
+
+
 
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+// Declare a function named findLongestWord that takes as an argument an array of words and
+// returns the longest one. If there are 2 with the same length, it should return the first occurrence.
+
+const words = ['mystery', 'brotcrodsoher', 'aviator', 'crocodile', 'crocodilo', 'orchard', 'crackpot'];
+
+function findLongestWord(arr){
+  if (arr.length === 0 ){return null};
+  let longestWord = arr[0];
+  arr.forEach(element => {
+    if(element.length > longestWord.length) {
+      longestWord = element;
+    }
+    //console.log(element.length);
+  });
+  console.log('#2 THE LONGEST WORD IS', longestWord);
+  return longestWord;
+}
+
+findLongestWord(words);
+
+
 
 // Iteration #3: Calculate the sum
+// Calculating a sum is as simple as iterating over an array and adding each of the elements together.
+// Declare a function named sumArray that takes as an argument an array of numbers, and returns the sum of
+// all of the numbers in the array. Later in the course we'll learn how to do this by using the reduce array
+// method, which will make your work significantly easier. For now, let's practice "manual" way using loops.
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumArray(arr){
+  let sum = 0;
+  arr.forEach(element => {
+    sum +=element;
+  });
+  console.log('#3 THE SUM IS', sum);
+  return sum;
+}
+
+sumArray(numbers);
+
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+// Declare a function named averageNumbers that expects an array of numbers and returns the average of the numbers:
+
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9, 5];
+//const emptyArray = [];
+
+function averageNumbers(arr){
+  if (arr.length === 0) {return null};
+  let average = sumArray(arr)/arr.length;
+  console.log('#4.1 AVERAGE IS', average);
+  return average;
+}
+
+//averageNumbers(emptyArray);
+averageNumbers(numbersAvg);
+
+
 
 // Level 2: Array of strings
+// Declare a function named averageWordLength that receives as a single argument an
+// array of words and returns the average length of the words:
+
 const wordsArr = [
   'seat',
   'correspond',
@@ -25,7 +94,26 @@ const wordsArr = [
   'palace'
 ];
 
+function averageWordLength(arr){
+  if(arr.length === 0) {return null};
+  let average = 0;
+  arr.forEach(element => {
+    average +=element.length;
+  });
+  average = average/(arr.length);
+  console.log('#4.2 THE AVERAGE LENGTH OF THE WORDS IS', average);
+  return average;
+}
+
+averageWordLength(wordsArr);
+
+
+
 // Iteration #5: Unique arrays
+// Take the following array, remove the duplicates, and return a new array. You're more than
+// likely going to want to check out the indexOf Array method.
+// Do this in the form of a function uniquifyArray that receives an array of words as a argument.
+
 const wordsUnique = [
   'crab',
   'poison',
@@ -39,6 +127,38 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
+function uniquifyArray(arr){
+  let newArray = [];
+  arr.forEach(element => {
+
+  });
+}
+////// INDEX OF É UM LOOP
+////// INDEX OF É UM LOOP
+// Declarou array com atributo 0
+var indices = [];
+// Declarou array e atribuiu
+var arrayTeste = ['a', 'b', 'a', 'c', 'a', 'd'];
+// Declarou um string com valor 'a'
+var element = 'a';
+// Declarou um number que recebe o indexOf de 'a' que é comeca com 0
+var idx = arrayTeste.indexOf(element);
+console.log(arrayTeste.indexOf(element));
+
+while (idx != -1) {
+  indices.push(idx);
+  idx = arrayTeste.indexOf(element, idx + 1);
+  //console.log(idx);
+}
+//console.log(indices);
+// [0, 2, 4]
+
+
+
+
+
+
 
 // Iteration #6: Find elements
 const wordsFind = [
