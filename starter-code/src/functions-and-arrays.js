aa79dfd1cb2557178bc468d63581590799b1a8d0
Sjard Geiersbach <sjard.g@gmail.com>
2020-02-03 20:21:47 +0100

    BER WD PT Sjard

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,18 +1,55 @@
-// Iteration #1: Find the maximum
-
-// Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+
+maxLength = 0;
+longestName = ""
+
+function findLongest (wordsArr) {
+  for (let i = 0; i< wordsArr.length; i++) {
+    if (wordsArr[i].length>maxLength) {
+      maxLength = wordsArr[i].length;
+      longestName = words[i]
+    } 
+  }
+}
+
+
+findLongest(words)
+console.log(maxLength)
+console.log(longestName)
+
+
+
+
+
+// #3
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-// Iteration #4: Calculate the average
-// Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+let sumArray = 0;
 
-// Level 2: Array of strings
-const wordsArr = [
+function summing() {
+  for (let i = 0; i<numbers.length; i++) {
+    sumArray += numbers[i]
+  }
+}
+summing()
+console.log(sumArray);
+
+
+
+
+// #4
+
+function averageNumber () {
+  averageResult = sumArray / numbers.length
+}
+averageNumber()
+console.log(`Average number of the given numbers is ${averageResult}`)
+
+
+
+const wordsGiven = [
   'seat',
   'correspond',
   'linen',
@@ -25,8 +62,22 @@ const wordsArr = [
   'palace'
 ];
 
-// Iteration #5: Unique arrays
-const wordsUnique = [
+let totalWordLength = 0; 
+let averageWordLengthi = 0
+
+function averageWordLength() {
+for (let i = 0; i< wordsGiven.length; i++) {
+  totalWordLength += wordsGiven[i].length; 
+averageWordLengthi = totalWordLength / wordsGiven.length
+}
+}
+
+averageWordLength()
+console.log(`Average word length is: ${averageWordLengthi}`)
+
+// Number 5
+
+const wordsForUnique = [
   'crab',
   'poison',
   'contagious',
@@ -40,8 +91,24 @@ const wordsUnique = [
   'bring'
 ];
 
-// Iteration #6: Find elements
-const wordsFind = [
+    function getUnique(array){
+        var uniqueArray = [];
+        
+        // Loop through array values
+        for(i=0; i < array.length; i++){
+            if(uniqueArray.indexOf(array[i]) === -1) {
+                uniqueArray.push(array[i]);
+            }
+        }
+        return uniqueArray;
+    }
+    
+    
+    var uniqueNames = getUnique(wordsForUnique);
+    console.log("If I delete all duplicates what we have left is: " + uniqueNames)
+
+    // Number 6
+    const words2 = [
   'machine',
   'subset',
   'trouble',
@@ -52,8 +119,16 @@ const wordsFind = [
   'disobedience'
 ];
 
-// Iteration #7: Count repetition
-const wordsCount = [
+let gesucht = ""
+function doesWordExist (wordPool, search) {
+gesucht = wordPool.includes(search);
+console.log(gesucht)
+}
+
+doesWordExist(words2, "eating")
+
+
+const words3 = [
   'machine',
   'matter',
   'subset',
@@ -66,28 +141,14 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+let count = ""
+function howManyTimes(array3,value3){
+    
+    for(i = 0; i < array3.length; i++){
+        if(array3[i] == value3){count++}
+    }
+    return count;
+}
 
-// Iteration #8: Bonus
-
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+howManyTimes(words3, "matter")
+console.log(count)
