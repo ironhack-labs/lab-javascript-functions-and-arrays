7c8d2f1a999a21b469444bb795cfc8fd2170d82c
juliamendesc <juliamendesc@gmail.com>
2020-02-16 13:26:34 +0000

    jasmine lab

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,18 +1,143 @@
 // Iteration #1: Find the maximum
+function maxOfTwoNumbers(a, b) {
+  if (a > b) return a
+  if (a < b) return b
+  if (a === b) return a,b;
+}
+maxOfTwoNumbers(1,2)
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+function findLongestWord(word) {
+  let longest = "";
+  if (word <= 0) {
+    return null;
+  }
 
+//   //ALTERNATIVE
+// if (!Array.length) {
+//   return null;
+// }
+
+  // } else if (words = "") {
+  //   return words;
+  // }
+   for (let i = 0; i < word.length; i++) {
+    if (word[i].length > longest.length) {
+      longest = word[i];
+    }
+  } return longest
+  }
+
+// Iteration #3: Calculate the sum
+function sumNumbers(numbers) {
+  let sum = 0;
+    if (numbers.length == 0){
+    return 0;
+  } 
+  for (let i=0; i <numbers.length; i++) {
+    if (numbers.length == 1) {
+    return sum += numbers[i];
+  } else if (numbers[i] === 0) {
+    return 0;
+  }
+  for (let i=0; i < numbers.length; ++i) {
+    sum += numbers[i]; // add each element in an array to total
+  } return sum; // return sum of elements in array
+}
+}
+// Alternative
+// function sumNumbers (arrayOfNumbers) {
+// let number = 0;
+// for (let i=0; i < arrayOfNumbers.length; i++) {
+  // number = number + arrayOfNumbers[i]; 
+// } return number;
+// }
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+function sum (mixedArr) {
+  let number = 0;
+  if (mixedArr.length == 0){
+    return 0;
+  }
+  for (let i=0; i < mixedArr.length; i++) {
+    if (typeof mixedArr[i] == "string") {
+      number += mixedArr[i].length}
+    else if (typeof mixedArr[i] == "boolean") {
+      number += Number(mixedArr[i])
+    } else {
+      number += mixedArr[i];
+    }  
+  } return number;
+}
+    // if (typeof mixedArr[i] == "object" || "array") {
+    //   let e = new Error("Unsupported data type sir or ma'am");
+    //   throw e;
+    //   }
+  // NOT WORKING
+    
+// function sum(numbers) {
+//   let sum = 0;
+//     if (numbers.length == 0){
+//     return 0;
+//   } 
+//   for (i=0; i <numbers.length; i++) {
+//     if (numbers.length == 1) {
+//     return sum += numbers[i];
+//   } else if (numbers[i] === 0) {
+//     return 0;
+//   }
+//   for (i=0; i < numbers.length; ++i) {
+//     sum += numbers[i]; // add each element in an array to total
+//   } return sum; // return sum of elements in array
+// }
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(numbersAvg) {
+  let sum = 0;
+  if (!numbersAvg.length){
+    return null;
+  }
+  for (let i=0; i < numbersAvg.length; i++) {
+    sum += numbersAvg[i] / numbersAvg.length;
+  } 
+  return Math.round(sum); // return sum of elements in array
+}
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(wordsArr) {
+  let sum = 0;
+  if (!wordsArr.length) {
+    return null;
+  }
+  for (let i=0; i < wordsArr.length; i++) {
+    if (typeof wordsArr[i] == "string") {
+      sum += wordsArr[i].length / wordsArr.length
+    }
+  } return sum;
+}
+
+//BONUS
+function avg (arr) {
+  let number = 0;
+  if (arr.length == 0){
+    return null;
+  }
+  for (let i=0; i < mixedArr.length; i++) {
+    if (typeof mixedArr[i] == "string") {
+      number += mixedArr[i].length
+    } else {
+      number += mixedArr[i];
+    }
+    } return number / mixedArr.length;
+    }
+// Jasmine wants 5.11 (don't know which mixed array it uses), but Ironhack git says 5.7, which makes this CORRECT!!! :)
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -29,8 +154,67 @@ const wordsUnique = [
   'bring'
 ];
 
+function uniquifyArray(wordsUnique) {
+  let indices = 0;
+  if (wordsUnique = []){
+    return null;
+  } 
+  for (let i = 0; i < wordsUnique.length; i++) {
+     if (indices.indexOf(wordsUnique[i]) == -1) {
+      indices.push(wordsUnique[i]);
+     } return indices;
+  }
+}
+  // } else {
+  //   switch(wordsUnique) {
+  //     case crab:
+  //       wordsUnique.indexOf(crab, 0) === 0;
+  //       indices.push(crab);
+  //     case poison:
+  //       wordsUnique.indexOf(poison, 0) === 0;
+  //       indices.push(poison);
+  //     case contagious:
+  //       wordsUnique.indexOf(contagious, 0) === 0;
+  //       indices.push(contagious);
+  //     case simple:
+  //       wordsUnique.indexOf(simple, 0) === 0;
+  //       indices.push(simple);
+  //     case bring:
+  //       wordsUnique.indexOf(bring, 0) === 0;
+  //       indices.push(bring);
+  //     case sharp:
+  //       wordsUnique.indexOf(sharp, 0) === 0;
+  //       indices.push(sharp);
+  //     case playground:
+  //       wordsUnique.indexOf(playground, 0) === 0;
+  //       indices.push(playground);
+  //     case communion:
+  //       wordsUnique.indexOf(communion, 0) === 0;
+  //       indices.push(communion);
+  //   }
+  // } return indices;
+  // } 
+
+
+// JASMINE SOLUTION WITH DIFFERENT WORDS - WHY IS IT COMMING OUT NULL?
+
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+function doesWordExist(wordsFind, newWord) {
+  if (wordsFind = []){
+    return null;
+  }
+for (let i=0; i < wordsFind.length; i++) {
+      if (wordsFind.includes(newWord[i], 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+// DID NOT WORK
+let hasMachine = doesWordExist(wordsFind, "machine");
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -46,6 +230,15 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+
+function howManyTimes(wordsCount, word){
+  var sum = 0;
+  for (let i = 0; i < wordsCount.length; i++) {
+    if (wordsCount[i] === word) {
+      sum += 1;
+  }
+} return sum;   
+}
 
 // Iteration #8: Bonus
 
