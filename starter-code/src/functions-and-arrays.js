b2e91cb4943c8a50e3ff9c0b85b2aa6830c0c223
Fábio Costa <fabiodanielmc@gmail.com>
2020-02-18 15:27:01 +0100

    finish iteration 4

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -27,16 +27,44 @@ function findLongestWord(arr){
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumArray (numArr) {
-  
+function sumNumbers (numArr) {
+  var sum = 0;
+  numArr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 }
+
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers (num){
+  if (num.length === 0){
+    return null;
+  }else{
+    var sumArr = sumNumbers(num);
+    var avg = sumArr/ (num.length);
+    return avg;
+  }
+}
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function  averageWordLength(arr){
+  if (arr.length === 0){
+    return null;
+  }else{
+    var wordLengthArr = [];
+    arr.forEach(element => {
+      wordLengthArr.push(element.length);   
+    });
+    return averageNumbers(wordLengthArr);
+  }
+}
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,6 +80,7 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
