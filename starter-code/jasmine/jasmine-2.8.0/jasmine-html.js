42dbe7222c802ef062440c0bfb7f76faed1735f6
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-02-18 11:53:17 +0100

    first draft

--- starter-code/jasmine/jasmine-2.8.0/jasmine-html.js
+++ starter-code/jasmine/jasmine-2.8.0/jasmine-html.js
@@ -7,7 +7,7 @@ a copy of this software and associated documentation files (the
 without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
+the following conditions:B
 
 The above copyright notice and this permission notice shall be
 included in all copies or substantial portions of the Software.
--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,4 +1,9 @@
 // Iteration #1: Find the maximum
+function maxOfTwoNUmbers(num1,num2) {
+  if (num1>num2) {
+    return num1;
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
