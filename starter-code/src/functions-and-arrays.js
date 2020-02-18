e73aa3fa0f12005d2180bb211c2f6ae53481dda4
“Alexandra <alex.muskovszky@gmail.com>
2020-02-18 11:30:10 +0100

    first commit

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,4 +1,11 @@
 // Iteration #1: Find the maximum
+function maxOfTwoNumbers (num1, num2) {
+  if(num1 > num2) {
+    return num1;
+  } else if (num2>num1) {
+    return num2;
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
