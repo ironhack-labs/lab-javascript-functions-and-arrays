d8a245aea9379b4380f828d2dd4472e01e07777a
Felipe Massao <massao.felipe@gmail.com>
2020-02-20 15:49:48 -0300

    ateh parte 3 feita

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,15 +1,59 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(num1, num2){
+  return Math.max(num1, num2);
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(words){
+  if(!words || words.length === 0) return null;
+  let longestWord = words[0];
+  words.forEach( word => {
+    if(longestWord.length < word.length) longestWord = word; 
+  })
+  return longestWord;
+}
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(numbers) {
+  let sum =0;
+  numbers.forEach( num => sum += num )
+  return sum;
+}
+
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+function sum(mixedArr) {
+  let sum =0;
+  mixedArr.forEach( e => {
+    switch(typeof e){
+      case 'number':
+        sum += e;
+        break;
+      case 'string':
+        sum += e.length;
+        break;
+      case 'boolean':
+        if(e) sum++;
+        break;
+      default:
+        throw new Error("Unsupported data type sir or ma'am");
+    }
+  });
+  return sum;
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers(numbersAvg){
+  let avg =0;
+  numbersAvg.forEach( num => avg += num )
+  return avg/numbersAvg.length;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
