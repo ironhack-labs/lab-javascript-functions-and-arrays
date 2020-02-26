d3d0ee82917441ef71d3846810aade5b1c4658fb
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-02-26 03:26:38 -0300

    correct a problem in the loop of the last function

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -185,8 +185,8 @@ const greatestProduct = matrix => {
         axisX = 0,
         axisY = 0;
 
-    for (let i = 0, size = matrix.length - 4; i < size; i += 1) {
-        for (let j = 0, size = matrix[i].length - 4; j < size; j += 1) {
+    for (let i = 0, size = matrix.length - 3; i < size; i += 1) {
+        for (let j = 0, size = matrix[i].length - 3; j < size; j += 1) {
             axisX = matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
             axisY = matrix[i][j] * matrix[i + 1][j] * matrix[i + 2][j] * matrix[i + 3][j];
             if (axisX > product) product = axisX;
