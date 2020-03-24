4a3940737dd33705700ce69a080bb532ed4cf75e
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-03-25 00:05:22 +0100

    almost done

---
+++ src/maxOfTwoNumbers.js
@@ -0,0 +1,11 @@
+// Iteration #1: Find the maximum
+function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else if (n2 > n1) {
+    return n2;
+  } else {
+    return "They are the same";
+  }
+}
