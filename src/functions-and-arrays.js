743a879f516dcb4f6afa3293def568f05a93404f
Benjamin Chimoy <benjaminchimoy@benjamins-mbp.localdomain>
2020-03-30 09:49:25 +0200

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,82 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+};
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(arr) {
+  let longestWord = "a";
+  if (arr.length === 0) {
+    return null;
+  }
+  for (i = 0; i < arr.length; i++) {
+
+    if (arr[i].length > longestWord.length) {
+      longestWord = arr[i];
+    }
+  }
+  return longestWord;
+}
+
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+
+
+
+
+
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let sumOfWordsLength = 0 ;
+  for(i=0 ; i < arr.length; i++) {
+    sumOfWordsLength += arr[i].length ;
+  }
+
+  return sumOfWordsLength / arr.length ;
+
+}
+
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -28,6 +92,9 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
+
+
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
@@ -46,6 +113,16 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+
+function howManyTimes(haystack, needle) {
+  let count = 0;
+  for(let word of haystack) {
+    if(word === needle) {
+      count++;
+    }
+  }
+  return count;
+}
 
 // Iteration #8: Bonus
 
