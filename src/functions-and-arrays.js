d6a09f12fcf9c5efe4ac7992b7de2c169ba4d2f6
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-03-30 20:07:30 +0200

    Hasta la 3ra iteración

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,15 +1,57 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(num1, num2){
+  return Math.max(num1, num2);
+}
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(wordsArray){
+  if (wordsArray.length === 0){
+    return null;
+  }
+  let tam = 0;
+  let palabraGanadora: null;
+  for(let i = 0; i < wordsArray.length; i++){
+    // contar el tamaño de la string
+      if(wordsArray[i].length > tam){
+        tam = wordsArray[i].length;
+        palabraGanadora = wordsArray[i];
+      }
+  }
+  return palabraGanadora;
+  //Iteración 
+  //Encontrar la palabra más grande
+  // retornar palabra más grande
+
+};
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(arrayNumbers){
+  if (arrayNumbers.length === 0;){
+    return 0;
+  }
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+  function averageNumbres(num){
+
+    // probar si es vacío y retornar null
+    if (num.length == 0){
+      return null;
+
+    let average = sumNumbers(num) / num.length
+    }
+  }
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
