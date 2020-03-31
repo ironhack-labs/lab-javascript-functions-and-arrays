4654ded1d68df46ccd39bf575c2ebea641720183
Justin Ochoa <just.och92@gmail.com>
2020-03-31 18:04:18 -0400

    first-try

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,16 +1,50 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(num1, num2){
+  if(num1>num2){
+return num1;
+  }
+else{
+  return num2;
+}
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
+function findLongestWord(arr) {
+  if (arr.length === 0) {
+    return null
+  }
+let longestWord = "";
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > longestWord.length) {
+        longestWord = arr[i];
+      }
+    }
+    return longestWord;
+}
 // Iteration #3: Calculate the sum
-
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumNumbers(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
+const averageNumbers= (numbers) => {
+  if(numbers.length===0){
+    return null
+  }
+  let sum= 0
+  for (const number of numbers){
+    sum += number
+  }
+  return sum / numbers.length
+}
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
@@ -28,7 +62,10 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
-
+function uniquifyArray(arr){
+  if (arr.length===0)
+  return null;
+}
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
