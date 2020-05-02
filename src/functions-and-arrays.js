1bbfbec3f49ba9c42d9aac8e65e71db4df8d9935
Old <onelifedesigning@gmail.com>
2020-05-02 14:02:16 +0200

    Calculate the sum

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -34,6 +34,15 @@ findLongestWord(words)
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(numbers) {
+    let sumNum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sumNum += numbers[i]
+    }
+    return sumNum
+}
+sumNumbers(numbers)
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
