bf9200f896843e2c4a88a08d1f500a071f5a1d40
Miguel Valle <miguelbachi5a@gmail.com>
2020-05-02 14:53:19 +0200

    Iteration 4 Level 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -63,7 +63,18 @@ function sum(array) {
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(array) {
+  if (array.length === 0){
+    return null
+  }
 
+  let summatory = 0
+  for (let i = 0; i < array.length; i++){
+    summatory += array[i]
+  }
+  const average = summatory / array.length
+  return average
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
