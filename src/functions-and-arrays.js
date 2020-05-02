354311875ac047cf9574a92f08a661c845660ba1
Pumpel <romanbaos@gmail.com>
2020-05-02 13:46:27 +0200

    Fixed typo

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -6,7 +6,7 @@ function maxOfTwoNumbers(firstNumber,secondNumber) {
     return secondNumber
   }
   */
-  return Math.maxOfTwoNumbers(firstNumeber,secondNumber)  
+  return Math.maxOfTwoNumbers(firstNumber,secondNumber)  
 }
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
