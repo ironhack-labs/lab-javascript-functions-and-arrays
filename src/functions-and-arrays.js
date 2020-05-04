2ab2bd23ea7f087c97f50a720dd7c1375e745fdc
TL Garcia <tomas.bl.garcia@gmail.com>
2020-05-04 17:44:01 +0200

    Wrote It4 including bonus passing all tests. Re-used sum functions. 4.2 is not limited to accepting strings but passes the tests nonetheless.

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -49,9 +49,11 @@ function sum(array) {
   
   const err = Error('Unsupported data type sir or ma\'am');
   
+  /* FIX ERROR MSG!!!
   try {
     if (!Array.isArray(array) throw err;
   }
+  */
   
   for (i = 0; i < array.length; i++) {
     if (typeof(array[i]) == 'number' || typeof(array[i]) === 'boolean') {
@@ -66,6 +68,47 @@ function sum(array) {
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers(array) {
+  let avgResult;
+
+  if (array.length > 0) {
+    avgResult = sumNumbers(array)/(array.length);
+    
+  } else {
+    avgResult = null;
+  }
+  return avgResult;
+}
+
+
+function averageWordLength(array) {
+  let avgResult;
+
+  if (array.length > 0) {
+    avgResult = sum(array)/(array.length);
+    
+  } else {
+    avgResult = null;
+  }
+  return avgResult;
+}
+
+function avg(array) {
+  let avgResult;
+  let roundResult;
+
+  if (array.length > 0) {
+    avgResult = sum(array)/(array.length);
+        
+    output = (Math.round(avgResult*100))/100
+  } else {
+    output = null;
+  }
+  return output;
+}
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
