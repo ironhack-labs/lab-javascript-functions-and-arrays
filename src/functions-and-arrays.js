cbb3df6848989cfe237d7611d83cf3defb4e0e0a
martaguirre91 <marta@FONETIC-MSI93.fonetic.local>
2020-05-06 16:03:53 +0200

    js functions

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -137,8 +137,30 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
+
+
+
+
 function greatestProduct(matrix){
-}
+  max_val = 0
+  for (i=0; i < matrix.length; i++){
+    for (j=0; j < matrix.length; j++){
+      if (j-3 >=0){
+        result = matrix[i][j] * matrix[i][j - 1] *  matrix[i][j - 2] * matrix[i][j - 3]; 
+        if (max_val < result) 
+          max_val = result;         
+        } 
+        if ((i - 3) >= 0){ 
+          result = matrix[i][j] * matrix[i - 1][j] *  matrix[i - 2][j] * matrix[i - 3][j]; 
+          if (max_val < result) 
+            max_val = result; 
+        } 
+      }
+
+    }
+    return max_val
+  }
+
 
 function greatestProductOfDiagonals(matrix){
 }
