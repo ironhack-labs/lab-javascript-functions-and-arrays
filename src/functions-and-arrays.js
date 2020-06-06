2bd46a7244b8787444b9bc5da4fca5e978c72ee1
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-06-06 16:06:44 +0200

    iteration-d completed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration #1: Find the maximum
-
+const maxOfTwoNumbers = (num1, num2) => {
+  // if(num1 >= num2){
+  //   return num1;
+  // }else{
+  //   return num2;
+  // }
+  return num1 >= num2 ?  num1 :  num2;
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
