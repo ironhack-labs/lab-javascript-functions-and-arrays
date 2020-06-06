82e8f0b98dad4eb7ec7ad04f189b5b3b07fd7b9e
Willem Prins <willemprins93@gmail.com>
2020-06-06 15:42:36 +0100

    add iteration 2 and 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -6,13 +6,50 @@ function maxOfTwoNumbers(a, b){
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+function findLongestWord(arr){
+  let longestWord = arr[0];
+  for (i = 1; i < arr.length; i++){
+    if (arr[i].length > longestWord.length){
+      longestWord = arr[i];
+    }
+  }
+  return longestWord;
+}
 
+// Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumNumbers(arr){
+  let sum = 0;
+  arr.forEach(function(number){
+    sum += number;
+  })
+  return sum;
+}
+
+function sum(arr){
+  sum = 0;
+  for (i in arr){
+    if (typeof arr[i] == String){
+      sum += arr[i].length;
+    } else if (typeof arr[i] == Boolean){
+      if (arr[i] == true){
+        sum += 1;
+      } else {
+        sum += 0;
+      }
+    } else {
+      sum += arr[i];
+    }
+    return sum;
+  }
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
