6a508ddfe3894ea11db8060f21f164d1b2198762
abWasHere <audrey.belson@gmail.com>
2020-06-12 09:47:16 +0200

    end of the lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -21,11 +21,6 @@ function findLongestWord(arr) {
   return longuestWord;
 }
 
-/*
-var test = findLongestWord(words);
-console.log(test);
-*/
-
 // Iteration #3: Calculate the sum
 
 function sumNumbers(arr) {
@@ -55,12 +50,14 @@ function sum(arr) {
   return count;
 }
 
-let badArr = ["de", 16, "e", true, [1, 2]];
-
+let badArr = ["de", 16, "e", true, [1, 2]]; // should throw an error
 let sumBig = sum(mixedArr);
-//console.log(sumBig);
+console.log("sum Function check : " + sumBig);
+/* let sumBadArr= sum(badArr);
+console.log(sumBadArr); */
 
 // Iteration #4: Calculate the average
+
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
@@ -96,7 +93,7 @@ function averageWordLength(strArr) {
 
 let wordUn = ["un"];
 let testWordAv = averageWordLength(wordUn);
-console.log(testWordAv);
+console.log("averageWordLength Function check : " + testWordAv);
 
 // bonus it 4 level 2
 
@@ -112,9 +109,7 @@ function avg(mixedArr) {
 
 let wordMix = [6, 12, "miami", 1, "barca", "200", "lisboa", 8, false];
 let testWordMix = avg(wordMix);
-console.log(testWordMix);
-
-// --------------------------------------
+console.log("avg (of mixed arr) Function check : " + testWordMix);
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -146,6 +141,7 @@ function uniquifyArray(arr) {
 }
 
 let testUniq = uniquifyArray(wordsUnique);
+console.log("uniquifyArray Function check : ");
 console.log(testUniq);
 
 // Iteration #6: Find elements
@@ -191,323 +187,70 @@ function howManyTimes(arr, word) {
 
 // Iteration #8: Bonus
 
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [
-    49,
-    49,
-    99,
-    40,
-    17,
-    81,
-    18,
-    57,
-    60,
-    87,
-    17,
-    40,
-    98,
-    43,
-    69,
-    48,
-    4,
-    56,
-    62,
-    0,
+let matrix = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1][
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 100, 20, 1, 1, 1, 1, 1, 1, 1, 1, 1)
   ],
-  [
-    81,
-    49,
-    31,
-    73,
-    55,
-    79,
-    14,
-    29,
-    93,
-    71,
-    40,
-    67,
-    53,
-    88,
-    30,
-    3,
-    49,
-    13,
-    36,
-    65,
-  ],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [
-    22,
-    31,
-    16,
-    71,
-    51,
-    67,
-    63,
-    89,
-    41,
-    92,
-    36,
-    54,
-    22,
-    40,
-    40,
-    28,
-    66,
-    33,
-    13,
-    80,
-  ],
-  [
-    24,
-    47,
-    32,
-    60,
-    99,
-    3,
-    45,
-    2,
-    44,
-    75,
-    33,
-    53,
-    78,
-    36,
-    84,
-    20,
-    35,
-    17,
-    12,
-    50,
-  ],
-  [
-    32,
-    98,
-    81,
-    28,
-    64,
-    23,
-    67,
-    10,
-    26,
-    38,
-    40,
-    67,
-    59,
-    54,
-    70,
-    66,
-    18,
-    38,
-    64,
-    70,
-  ],
-  [
-    67,
-    26,
-    20,
-    68,
-    2,
-    62,
-    12,
-    20,
-    95,
-    63,
-    94,
-    39,
-    63,
-    8,
-    40,
-    91,
-    66,
-    49,
-    94,
-    21,
-  ],
-  [
-    24,
-    55,
-    58,
-    5,
-    66,
-    73,
-    99,
-    26,
-    97,
-    17,
-    78,
-    78,
-    96,
-    83,
-    14,
-    88,
-    34,
-    89,
-    63,
-    72,
-  ],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [
-    16,
-    39,
-    5,
-    42,
-    96,
-    35,
-    31,
-    47,
-    55,
-    58,
-    88,
-    24,
-    0,
-    17,
-    54,
-    24,
-    36,
-    29,
-    85,
-    57,
-  ],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [
-    19,
-    80,
-    81,
-    68,
-    5,
-    94,
-    47,
-    69,
-    28,
-    73,
-    92,
-    13,
-    86,
-    52,
-    17,
-    77,
-    4,
-    89,
-    55,
-    40,
-  ],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [
-    88,
-    36,
-    68,
-    87,
-    57,
-    62,
-    20,
-    72,
-    3,
-    46,
-    33,
-    67,
-    46,
-    55,
-    12,
-    32,
-    63,
-    93,
-    53,
-    69,
-  ],
-  [
-    4,
-    42,
-    16,
-    73,
-    38,
-    25,
-    39,
-    11,
-    24,
-    94,
-    72,
-    18,
-    8,
-    46,
-    29,
-    32,
-    40,
-    62,
-    76,
-    36,
-  ],
-  [
-    20,
-    69,
-    36,
-    41,
-    72,
-    30,
-    23,
-    88,
-    34,
-    62,
-    99,
-    69,
-    82,
-    67,
-    59,
-    85,
-    74,
-    4,
-    36,
-    16,
-  ],
-  [
-    20,
-    73,
-    35,
-    29,
-    78,
-    31,
-    90,
-    1,
-    74,
-    31,
-    49,
-    71,
-    48,
-    86,
-    81,
-    16,
-    23,
-    57,
-    5,
-    54,
-  ],
-  [
-    1,
-    70,
-    54,
-    71,
-    83,
-    51,
-    54,
-    69,
-    16,
-    92,
-    33,
-    48,
-    61,
-    43,
-    52,
-    1,
-    89,
-    19,
-    67,
-    48,
-  ],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 20, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+// should return 20 000
 
-function greatestProduct (arr) {
-  
+function greatestProduct(matrixArr) {
+  if (matrixArr.length === 0) {
+    return null;
+  } else {
+    // new list that will contain the products of 4 elements in rows and in columns:
+    let productsList = [];
+
+    // this nested loop pushes the products in ROW in the list of products :
+    for (let i = 0; i < matrixArr.length; i++) {
+      for (let j = 0; j < matrixArr[i].length - 4; j++) {
+        productsList.push(
+          matrixArr[i][j] *
+            matrixArr[i][j + 1] *
+            matrixArr[i][j + 2] *
+            matrixArr[i][j + 3]
+        );
+      }
+    }
+
+    // this nested loop pushes the products in COLUMN in the list of products :
+    for (let j = 0; j < matrixArr.length; j++) {
+      for (let i = 0; i < matrixArr.length - 4; i++) {
+        productsList.push(
+          matrixArr[i][j] *
+            matrixArr[i + 1][j] *
+            matrixArr[i + 2][j] *
+            matrixArr[i + 3][j]
+        );
+      }
+    }
+
+    // now, I search in productsList which one is the highest and store it in a new var :
+    let maxProduct = 0;
+    for (let z = 0; z < productsList.length; z++) {
+      if (productsList[z] > maxProduct) maxProduct = productsList[z];
+    }
+    return maxProduct;
+  }
 }
+
+let testGreatProduct = greatestProduct(matrix);
+console.log("greatestProduct Function check : " + testGreatProduct);
