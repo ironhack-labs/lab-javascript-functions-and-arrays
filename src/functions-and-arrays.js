0f4cb7cff7fd613938e03245ba0ecd0086b35074
Claudia Layrisse <clayrisse@ClaudiaLayrisse.localdomain>
2020-07-02 00:42:31 +0200

    intento 2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,93 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(num1,num2){
+  if (num1==num2){
+    return num1;
+  }else if (num1 > num2){
+    return num1;
+  }else {
+    return num2;
+  }
+}
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpothhhhhhhhh'];
+function findLongestWord(arr){
+  if (arr.length == 0){
+    return null;
+  }
+ 
+ let comparator = arr[0];
+  for (let i = 0; i < arr.length; i++){
+    if (comparator.length < arr[i].length) {
+      comparator = arr[i];
+    } 
+  }
+   return comparator;
+} 
+findLongestWord(words);
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumNumbers(arr){
+  if (arr.length == 0){
+    return 0;
+} 
+let sum = 0;
+for (let i=0; i < arr.length; i++) {
+ sum += arr[i];
+} 
+return sum
+}
+//3.1
+function sum(arr) {
+  if (arr.length == 0){
+    return 0;
+} 
+let sum = 0;
+for (let i=0; i < arr.length; i++) {
+ sum += arr[i];
+} 
+return sum
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(arr) { 
+  if (arr.length == 0){
+    return null;
+} 
+//let sum = 0;
+//for (let i=0; i < arr.length; i++) {
+// sum += arr[i];
+//} 
+//return sum
+let average = sum(arr) / arr.length;
+ return average
+}
+
+
+
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(arr){
+  if (arr.length == 1){
+    return arr.length;
+} 
+  let arrLength = []
+  for (let x = 0; x < arr.length; x++) {
+    arrLength.push(arr[i])
+  }
+  let strAverage = averageNumbers(arrLength);
+ return strAverage
+}
+
+
+
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -28,6 +103,15 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
+function uniquifyArray(arr) {
+  if (arr.length == 0){
+    return null;
+}
+
+}
+
+
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
