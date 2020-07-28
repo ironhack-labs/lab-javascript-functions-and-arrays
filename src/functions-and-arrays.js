c49d8804626a47be6f5aac7cd5c8a69edbdda689
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-07-28 15:07:13 +0200

    finish third problem

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -25,6 +25,14 @@ function findLongestWord(arr){
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers (numbersArr){
+  let sum = 0;
+  for (i = 0; i < numbersArr.length; i++){
+    sum = numbersArr[i] + sum
+  }
+  return sum
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
