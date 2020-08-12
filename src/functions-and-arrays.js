670822cb28ae6ea7c61e49fc6f06c29c4c77e28c
Jonathan del Rio <delriog.jonathan@gmail.com>
2020-08-12 09:19:39 +0200

    lab functions and arrays Jonathan del Rio

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,16 +1,71 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers (number1,number2) {
+
+  return Math.max (number1, number2) 
+}
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord (words) {
+
+  if (words.length === 0){
+
+    return null;
+  }
+  let word = "";
+  for (let i=0; i < words.length; i++) {
+    if (word.length < words[i].length) {
+      word = words[i]
+    }
+  }
+
+  return word
+
+}
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers (numbers) {
+
+  if (numbers.length === 0) {
+
+    return 0;
+  }
+let sum = 0;
+for (let i=0; i<numbers.length; i++) {
+
+  sum += numbers[i]
+}
+
+return sum 
+}
+
+
+  
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers (numbersAvg) {
+
+  if (numbersAvg.length === 0){
+    return null;
+  }
+
+  let sum = 0;
+  for( var i = 0; i < numbersAvg.length; i++ ){
+    sum += parseInt( numbersAvg[i], 10 ); //don't forget to add the base
+}
+
+var avg = sum/elmt.length;
+}
+ 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
