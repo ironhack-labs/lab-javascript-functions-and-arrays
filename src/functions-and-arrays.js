4366be37d796a0994a2f23e1faf40f7d5e35641b
Lcdevi <caillauxlaura@gmail.com>
2020-08-27 16:52:54 +0200

    iteration 3 : done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -53,16 +53,18 @@ function sumNumbers (arr2) {
       console.log(sum)
     } if (sum === 0) {
       return 0;
+    } else if (sum > 0) {
+      return sum;
     }
   }
 }
-  
-
 
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
