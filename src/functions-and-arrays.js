94772b0b8cddc0f2bcbccb72083c864885d4fe32
Emma Northridge <emmanorthridge1@gmail.com>
2020-10-13 13:54:13 +0200

    changes

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -40,6 +40,7 @@ const sumNumbers = numbers => {
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+j
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
