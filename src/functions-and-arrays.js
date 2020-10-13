e0d20659dcea0606af9c6ab832d201b5ff1cb255
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-13 21:32:20 +0200

    two attempts to do bonus #8, commented out for now

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,37 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(num1, num2) {
   if (num1 > num2) {
-    return num1
+    return num1;
   } else {
-    return num2
+    return num2;
   }
 }
 
-
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
 function findLongestWord(array) {
-  let longestWord = ""
+  let longestWord = "";
   if (!array.length) {
     return null;
   }
 
   for (let i = 0; i < array.length; i++) {
     if (longestWord.length < array[i].length) {
-      longestWord = array[i]
+      longestWord = array[i];
     } else {
       continue;
     }
-
-
-
-
   }
-  return longestWord
+  return longestWord;
 }
 
 // Iteration #3: Calculate the sum
@@ -36,40 +39,35 @@ function findLongestWord(array) {
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(array) {
-  let sum = 0
+  let sum = 0;
 
   for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] != 'number') {
+    if (typeof array[i] != "number") {
       continue;
-
     } else {
-      sum = sum + array[i]
-
+      sum = sum + array[i];
     }
-
   }
   return sum;
-
 }
 
 function sum(array) {
-  let sum = 0
+  let sum = 0;
 
   for (let i = 0; i < array.length; i++) {
     if (
-      !(typeof array[i] === 'number' ||
+      !(
+        typeof array[i] === "number" ||
         typeof array[i] === "string" ||
-        typeof array[i] === 'boolean')
+        typeof array[i] === "boolean"
+      )
     ) {
-      throw new Error("Unsupported data type sir or ma'am")
-
-    } else if (typeof array[i] === 'number') {
-      sum = sum + array[i]
-
+      throw new Error("Unsupported data type sir or ma'am");
+    } else if (typeof array[i] === "number") {
+      sum = sum + array[i];
     } else if (typeof array[i] === "string") {
-      sum = sum + array[i].length
-
-    } else if (typeof array[i] === 'boolean') {
+      sum = sum + array[i].length;
+    } else if (typeof array[i] === "boolean") {
       if (array[i] === true) {
         sum++;
       } else {
@@ -78,111 +76,119 @@ function sum(array) {
     }
   }
   return sum;
-
 }
-
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function averageNumbers(array) {
-
   if (!array.length) {
     return null;
   } else {
-    let sum = 0
+    let sum = 0;
     for (let i = 0; i < array.length; i++) {
-      sum = sum + array[i]
+      sum = sum + array[i];
     }
-    let average = sum / array.length
-    return average
+    let average = sum / array.length;
+    return average;
   }
-
 }
 
 // Level 2: Array of strings
-const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
-
+const wordsArr = [
+  "seat",
+  "correspond",
+  "linen",
+  "motif",
+  "hole",
+  "smell",
+  "smart",
+  "chaos",
+  "fuel",
+  "palace",
+];
 
 function averageWordLength(array) {
   if (!array.length) {
     return null;
   } else {
-    let sum = 0
+    let sum = 0;
     for (let i = 0; i < array.length; i++) {
-      sum = sum + array[i].length
+      sum = sum + array[i].length;
     }
-    let average = sum / array.length
-    return average
+    let average = sum / array.length;
+    return average;
   }
-
 }
 
 function avg(arr) {
-  let sum = 0
+  let sum = 0;
   if (!arr.length) {
-    return null
+    return null;
   }
 
   for (let i = 0; i < arr.length; i++) {
     if (
-      !(typeof arr[i] === 'number' ||
+      !(
+        typeof arr[i] === "number" ||
         typeof arr[i] === "string" ||
-        typeof arr[i] === 'boolean')
+        typeof arr[i] === "boolean"
+      )
     ) {
-      throw new Error("Unsupported data type sir or ma'am")
-    } else if (typeof arr[i] === 'number') {
-      sum = sum + arr[i]
-
+      throw new Error("Unsupported data type sir or ma'am");
+    } else if (typeof arr[i] === "number") {
+      sum = sum + arr[i];
     } else if (typeof arr[i] === "string") {
-      sum = sum + arr[i].length
-
-    } else if (typeof arr[i] === 'boolean') {
+      sum = sum + arr[i].length;
+    } else if (typeof arr[i] === "boolean") {
       if (arr[i] === true) {
         sum++;
       } else {
         continue;
       }
-
     }
-
   }
-  let average = (sum / arr.length)
-  return parseFloat(average.toFixed(2))
-
+  let average = sum / arr.length;
+  return parseFloat(average.toFixed(2));
 }
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
-  'crab',
-  'poison',
-  'contagious',
-  'simple',
-  'bring',
-  'sharp',
-  'playground',
-  'poison',
-  'communion',
-  'simple',
-  'bring'
+  "crab",
+  "poison",
+  "contagious",
+  "simple",
+  "bring",
+  "sharp",
+  "playground",
+  "poison",
+  "communion",
+  "simple",
+  "bring",
 ];
 
 function uniquifyArray(array) {
   if (!array.length) {
     return null;
-
   } else {
     array = Array.from(new Set(array));
-
   }
 
-  return array
+  return array;
 }
 
-
 // Iteration #6: Find elements
-const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+const wordsFind = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
 function doesWordExist(array, word) {
   if (!array.length) {
@@ -190,70 +196,398 @@ function doesWordExist(array, word) {
   } else {
     for (let i = 0; i < array.length; i++) {
       if (array[i] === word) {
-        return true
+        return true;
       } else {
         continue;
       }
-
     }
   }
-  return false
+  return false;
 }
 
 // Iteration #7: Count repetition
 const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
+  "machine",
+  "matter",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "matter",
+  "truth",
+  "disobedience",
+  "matter",
 ];
 
 function howManyTimes(array, word) {
-  let sum = 0
+  let sum = 0;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === word) {
-      sum++
+      sum++;
     } else {
       continue;
     }
-
   }
-  return sum
+  return sum;
 }
 
 // Iteration #8: Bonus
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+  [
+    49,
+    49,
+    99,
+    40,
+    17,
+    81,
+    18,
+    57,
+    60,
+    87,
+    17,
+    40,
+    98,
+    43,
+    69,
+    48,
+    4,
+    56,
+    62,
+    0,
+  ],
+  [
+    81,
+    49,
+    31,
+    73,
+    55,
+    79,
+    14,
+    29,
+    93,
+    71,
+    40,
+    67,
+    53,
+    88,
+    30,
+    3,
+    49,
+    13,
+    36,
+    65,
+  ],
   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+  [
+    22,
+    31,
+    16,
+    71,
+    51,
+    67,
+    63,
+    89,
+    41,
+    92,
+    36,
+    54,
+    22,
+    40,
+    40,
+    28,
+    66,
+    33,
+    13,
+    80,
+  ],
+  [
+    24,
+    47,
+    32,
+    60,
+    99,
+    3,
+    45,
+    2,
+    44,
+    75,
+    33,
+    53,
+    78,
+    36,
+    84,
+    20,
+    35,
+    17,
+    12,
+    50,
+  ],
+  [
+    32,
+    98,
+    81,
+    28,
+    64,
+    23,
+    67,
+    10,
+    26,
+    38,
+    40,
+    67,
+    59,
+    54,
+    70,
+    66,
+    18,
+    38,
+    64,
+    70,
+  ],
+  [
+    67,
+    26,
+    20,
+    68,
+    2,
+    62,
+    12,
+    20,
+    95,
+    63,
+    94,
+    39,
+    63,
+    8,
+    40,
+    91,
+    66,
+    49,
+    94,
+    21,
+  ],
+  [
+    24,
+    55,
+    58,
+    5,
+    66,
+    73,
+    99,
+    26,
+    97,
+    17,
+    78,
+    78,
+    96,
+    83,
+    14,
+    88,
+    34,
+    89,
+    63,
+    72,
+  ],
   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+  [
+    16,
+    39,
+    5,
+    42,
+    96,
+    35,
+    31,
+    47,
+    55,
+    58,
+    88,
+    24,
+    0,
+    17,
+    54,
+    24,
+    36,
+    29,
+    85,
+    57,
+  ],
   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+  [
+    19,
+    80,
+    81,
+    68,
+    5,
+    94,
+    47,
+    69,
+    28,
+    73,
+    92,
+    13,
+    86,
+    52,
+    17,
+    77,
+    4,
+    89,
+    55,
+    40,
+  ],
   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+  [
+    88,
+    36,
+    68,
+    87,
+    57,
+    62,
+    20,
+    72,
+    3,
+    46,
+    33,
+    67,
+    46,
+    55,
+    12,
+    32,
+    63,
+    93,
+    53,
+    69,
+  ],
+  [
+    4,
+    42,
+    16,
+    73,
+    38,
+    25,
+    39,
+    11,
+    24,
+    94,
+    72,
+    18,
+    8,
+    46,
+    29,
+    32,
+    40,
+    62,
+    76,
+    36,
+  ],
+  [
+    20,
+    69,
+    36,
+    41,
+    72,
+    30,
+    23,
+    88,
+    34,
+    62,
+    99,
+    69,
+    82,
+    67,
+    59,
+    85,
+    74,
+    4,
+    36,
+    16,
+  ],
+  [
+    20,
+    73,
+    35,
+    29,
+    78,
+    31,
+    90,
+    1,
+    74,
+    31,
+    49,
+    71,
+    48,
+    86,
+    81,
+    16,
+    23,
+    57,
+    5,
+    54,
+  ],
+  [
+    1,
+    70,
+    54,
+    71,
+    83,
+    51,
+    54,
+    69,
+    16,
+    92,
+    33,
+    48,
+    61,
+    43,
+    52,
+    1,
+    89,
+    19,
+    67,
+    48,
+  ],
 ];
-
+/*
 function greatestProduct(matrix) {
+  let product = 0;
+  let horMin;
+  let horPlus;
+  let verMin;
+  let verPlus;
+  for (let i = 3; i < matrix.length - 3; i++) {
+    for (let j = 3; j < matrix[i].length - 3; j++) {
+      horMin =
+        matrix[i][j] * matrix[i - 1][j] * matrix[i - 2][j] * matrix[i - 3][j];
+      horPlus =
+        matrix[i][j] * matrix[i++][j] * matrix[i + 2][j] * matrix[i + 3][j];
+      verMin =
+        matrix[i][j] * matrix[i][j - 1] * matrix[i][j - 2] * matrix[i][j - 3];
+      verPlus =
+        matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
+
+      if (product < horPlus) {
+        product = horPlus;
+      }
+      if (product < horMin) {
+        product = horMin;
+      }
+      if (product < verMin) {
+        product = verMin;
+      }
+      if (product < verPlus) {
+        product = verPlus;
+      }
+    }
+    return product;
+  }
+}
+greatestProduct(matrix);
+*/
+
+/*function greatestProduct(matrix) {
   let product = 0
   for (let i = 0; i < matrix.length; i++) {
 
@@ -285,3 +619,4 @@ function greatestProduct(matrix) {
   }
   return product
 }
+*/
