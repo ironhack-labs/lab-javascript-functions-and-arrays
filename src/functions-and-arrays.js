97b06bf7710e9c17a01cc605578c6c35a9df3646
RFeldman84 <rachel.feldman84@gmail.com>
2020-10-13 13:21:50 -0400

    use reduce mixed array, start trying matrix

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -33,9 +33,12 @@ function notStrings(){
    return "enter words ONLY"
 }
 
+
 function nullArrayStrings(array){
   return !array || array.length ===  0 || Array.isArray(array) === false 
  }
+
+
 // Iteration #1: Find the maximum
 // Define a function `maxOfTwoNumbers` that takes two numbers as arguments and returns the largest.
 
@@ -75,12 +78,12 @@ cl('----------------------')
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
 const words2 = ['mystery', false , 'aviator'];
 
 
+
 function findLongestWord(array){
-  if(nullArrayStrings(array)) return null; 
+  if(nullArrayStrings(array)) return null;
   if(checkStingsArr(array) === false) return notStrings()
   let longestWord = array[0]
   for(elem of array){
@@ -106,6 +109,8 @@ cl('----------------------')
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 const numbers2 = [6, "12", 1, 18];
 const numbers3 = []
+
+///reduce
 function sumNumbers(array){
   if(nullArrayNumbers(array)) return null;
   if(checkNumbersArr(array)=== false) return notNumber();
@@ -115,7 +120,7 @@ function sumNumbers(array){
   return sumOfArray
 }
 
-//or
+//OR
 // function sumNumbers(array){
 //   if(nullArrayNumbers(array)) return null;
 //   if(checkNumbersArr(array)=== false) return notNumber();
@@ -143,41 +148,46 @@ cl('----------------------')
 
 // #### Bonus - Iteration #3.1: A generic `sum()` function
 
-// **The goal: Learn how to refactor your code.** :muscle:
-
-// In the iteration 3, you created a function that returns the sum of an array of numbers. But what if we wanted to know how much is the sum of the length of all of the words in an array? What if we wanted to add _boolean_ values to the mix? We wouldn't be able to use the same function as above, or better saying, we would have to _tweak_ it a little bit so that it can be reused no matter what is in the array that is passed as argument when function `sumNumbers()` is called.
-
-// Here we are applying a concept we call **polymorphism**, that is, dealing with a functions' input independently of the types they are passed as.
-
-// Let's create a new function `sum()` that calculates the sum for array filled with (_almost_) any type of data. Note that strings should have their length added to the total, and boolean values should be coerced into their corresponding numeric values. Check the tests for more details.
-
 //****GOOOOOOOGLE!********Bonus: Calculate the sum should throw an error when unsupported data type (object or array) present in the array
 
-const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
-// should return: 57
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10]; // should return: 57
+
+const mixedArr2 = [6, [], 12, 'miami', 1, true, {}, 'barca', '200', 'lisboa', 8, 10]; // 12 - 2 errors [] {}
+
+// function sum(array){
+//   nullArrayStrings(array)
+//   let sumArray = 0
+//   for(let i = 0; i < array.length; i++){ 
+//     switch(typeof array[i]){
+//       case "string":
+//         sumArray += array[i].length
+//         break;
+//       case "boolean": 
+//         sumArray += Number(array[i])
+//         break;
+//       case  "number": 
+//         sumArray += array[i]
+//         break;
+//       //default:
+//         //...throw error stuff
+        
+//     }
+      
+//   }
+//   return sumArray
+// }
+
+
+////OR...with REDUCE():
 
 function sum(array){
-  nullArrayStrings(array)
-  let sumArray = 0
-  for(let i = 0; i < array.length; i++){ 
-    switch(typeof array[i]){
-      case "string":
-        sumArray += array[i].length
-        break;
-      case "boolean": 
-        sumArray += Number(array[i])
-        break;
-      case  "number": 
-        sumArray += array[i]
-        break;
-      //default:
-        //...throw error stuff
-        
-    }
-      
-  }
-  return sumArray
+  if(nullArrayStrings(array)) return null;
+  return array.reduce((acc, curr)=>{
+    return acc + (typeof curr === "string" ?  curr.length  : typeof curr === "boolean" ? Number(curr) : typeof curr ==="number" ? curr : 0)
+  },0)    
 }
+
+
 
 cl(`sum(mixedArr) should ===> 57:`)
 cl(sum(mixedArr))
@@ -191,7 +201,9 @@ const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 // sum - 48  avg - 6
 function averageNumbers(array){
   if(nullArrayStrings(array)) return null;
-  return sum(array)/ array.length  
+  //return sum(array)/ array.length  
+  //or
+  return sumNumbers(array)/ array.length
 }
 cl('~~~ averageNumbers(array) ~~~')
 cl(`[${numbersAvg}] should ==> 6:`)
@@ -201,8 +213,29 @@ cl(averageNumbers([]))
 
 
 
+cl('----------------------------------')
+// #### Bonus - Iteration #4.1: A generic `avg()` function
+
+// Create function `avg(arr)` that receives any mixed array and calculates average. Consider as mixed array an array filled with numbers and/or strings and/or booleans. We are following a similar logic to the one applied on the bonus iteration 4.1.
+// // should return: 5.7
+
+
+
+
+// #### Level 2: Array of strings
+
+// Declare a function named `averageWordLength` that receives as a single argument an array of words and returns the average length of the words:
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+
+function averageWordLength(array){
+
+}
+
+
+
+cl('----------------------------------')
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -219,8 +252,32 @@ const wordsUnique = [
   'bring'
 ];
 
+
+
+
+
+
+
+
+cl('----------------------------------')
+
+
 // Iteration #6: Find elements
+// ### Iteration #6: Find elements
+// Let's create a simple array search.
+// Declare a function named `doesWordExist` that will take in an array of words as one argument, and a word to search for as the other. Return `true` if it exists, otherwise, return `false`. **Don't** use `indexOf` for this one.
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+
+
+
+
+
+
+cl('----------------------------------')
+
+// ### Iteration #7: Count repetition
+// Declare a function named `howManyTimes` that will take in an array of words as the first argument, and a word to search for as the second argument. The function will return the number of times that word appears in the array.
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -237,7 +294,21 @@ const wordsCount = [
   'matter'
 ];
 
-// Iteration #8: Bonus
+
+
+cl('----------------------------------')
+
+// ### Bonus - Iteration #8: Product of adjacent numbers
+
+// What is the greatest product of four adjacent numbers? We consider adjacent any four numbers that are next to each other horizontally or vertically.
+
+
+// ### Bonus - Iteration #8.1: Product of diagonals
+
+// Following the logic you've used in iteration #8, declare a function called `greatestProductOfDiagonals(matrix)`. It takes a matrix as a parameter and returns the greatest product of any four values layed out diagonally, in either direction.
+
+
+
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
@@ -261,3 +332,284 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+
+///....code REFACTOR for bigger grid
+
+// function greatestProduct(matrix){
+  
+
+
+// }
+  
+
+//cl(`~~greatestProduct(matrix)~~ should be 51267216: --->`)
+//cl(greatestProduct(matrix)) //51267216
+
+//// ~~~ NOTES ~~~~  // 1. Product of adjacent numbers
+
+
+
+
+ // function greatestProductOfDiagonals(matrix){
+  
+// }
+
+
+//cl(`~~`greatestProductOfDiagonals(matrix)`~~ should be : --->`)
+//cl(greatestProductOfDiagonals(matrix)) //
+//cl('-------------------------------')
+
+
+
+// /////////////////////////////////
+
+//// notes
+// if(i<2){
+//   cl(sx[i][i] * sx[i][i+1] * sx[i][i+2]*sx[i][i+3])
+//   }
+//first row [0][0]  1,2,3,4, & 2nd row [1][1] 20,3,4,5 stops might be useful for diagonally not this(adj)!
+
+
+
+
+
+
+
+// Iteration #8: Bonus
+
+// 1. Product of adjacent numbers
+// 2. Iteration #8.1: Product of diagonals
+// Declare a function named `greatestProduct(matrix)` to find it in the 20×20 grid below!
+// The greatest product will be the `20`x`20`x`20`x`4` = `32000`.
+
+// let smx = [
+//   [ 1,  2, 3, 4, 5], 
+//   [ 1, 20, 3, 4, 5], 
+//   [ 1, 20, 3, 4, 5],
+//   [ 1, 20, 3, 4, 5],
+//   [ 1,  4, 3, 4, 5],
+//   ]
+  
+  
+  // function greatestProduct(smx){
+  
+  // }
+  
+//cl(`~~greatestProduct(smx)~~ should be 3200: --->`)
+//cl(greatestProduct(smx)) //32000
+
+ // function greatestProductOfDiagonals(smx){
+  
+  // }
+
+
+//cl(`~~`greatestProductOfDiagonals(smx)`~~ should be : --->`)
+//cl(greatestProductOfDiagonals(smx)) //
+
+
+//// ~~~ NOTES ~~~~  // 1. Product of adjacent numbers
+// nested loops 1. for 1st[index] stay at same until 2 iterations(stop index 2) 2. for 2nd[index]and conditions for each 
+//itterate vertically n horizontally
+//if hor[i][2] ver [2][i] need stop if less than 4 nums (stop index 2 horizontally & vertically)
+//using smx.length whole and smx[0].length rows
+//hor smx[j][i]?
+//vert smx[i][j] * smx[i+1][j].....
+
+
+// let results = 0 
+// // let tempResults =  example =>
+// smx[0][i]*smx[0][i+1]*smx[i+2][]*smx[i+3]
+//  if(tempResults > results) results = tempResults 
+
+
+//TEST LOOPS
+
+// let j=0
+// let i=0
+// for(; i < smx.length-1; i++){
+//   if(i>= 0 && i<2)
+//     cl(`i ${i} : [i]: ${smx[i]}`)
+//   cl(smx[j][i] * smx[j][i+1] * smx[j][i+2]* smx[j][i+3]) // does each row
+//   for(j; j < smx.length-1; j++) {
+//     if( j>=0 && j<2)
+//    cl(smx[j][i] * smx[j][i+1] * smx[j][i+2]* smx[j][i+3])
+//    cl([smx[j][i] , smx[j][i+1] , smx[j][i+2], smx[j][i+3]]) 
+//   cl(smx[i][j] * smx[i][j+1] * smx[i][j+2]* smx[i][j+3]) 
+//   cl([smx[i][j] , smx[i][j+1] , smx[i][j+2], smx[i][j+3]]) 
+//   cl(`j ${j} : [j]: ${smx[j]}`)
+//    }
+  
+//   }
+
+// cl('----------------------------------')
+
+
+// for(let i=0; i < smx.length-1; i++){
+//   if(i>= 0 && i<2)
+//   cl(`OUTTER i: index- ${i} : [i]- ${smx[i]}`)
+//   cl(`OUTTER j: index- ${j} : [j]- ${smx[j]}`)
+  
+//  cl(`OUTTER[i][j]: [${smx[i][j]} ,${smx[i][j+1]}, ${smx[i][j+2]}, ${smx[i][j+3]}] --->`)
+//  cl(smx[i][j] * smx[i][j+1] * smx[i][j+2]* smx[i][j+3])
+
+//   cl(`OUTTER[j][i]:[${smx[j][i]} ,${smx[j][i+1]}, ${smx[j][i+2]}, ${smx[j][i+3]}] --->`)
+//   cl(smx[j][i] * smx[j][i+1] * smx[j][i+2]* smx[j][i+3]) // does each row
+  
+  
+//   for(let j=0; j < smx.length-1; j++) {
+//     if( j>=0 && j<2)
+    
+//   cl(`INNER[j][i]: [${smx[j][i]} , ${smx[j][i+1]} , ${smx[j][i+2]}, ${smx[j][i+3]}] --> `) 
+//   cl(smx[j][i] * smx[j][i+1] * smx[j][i+2]* smx[j][i+3])
+   
+//  cl(`INNER[i][j]: [${smx[i][j]} , ${smx[i][j+1]} , ${smx[i][j+2]}, ${smx[i][j+3]}] --->`) 
+//   cl(smx[i][j] * smx[i][j+1] * smx[i][j+2]* smx[i][j+3]) 
+ 
+//   cl(`INNER i: index- ${i} : [i]- ${smx[i]}`)
+//   cl(`INNER j: index- ${j} : [j]- ${smx[j]}`)
+
+//    }
+  
+//   } 
+
+
+
+
+// RESULTS
+// ----------------------------------
+// i 0 : [i]: 1,2,3,4,5
+// 24
+// 24
+// [ 1, 2, 3, 4 ]
+// 24
+// [ 1, 2, 3, 4 ]
+// j 0 : [j]: 1,2,3,4,5
+// 240
+// [ 1, 20, 3, 4 ]
+// 120
+// [ 2, 3, 4, 5 ]
+// j 1 : [j]: 1,20,3,4,5
+// [ 1, 20, 3, 4 ]
+// NaN
+// [ 3, 4, 5, undefined ]
+// j 2 : [j]: 1,20,3,4,5
+// [ 1, 20, 3, 4 ]
+// NaN
+// [ 4, 5, undefined, undefined ]
+// j 3 : [j]: 1,20,3,4,5
+// i 1 : [i]: 1,20,3,4,5
+// 240
+// NaN
+// NaN
+// ----------------------------------
+// OUTTER i: index- 0 : [i]- 1,2,3,4,5
+// OUTTER j: index- 4 : [j]- 1,4,3,4,5
+// OUTTER[i][j]: [5 ,undefined, undefined, undefined] --->
+// NaN
+// OUTTER[j][i]:[1 ,4, 3, 4] --->
+// 48
+// INNER[j][i]: [1 , 2 , 3, 4] --> 
+// 24
+// INNER[i][j]: [1 , 2 , 3, 4] --->
+// 24
+// INNER i: index- 0 : [i]- 1,2,3,4,5
+// INNER j: index- 0 : [j]- 1,2,3,4,5
+// INNER[j][i]: [1 , 20 , 3, 4] --> 
+// 240
+// INNER[i][j]: [2 , 3 , 4, 5] --->
+// 120
+// INNER i: index- 0 : [i]- 1,2,3,4,5
+// INNER j: index- 1 : [j]- 1,20,3,4,5
+// 240
+// INNER[i][j]: [3 , 4 , 5, undefined] --->
+// NaN
+// INNER i: index- 0 : [i]- 1,2,3,4,5
+// INNER j: index- 2 : [j]- 1,20,3,4,5
+// 240
+// INNER[i][j]: [4 , 5 , undefined, undefined] --->
+// NaN
+// INNER i: index- 0 : [i]- 1,2,3,4,5
+// INNER j: index- 3 : [j]- 1,20,3,4,5
+// OUTTER i: index- 1 : [i]- 1,20,3,4,5
+// OUTTER j: index- 4 : [j]- 1,4,3,4,5
+// OUTTER[i][j]: [5 ,undefined, undefined, undefined] --->
+// NaN
+// OUTTER[j][i]:[4 ,3, 4, 5] --->
+// 240
+// INNER[j][i]: [2 , 3 , 4, 5] --> 
+// 120
+// INNER[i][j]: [1 , 20 , 3, 4] --->
+// 240
+// INNER i: index- 1 : [i]- 1,20,3,4,5
+// INNER j: index- 0 : [j]- 1,2,3,4,5
+// INNER[j][i]: [20 , 3 , 4, 5] --> 
+// 1200
+// INNER[i][j]: [20 , 3 , 4, 5] --->
+// 1200
+// INNER i: index- 1 : [i]- 1,20,3,4,5
+// INNER j: index- 1 : [j]- 1,20,3,4,5
+// 1200
+// INNER[i][j]: [3 , 4 , 5, undefined] --->
+// NaN
+// INNER i: index- 1 : [i]- 1,20,3,4,5
+// INNER j: index- 2 : [j]- 1,20,3,4,5
+// 1200
+// INNER[i][j]: [4 , 5 , undefined, undefined] --->
+// NaN
+// INNER i: index- 1 : [i]- 1,20,3,4,5
+// INNER j: index- 3 : [j]- 1,20,3,4,5
+// OUTTER j: index- 4 : [j]- 1,4,3,4,5
+// OUTTER[i][j]: [5 ,undefined, undefined, undefined] --->
+// NaN
+// OUTTER[j][i]:[3 ,4, 5, undefined] --->
+// NaN
+// INNER[j][i]: [3 , 4 , 5, undefined] --> 
+// NaN
+// INNER[i][j]: [1 , 20 , 3, 4] --->
+// 240
+// INNER i: index- 2 : [i]- 1,20,3,4,5
+// INNER j: index- 0 : [j]- 1,2,3,4,5
+// INNER[j][i]: [3 , 4 , 5, undefined] --> 
+// NaN
+// INNER[i][j]: [20 , 3 , 4, 5] --->
+// 1200
+// INNER i: index- 2 : [i]- 1,20,3,4,5
+// INNER j: index- 1 : [j]- 1,20,3,4,5
+// NaN
+// INNER[i][j]: [3 , 4 , 5, undefined] --->
+// NaN
+// INNER i: index- 2 : [i]- 1,20,3,4,5
+// INNER j: index- 2 : [j]- 1,20,3,4,5
+// NaN
+// INNER[i][j]: [4 , 5 , undefined, undefined] --->
+// NaN
+// INNER i: index- 2 : [i]- 1,20,3,4,5
+// INNER j: index- 3 : [j]- 1,20,3,4,5
+// OUTTER j: index- 4 : [j]- 1,4,3,4,5
+// OUTTER[i][j]: [5 ,undefined, undefined, undefined] --->
+// NaN
+// OUTTER[j][i]:[4 ,5, undefined, undefined] --->
+// NaN
+// INNER[j][i]: [4 , 5 , undefined, undefined] --> 
+// NaN
+// INNER[i][j]: [1 , 20 , 3, 4] --->
+// 240
+// INNER i: index- 3 : [i]- 1,20,3,4,5
+// INNER j: index- 0 : [j]- 1,2,3,4,5
+// INNER[j][i]: [4 , 5 , undefined, undefined] --> 
+// NaN
+// INNER[i][j]: [20 , 3 , 4, 5] --->
+// 1200
+// INNER i: index- 3 : [i]- 1,20,3,4,5
+// INNER j: index- 1 : [j]- 1,20,3,4,5
+// NaN
+// INNER[i][j]: [3 , 4 , 5, undefined] --->
+// NaN
+// INNER i: index- 3 : [i]- 1,20,3,4,5
+// INNER j: index- 2 : [j]- 1,20,3,4,5
+// NaN
+// INNER[i][j]: [4 , 5 , undefined, undefined] --->
+// NaN
+// INNER i: index- 3 : [i]- 1,20,3,4,5
+// INNER j: index- 3 : [j]- 1,20,3,4,5
