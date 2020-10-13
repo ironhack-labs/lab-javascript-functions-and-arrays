7666f5165f3f09070b184791c2241775b75fd437
Margarida <margarida97pt@gmail.com>
2020-10-13 18:49:29 +0100

    not done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,77 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  }
+
+  return numberTwo;
+}
+
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+function findLongestWord (words) {
+  if (words.length === 0) {
+  return null;
+  }
+
+  let longestWord = '';
+    for (i=0; i< words.length; i++) {
+      if (words[i].length > longestWord.length) {
+        longestWord = words[i];
+    }
+  }
+    console.log(longestWord);
+    return longestWord;
+  }
+  findLongestWord(words);
+     
+
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers(numbers) {
+  let sum = 0
+
+  for (let i = 0; i<numbers.length; i++) {
+
+      sum = sum + numbers[i];
+}
+return sum;
+}
+
+
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
+
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers(numbersAvg) {
+
+  if (numbersAvg.length === 0) {
+    return 'The array is empty';
+  }
+}
+  result = average([]);
+  console.log(result);
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(wordsArr) {
+
+  if (wordsArr.length === 1) {
+    return 'The array is empty';
+  }
+  return sum(wordsArr) / wordsArr.length;
+  }
+  
+  result = average([]);
+  console.log(result);
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -28,6 +87,17 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
+function averageWordLength(wordsUnique) {
+
+  if (wordsUnique.length === 0) {
+    return 'The array is empty';
+  }
+  return sum(wordsUnique) / wordsUnique.length;
+  }
+  
+  result = average([]);
+  console.log(result);
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
