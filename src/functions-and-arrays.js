adb11f2add897ed6f4686c3f5c35d3d672750517
James Shusted <jshusted916@gmail.com>
2020-10-14 06:30:59 +0100

    stuck on iteration 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,15 +1,46 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers (numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  } else {
+    return numberTwo;
+  }
+};
+
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord (words) {
+  let longestWord = '';
+  for (let i=0; i<words.length; i++) {
+   if (words[i].length > longestWord.length) {
+    longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
+findLongestWord (words);
 
 // Iteration #3: Calculate the sum
 
+
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumNumbers (numbers) {
+  let sum = 0;
+  for (let i=0; i<numbers.length; i++) {
+    sum = sum + numbers[i]
+  }
+  return sum
+}
+console.log(sumNumbers(numbers))
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers (sumNumbers) {
+  
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
