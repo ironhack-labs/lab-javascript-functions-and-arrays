43027ac16462f499fc4c2694b7e0c2043ff1cf1f
Jesus Rua <jesus.rua.pareja@gmail.com>
2020-10-20 19:55:35 +0200

    Iteration 4 TEST

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -44,6 +44,14 @@ function sumNumbers(someArray) {
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers(someArray) {
+  let finalAverage = 0
+  for (i=0; i<someArray.length; i++) {
+    finalAverage = (finalAverage + someArray[i]) / someArray.length
+  }
+  return finalAverage
+}
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
