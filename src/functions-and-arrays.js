15ae3794ffb58b670ab5aae4bbdb2abfeed26125
migueltomarnogueira <migueltomarnogueira@mac.com>
2020-10-31 10:11:49 +0000

    iterations 1 and 2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,14 +1,44 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 > number2) {
+    return number1;
+  } else if (number2 < number1) {
+    return number2;
+  } else {
+    return number1, number2;
+  }
+}
+
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+function findLongestWord(array) {
+  if (array.length === 0) {
+    return null;
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
+    let longestWord = "";
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].length > longestWord.length) {
+        longestWord = array[i];
+      } 
+      }
+      return longestWord;
+    }
 
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
-// Iteration #4: Calculate the average
-// Level 1: Array of numbers
+  }
+  
+  
+  // Iteration #3: Calculate the sum
+  
+  const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+  
+  
+  // Iteration #4: Calculate the average
+  // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 // Level 2: Array of strings
