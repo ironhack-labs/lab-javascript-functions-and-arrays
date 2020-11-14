1b96f6342f7d830c47037d28dcbaf4bfc8227ac4
Manuel Carrillo <manucaralmo@gmail.com>
2020-11-14 13:01:11 +0100

    iterartion 1 and 2 completed and tested

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,9 @@
 // Iteration #1: Find the maximum
 
+let maxOfTwoNumbers = function (num1, num2) {
+  
+}
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
