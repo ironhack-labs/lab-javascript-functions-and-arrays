a8b60e390cc9bb12dd3b9ac7a74c2937ca2b086b
cisnerosjj <jorgecisnerosgavin@gmail.com>
2020-11-15 20:06:20 +0100

    Hasta el 4.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -36,26 +36,43 @@ let word= "";
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(num){
-  if (num.length === 0){
-    return 0
-  } else if (num.length === 1){
-  return num[0]
-  }else{
+  let sum = 0;
+
     for(let i = 0; i < num.length; i++){
-      if ( num [i] === 0){
-        return 0
-      }else {
-        return [i]
-      }
-    }
+      sum += num[i]
+    
   }
+  
+  return sum
+}
+
+// Bono 3.1: Una sum()función genérica
+
+function sum(){
+  return 0;   
+
+}
+
+// Iteration #4: Calculate the average
+
+function averageNumbers(numbers){
+    
+  if(numbers.length === 0){
+    return null;
+  }
+
+  let total = 0;
+
+  for(let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
 }
 
 
+  return total / numbers.length;
 
-// Iteration #4: Calculate the average
-// Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+}
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
