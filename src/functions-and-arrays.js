2fefe95a3a636f14a93f1ee63f5a76fda4447072
MP <princi8@gmail.comm>
2021-01-12 18:29:54 +0100

    Update functions-and-arrays.js
    Not finished yet.  Work in progress

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,68 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(a,b) {
+
+  if (a>b) {return a;}
+  if (b>a) {return b;}
+  if (b===a) {return b;}
+};
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(words){
+
+  if (words.length==1) {
+    return words[0];
+  }
+
+  let longestWord = "";
+  if ( words.length == 0) {
+    longestWord = null;
+    return longestWord;
+  }
+
+    for (i = 0; i < words.length; i++) {
+      if (words[i].length > longestWord.length) { 
+        longestWord = words[i];  
+      }
+    }
+    console.log (longestWord);
+  }
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+
+
 // Iteration #4: Calculate the average
+
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers (numbers1) {
+  let sumNumber = 0;  
+  for (i = 0; i < numbers1.length; i++) {
+    sumNumber = sumNumber + numbers1[i];
+  }
+ return sumNumber/(numbers1.length);
+}
+
+
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+
+function averageWordLength (arr1) {
+  console.log(arr1.length);
+  let totWordLength=0;
+  for (i=0; i < arr1.length; i++) {
+    totWordLength=totWordLength+arr1[i].length;   
+  }
+  totWordLength = totWordLength / arr1.length;
+   console.log(totWordLength);
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
