efc01e87cd13cbaba83463efab7e149deb15d1a0
Daniela Alonzo <monsterjunior@outlook.es>
2021-01-12 20:20:16 -0600

    Reaizamos ejercicio nombre de funciones

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,55 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+function findLongestWord(array) {
+  let longestWord = ''
+  if (!array.length) {
+    return null }
+    array.forEach(word => {
+      if (word.length > longestWord.length) {
+        longestWord = word
+      }
+    })
+    return longestWord
+}
 
+// Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumNumbers(array) {
+  if (array = []) {
+    return 0
+  } let sumAllNumbers = array.forEach(function(numbers) {
+    return sumAllNumbers
+  })
+}
+
+function sum() {
+  
+}
+
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageNumbers() {
+
+}
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength() {
+
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -29,8 +66,16 @@ const wordsUnique = [
   'bring'
 ];
 
+function avg() {
+
+}
+
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+function uniquifyArray() {
+  
+}
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -47,7 +92,19 @@ const wordsCount = [
   'matter'
 ];
 
+function doesWordExist() {
+
+}
+
 // Iteration #8: Bonus
+
+function howManyTimes() {
+
+}
+
+function greatestProduct() {
+  
+}
 
 const matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
