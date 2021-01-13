2b9c7f339a7305d7470e2a4139e4eb9eb64d5eff
Michael Raman <raman_michael@hotmail.com>
2021-01-13 10:25:15 +0100

    finished 8.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -226,3 +226,20 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+function greatestProduct(matrix) {
+  const arrOfProducts = [];
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+        if (i-2>0) {
+          arrOfProducts.push(matrix[i][j]*matrix[i-1][j]*matrix[i-2][j]*matrix[i-3][j]);
+        }
+        if (j-2>0) {
+          arrOfProducts.push(matrix[i][j]*matrix[i][j-1]*matrix[i][j-2]*matrix[i][j-3]);
+        }
+      }
+    }
+  let highestNum = Math.max.apply(null, arrOfProducts);
+  return highestNum;
+}
+
