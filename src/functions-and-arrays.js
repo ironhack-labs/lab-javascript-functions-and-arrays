ef6914208fc11e3e98f937baad78d399cc1708cc
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-01-19 18:34:00 -0300

    add bonuses 8 and 8.1. lab completed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,4 +1,5 @@
 // Iteration #1: Find the maximum
+
 function maxOfTwoNumbers (num1, num2) {
   if (num1 >= num2) {
     return num1;
@@ -9,10 +10,7 @@ function maxOfTwoNumbers (num1, num2) {
 
 //console.log(maxOfTwoNumbers(8, 8));
 
-// // Iteration #2: Find longest word
-// Create a function named `findLongestWord` that takes as an argument an array of words and returns the longest one. If there are 2 with the same length, it should return the first occurrence.
-
-// You can use the following array to test your solution:
+// Iteration #2: Find longest word
 
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
@@ -38,9 +36,6 @@ for (let i = 0; i < arrayOfwords.length; i++) {
 
 
 // Iteration #3: Calculate the sum
-// Calculating a sum can be as simple as iterating over an array and adding each of the elements together.
-
-// Declare a function named sumNumbers that takes an array of numbers as an argument, and returns the sum of all of the numbers in the array. Later in the course we will learn how to do this by using the reduce array method, which will make your work significantly easier. For now, let's practice "declarative" way adding values, using loops.
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
@@ -57,10 +52,6 @@ function sumNumbers(arrayOfNumbers) {
 // console.log(sumNumbers(numbers))
 
 // #### Bonus - Iteration #3.1: A generic `sum()` function
-
-// Let's create a new function `sum()` that calculates the sum for array filled with (_almost_) any type of data. Note that strings should have their length added to the total, and boolean values should be coerced into their corresponding numeric values. Check the tests for more details.
-
-// You can use the following array to test your solution:
 
 const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10]; // should return: 57
 
@@ -86,16 +77,7 @@ function sum(anyArray) {
 
 // console.log(sum(mixedArr))
 
-// // Iteration #4: Calculate the average
-// // Level 1: Array of numbers
-
-// // Calculating an average is an extremely common task. Let's practice it a bit.
-
-// The logic behind this:
-
-// Find the sum as we did in the first exercise (or how about reusing that the sumNumbers()?)
-// Take that sum and divide it by the number of elements in the list.
-
+// Iteration #4: Calculate the average
 
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
@@ -113,10 +95,8 @@ function averageNumbers(arrayOfNumbers) {
 
 // console.log(averageNumbers(numbersAvg))
 
-// // Level 2: Array of strings
-// Declare a function named averageWordLength that receives as a single argument an array of words and returns the average length of the words:
+  // Level 2: Array of strings
 
-// You can use the following array to test your solution:
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
 function averageWordLength (arrayOfwords) {
@@ -141,9 +121,7 @@ function averageWordLength (arrayOfwords) {
 
 // console.log(averageWordLength(['wordsArr', 2, 'ow']))
 
-// #### Bonus - Iteration #4.1: A generic `avg()` function
-
-// Create function `avg(arr)` that receives any mixed array and calculates average. Consider as mixed array an array filled with numbers and/or strings and/or booleans. We are following a similar logic to the one applied on the bonus iteration 4.1. :wink:
+  // #### Bonus - Iteration #4.1: A generic `avg()` function
 
 function avg(arr) {
   if (arr.length < 1) {
@@ -160,11 +138,7 @@ function avg(arr) {
 // console.log(avg(mixedArr))
 
 
-// // Iteration #5: Unique arrays
-// Take the following array, remove the duplicates, and return a new array. You are more than likely going to want to check out the indexOf Array method.
-
-// Do this in the form of a function uniquifyArray that receives an array of words as a argument.
-
+// Iteration #5: Unique arrays
 
 const wordsUnique = [
   'crab',
@@ -199,9 +173,6 @@ function uniquifyArray(arrayOfwords) {
 // console.log(uniquifyArray(wordsUnique))
 
 // Iteration #6: Find elements
-// Let's create a simple array search.
-
-// Declare a function named doesWordExist that will take in an array of words as one argument, and a word to search for as the other. Return true if it exists, otherwise, return false. Don't use indexOf for this one.
 
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
@@ -222,7 +193,6 @@ function doesWordExist(arrayOfwords, word) {
 // console.log(doesWordExist(wordsFind, 'asdf'))
 
 // Iteration #7: Count repetition
-// Declare a function named howManyTimes that will take in an array of words as the first argument, and a word to search for as the second argument. The function will return the number of times that word appears in the array.
 
 const wordsCount = [
   'machine',
@@ -252,27 +222,83 @@ function howManyTimes(arrayOfwords, word) {
 
 // console.log(howManyTimes(wordsCount, 'maer'))
 
-// // Iteration #8: Bonus
+// Iteration #8: Bonus
 
-// const matrix = [
-//   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-//   [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-//   [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-//   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-//   [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-//   [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-//   [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-//   [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-//   [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-//   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-//   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-//   [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-//   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-//   [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-//   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-//   [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-//   [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-//   [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-//   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-//   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-// ];
+const shortMatrix = [
+  [ 1,  2, 3, 4, 5],
+  [ 1, 20, 3, 4, 5],
+  [ 1, 20, 3, 4, 5],
+  [ 1, 20, 3, 4, 5],
+  [ 1,  4, 3, 4, 5]
+]
+
+const matrix = [
+  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+];
+
+function greatestProduct(matrix) {
+  let greatestHorizontalProduct = 0
+  let greatestVerticalProduct = 0
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i]. length; j++) {           
+      if (matrix[i][j+3]) {
+        let horizontalProduct = matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3];
+        if (horizontalProduct > greatestHorizontalProduct) {
+          greatestHorizontalProduct = horizontalProduct;
+        }
+      }
+      
+      if (matrix[i+3]) {
+         let verticalProduct = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j];
+         if (verticalProduct > greatestVerticalProduct) {
+           greatestVerticalProduct = verticalProduct;
+         }
+      }
+      
+      }     
+    }
+  
+    return greatestHorizontalProduct >= greatestVerticalProduct ? greatestHorizontalProduct : greatestVerticalProduct
+  }
+
+// console.log(greatestProduct(shortMatrix))
+
+  //Iteration #8.1: Bonus
+
+function greatestProductOfDiagonals(matrix) {
+  let greatestDiagonalProduct = 0;
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j+3] && matrix[i+3]) {
+        let product = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3];
+        
+        product > greatestDiagonalProduct ? greatestDiagonalProduct = product : greatestDiagonalProduct
+      }
+    }
+  }
+
+  return greatestDiagonalProduct;
+}
+
+console.log(greatestProductOfDiagonals(shortMatrix))
