9539419acf35969f445847017792a7db2f9f25e0
Magali <fort_magali@hotmail.fr>
2021-02-04 21:25:23 +0100

    add iteration 7

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -119,7 +119,14 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
-
+function howManyTimes(array, word) {
+  let count = 0;
+  for (let i=0; i<array.length; i++) {
+    if (array[i]===word) {
+      count+=1;
+    }
+  } return count;
+}
 // Iteration #8: Bonus
 
 const matrix = [
