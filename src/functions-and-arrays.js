565a08d912557499be35b5ffa4a61180c928374d
hisamparker <hisamparker@gmail.com>
2021-03-15 09:52:14 +0100

    Iteration 8 done but v ugly :/

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -162,3 +162,23 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+const greatestProduct = (arr) => {
+  let maxArr;
+  let total = 0;
+  for (innerArr of arr) {
+      let sortedArr = innerArr.sort();
+      let slicedArr = sortedArr.slice(innerArr.length-4)
+      let arrTotals = [];
+      arrTotals.push(slicedArr.reduce((total, num) => total * num));
+      
+      
+      for (let i = 0; i < arr.length; i++) {
+          if (total < Number(arrTotals)) {
+              total = (Number(arrTotals));
+          }
+          maxArr = total;
+      }
+  }  
+  return maxArr; 
+}
