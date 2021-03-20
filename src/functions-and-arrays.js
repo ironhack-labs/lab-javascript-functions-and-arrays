3854e49d5d8956e42181b28570baee492b709a81
Mohamud Issa <mohamud.issa@ymail.com>
2021-03-20 23:49:44 +0100

    completed matrix questions part 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -196,3 +196,29 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+
+maxProduct = 0;
+prod =0;
+
+function greatestProduct(array){
+
+  array.forEach(element => {
+
+    for(i=0; i < (element.length - 4); i++){
+
+      prod = element[i]*element[i+1]*element[i+2]*element[i+3]
+
+      if(prod > maxProduct){
+
+        maxProduct = prod
+
+      }
+    }
+  } 
+  )
+  console.log(maxProduct);
+  console.log(index);
+};
+
+greatestProduct(matrix);
