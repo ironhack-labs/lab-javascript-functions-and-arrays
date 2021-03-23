08364586ccde0d5eabf61566f1788903048b5df1
Bea Avis <bea.avis.p@gmail.com>
2021-03-23 21:02:34 +0100

    Add Iteration #3 and #3.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -34,3 +34,45 @@ let findLongestWord = (arr) => {
 }
 
 console.log(findLongestWord(words));
+
+// Iteration #3: Calculate the sum
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+let sumNumbers = (array) => {
+  let sum = 0;
+  array.forEach((el) => sum += el);
+  return sum;
+}
+console.log(sumNumbers(numbers));
+
+// Iteration #3.1: A generic sum() function
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+let sum = (array) => {
+  let totalSum = 0;
+  array.forEach((el) => {
+    switch (typeof el){
+      case "number":
+        totalSum += el;
+        break;
+      
+      case "string":
+        totalSum += el.length;
+        break;
+      
+      case "boolean":
+        if (el === true){
+          totalSum ++;
+          break;
+        } else {
+          break;
+        }  
+      
+      default:
+        throw new Error("ERROR. TYPE OF DATA NOT ALLOWED.");
+    }
+  });
+  return totalSum;
+}
+
+console.log(sum(mixedArr));
+
