218e9470a0a2049a27581a026ebae1871f4671dd
Bea Avis <bea.avis.p@gmail.com>
2021-03-23 21:01:48 +0100

    Add Iteration #2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -9,3 +9,28 @@ function maxOfTwoNumbers(num1, num2) {
   }
 }
 console.log(maxOfTwoNumbers(11,10));
+
+// Iteration #2: Find longest word
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+let findLongestWord = (arr) => {
+  let result;
+  if (arr.length > 0){
+    let largest = "";
+    words.forEach((el) => {
+      if(largest.length < el.length){
+        largest = el;
+      } 
+    });
+    return largest;
+  } else if (arr.length = 1){
+
+    return arr[0];
+
+  } else if (arr.length === 0){
+     result = null;
+  }
+  return result;
+}
+
+console.log(findLongestWord(words));
