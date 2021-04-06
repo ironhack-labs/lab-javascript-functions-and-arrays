d22a15ec441305714b5649ee3f904a7958aac5d7
amelol <75670435+amelol@users.noreply.github.com>
2021-04-06 20:41:30 +0200

    Despues de hablar con Carlos decidi dejar la ultima iteracion para luego, la he borrado en este commit para que no fallen todos los tests

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -174,14 +174,3 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
-
-
-const equalToOne = (currentValue) => currentValue = 1;
-
-const equalToTwo = (currentValue) => currentValue = 2;
-
-function greatestProduct(matrix){
-   if (matrix.every(equalToOne)){return 1}
-   else if(matrix.every(equalToTwo)){return 16}
-   else{return "error"}
-}
