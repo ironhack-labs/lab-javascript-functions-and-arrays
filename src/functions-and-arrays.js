cea1c467a35b4ec0b433d37b9fb7c91d17909ff7
Ele089 <ecore089@gmail.com>
2021-04-17 13:51:21 +0200

    iterations 1 2 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,16 +1,53 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers (num1, num2) {
+  if (num1 > num2) { 
+  return num1;
+} 
+else {
+  return num2;
+}
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(arrWords){
+  longWord = '';
+
+  if (arrWords.length === 0) {
+    return null
+  } else if (arrWords.length === 1) {
+    return arrWords[0]
+  }
+    for (let i=0; i < arrWords.length; i++) {
+      if (arrWords[i].length > longWord.length){ 
+      longWord = arrWords[i];
+        
+      } else if (arrWords[i].length === longWord.length) {
+        continue;
+      }
+   }
+    return longWord;
+  } 
 
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+function sumNumbers (arrNum){
+  let sum = 0;
+  for (let i = 0; i< arrNum.length; i++) {
+    sum = arrNum[i] + sum;
+  }
+  return sum;
+}
+
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
