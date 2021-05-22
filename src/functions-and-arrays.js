b6fa56d85d922a9887fccca1fc008aa87923117e
Julia Häußler <julia@haeuszler.de>
2021-05-22 15:21:42 +0200

    task 1 to 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,16 +1,54 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers (num1, num2) {
+  if (num1 > num2) {
+    return(num1)
+  } else if (num1 < num2) {
+    return(num2)
+  } else {
+    return(num1)
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-// Iteration #3: Calculate the sum
+function findLongestWord (words) {
+  if (words.length == 0) {
+    return null
+  }
+  let longestWord = ''
+  for (let i = 0; i < words.length; i++) {
+    const element = words[i];
+    if (element.length > longestWord.length) {
+      longestWord = element
+    }
+  }
+  return longestWord
+} 
 
+// Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumNumbers (numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  };
+  return sum
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers (numbersAvg) {
+  if (numbersAvg.length == 0) {
+    return null
+  }
+  let sum = sumNumbers (numbersAvg)
+  return sum / numbersAvg.length
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
