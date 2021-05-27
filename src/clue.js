c48679c456967ba85c445d191cc22567d6a81f0d
ross-u <cirkovic.uros.srb@gmail.com>
2021-05-27 23:56:34 +0200

    Update comments

--- src/clue.js
+++ src/clue.js
@@ -1,16 +1,17 @@
 // ITERATION 1
 
-// Suspects Collection
+// Suspects Array
 
 const suspectsArray = [];
 
-// Rooms Collection
+// Rooms Array
 
 const roomsArray = [];
 
-// Weapons Collection
+// Weapons Array
 
 const weaponsArray = [];
+
 
 // ITERATION 2
 
@@ -18,11 +19,15 @@ function selectRandom() {}
 
 function pickMystery() {}
 
+
 // ITERATION 3
 
 function revealMystery() {}
 
-// The following is required to make unit tests work. Please ignore it.
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = {
     suspectsArray,
