86581c790508aed66b224c6b5766c02cd77a3ded
Halil Ibrahim Kaya <halilibrahim280@gmail.com>
2021-06-01 17:02:08 +0200

    first iteration done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return num1, num2;
+  }
+}
 
 
 
