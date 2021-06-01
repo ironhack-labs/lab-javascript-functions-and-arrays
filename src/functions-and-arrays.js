4498277d7df377a6891a0bcaa259c5843af25ba0
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-06-01 23:40:03 +0200

    Lab of day 2 DONE!

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -218,8 +218,54 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+// function greatestProduct(matrix) {
+//   let n = matrix.length;
+//   let m = matrix[0].length;
+//   let maxP = 0;
+//   for (let i = 0; i < n; i++){
+//     for (let j = 0; j < m; j++){
+//       if (j+3 < m){
+//         let product = matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3];
+//         if (product > maxP){
+//           maxP = product;
+//         }
+//       }
+//       if (i+3 < n){
+//         let product = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j];
+//         if (product > maxP){
+//           maxP = product;
+//         }
+//       }
+      
+//     }
+//   }
+//   return maxP;
+// }
 
+
+function greatestProduct(matrix) {
+  let n = matrix.length;
+  let m = matrix[0].length;
+  let maxP = 0;
+  for (let i = 0; i < n; i++){
+    for (let j = 0; j < m; j++){
+      if (j+3 < m && i+3 < n){
+        let product = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3];
+        if (product > maxP){
+          maxP = product;
+        }
+      }
+      if (i+3 < n && j-3 >= 0){
+        let product = matrix[i][j] * matrix[i+1][j-1] * matrix[i+2][j-2] * matrix[i+3][j-3];
+        if (product > maxP){
+          maxP = product;
+        }
+      }
+      
+    }
+  }
+  return maxP;
+}
 
 
 
