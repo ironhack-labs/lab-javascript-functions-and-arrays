ad179b48af325a0aabfc248760ef40d53cbf0853
Chris Toth <christaylor269@gmail.com>
2021-07-10 19:51:52 +0200

    iteration 1&2 complete (I hope)

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,51 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+
+
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 >= num2){
+    return num1;
+  }
+  else if(num2 >= num1){
+    return num2;
+  }
+  else {
+    return num1,num2;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+
+
+function findLongestWord(words) {
+
+  let longestWord = "";
+  
+  if(!words.length) {
+    return null;
+  }
+
+  if(words.length === 1) {
+    return words[0];
+  }
+
+  for (i = 0; i < words.length; i++) {
+    if (longestWord.length < words[i].length){
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+
+}
+
+
+
+
+
+
 
 
 
