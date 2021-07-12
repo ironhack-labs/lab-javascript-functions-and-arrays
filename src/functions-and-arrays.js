0ce068581282aa4ea1d2a39b1bcf313aa38e086a
Daniel td <danieltd@gmail.com>
2021-07-12 21:34:26 +0200

    first draft, need to update some functions

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,41 +1,103 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return num1;
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if (!arr.length) {
+    return null;
+  }
 
-
+  let longest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length > longest.length) {
+      longest = arr[i];
+    }
+  }
+  return longest;
+}
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  if (!arr.length) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
 
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(arr) {
+  if (!arr.length) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
 
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(arr) {
+  if (!arr.length) {
+    return null;
+  }
+  let total = 0;
 
-function averageNumbers() {}
-
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  let average = total / arr.length;
+  return average;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  if (!arr.length) {
+    return null;
+  } else if (arr.length === 1) {
+    return arr[0].length;
+  }
+
+  let str = 0;
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i];
+  }
+  let average = parseInt(str.length / arr.length);
+  return average;
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(arr) {
+  if (!arr.length) {
+    return null;
+  }
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,16 +114,36 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(arr) {
+  if (!arr.length) {
+    return null;
+  }
 
+  let result = [];
 
+  for (i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arr) {
+  if (!arr.length) {
+    return null;
+  }
+let result = [];
 
-
+  for (i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      return true;
+} else if (result.indexOf(arr[i])  != -1 ) {
+  return false;
+}}}
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -78,7 +160,20 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+
+  let result = [];
+
+  for (i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) == -1) {
+      return 1;
+} else if (result.indexOf(arr[i])  != -1 ) {
+  return 0;
+}}
+}
 
 
 
@@ -106,10 +201,13 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
-
-
-
+function greatestProduct(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      return one;
+    }
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
