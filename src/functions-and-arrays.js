cd6222140bfcc6adc5c35149a0a01a82bc19fb2c
Ana Martins <ana.af.martins@gmail.com>
2021-08-10 22:15:47 +0200

    All iterationns but bonus #8

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,41 +1,105 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 > num2) {
+    return num1
+  } else {
+    return num2
+  }
+}
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
+function findLongestWord(anArray) {
+  let longestWord = ""
+  if (!anArray?.length) {
+    return null;
+      }
+  for (let i = 0; i < anArray.length; i++) {
+     if (longestWord.length < anArray[i].length) {
+       longestWord = anArray[i]
+     } 
+ }
+    return longestWord;
+}
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(anArray) {
+  let sum = 0
+    for(let i = 0; i < anArray.length; i++){
+    sum += anArray[i]
+  }
+  return sum
+}
 
 // Iteration #3.1 Bonus:
-function sum() {}
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
-
+function sum(anArray) {
+  let sum = 0
+  for(let i = 0; i < anArray.length; i++){
+    if (typeof anArray[i] === "string") {
+    sum += anArray[i].length
+    } else if (typeof anArray[i] === "object"){
+      throw "Unsupported data type sir or ma'am"
+      } else {
+        sum += anArray[i]
+      }
+  }
+  return sum
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(anArray) {
+  if (!anArray?.length) {
+    return null;
+      }
+  let sum = sumNumbers(anArray)
+  let average = sum / anArray.length
+  return average
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(anArray) { 
+ let totalLength = 0
+  let averageLength = 0
+  if (!anArray?.length) {
+    return null;
+      }
+  for (let i = 0; i < anArray.length; i++) {
+    totalLength += anArray[i].length;
+  }
+   averageLength = totalLength / anArray.length
+  return averageLength
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(anArray) {
+  let totalLength = 0
+  let averageLength = 0
+  if (!anArray?.length) {
+    return null;
+      }
+for(let i = 0; i < anArray.length; i++){
+    if (typeof anArray[i] === "string") {
+    totalLength += anArray[i].length
+    } else {
+        totalLength += anArray[i]
+      }
+  }
+  averageLength = totalLength / anArray.length
+  return +averageLength.toFixed(2)
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,16 +116,38 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
-
-
+function uniquifyArray(anArray) {
+  let uniqueArray = [];
+  if (!anArray?.length) {
+    return null;
+      }
+for(let i = 0; i < anArray.length; i++) {
+  if (uniqueArray.indexOf(anArray[i]) === -1) {
+    uniqueArray.push(anArray[i]);
+  }
+}
+return uniqueArray;
+}
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(anArray, aWord) {
+  if (!anArray?.length) {
+       return null;
+      }
+  if (!anArray) {
+    return null
+  }
+  for (let i = 0; i < anArray.length; i++) {
+    if (anArray[i] === aWord) {
+      return true
+    }
+  }
+  return false   
+}
 
-
+// A different, simpler solution would be to use anArray.includes(aWord)
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -78,7 +164,18 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(anArray, aWord) {
+  if (!anArray?.length) {
+       return 0;
+      }
+  let repetitions = 0
+  for (let i = 0; i < anArray.length; i++) {
+    if (anArray[i] === aWord) {
+      repetitions += 1
+    }
+  }
+  return repetitions
+}
 
 
 
