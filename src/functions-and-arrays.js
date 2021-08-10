3e05a7e981a7188d006a33c6cda252696d341cd6
lauraAlabau <arlaura3@gmail.com>
2021-08-10 14:39:23 +0200

    Find maximum

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2){
+    console.log(num1);
+  }else if  (num1 < num2){
+    console.log(num2);
+  }else{
+    console.log(num1);
+  }
+}
 
 
 
