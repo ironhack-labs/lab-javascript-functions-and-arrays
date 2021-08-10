2b73dbf273e40bff49f132817d7287836919463c
lauraAlabau <arlaura3@gmail.com>
2021-08-10 14:42:19 +0200

    find max con return

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,11 +1,11 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(num1, num2) {
   if (num1 > num2){
-    console.log(num1);
+    return num1;
   }else if  (num1 < num2){
-    console.log(num2);
+    return num2;
   }else{
-    console.log(num1);
+    return num1;
   }
 }
 
