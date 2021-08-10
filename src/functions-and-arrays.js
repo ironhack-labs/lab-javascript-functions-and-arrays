cde15d3ddc53d410d57f8ca783944ddf23ae7ffa
Sherab Pereira <sherabpereira@gmail.com>
2021-08-10 18:43:53 +0200

    Cambios menores

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,6 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(num1, num2) {
-  return num1 >= num2 ? num1 : num2;
+  return Math.max(num1,num2)
 }
 
 // Iteration #2: Find longest word
