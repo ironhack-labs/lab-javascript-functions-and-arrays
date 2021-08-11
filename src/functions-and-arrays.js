dda30c4700c2a9fafeea93234426656324b7a87a
Alvin Sewram <alvinsewram@Alvins-MacBook-Pro.local>
2021-08-11 09:11:24 -0400

    first commit

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,43 +1,130 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
 
+function maxOfTwoNumbers(num1, num2) {
+  switch (true) {
+    case num1 > num2:
+      return num1;
 
+    case num1 < num2:
+      return num2;
 
+    case num1 === num2:
+      return 'they are the same';
+
+    default:
+      return 'invalid entry';
+  }
+}
+console.log(maxOfTwoNumbers(4, 4));
+//=============================================================================================
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot', '123456789'];
 
+function findLongestWord(arrayOfWords) {
+  let currentLongestWord = '';
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    const word = arrayOfWords[i];
 
-
+    if (word.length > currentLongestWord.length) {
+      currentLongestWord = word;
+    }
+  }
+  return currentLongestWord;
+}
+console.log(findLongestWord(words));
+//=============================================================================================
 // Iteration #3: Calculate the sum
+
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrayOfNums) {
+  let currentSum = 0;
 
+  for (let i = 0; i < arrayOfNums.length; i++) {
+    currentSum = currentSum + arrayOfNums[i];
+  }
 
+  return currentSum;
+}
+console.log(sumNumbers(numbers));
 
+//=============================================================================================
 // Iteration #3.1 Bonus:
-function sum() {}
 
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
+function sum(array) {
+  currentSum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    const convertedValue = convert(value);
+    currentSum = currentSum + convertedValue;
+  }
+  return currentSum;
+}
+const convert = (value) => {
+  switch (typeof value) {
+    case 'string':
+      return value.length;
+    case 'number':
+      return value;
+    case 'boolean':
+      if (value === true) {
+        return 1;
+      } else {
+        return 0;
+      }
+    default:
+      return 'error';
+  }
+};
 
+console.log(sum(mixedArr));
+
+//=============================================================================================
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
+
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrayOfNums) {
+  let currentSum = 0;
 
+  for (let i = 0; i < arrayOfNums.length; i++) {
+    currentSum = currentSum + arrayOfNums[i];
+  }
 
+  return currentSum / arrayOfNums.length;
+}
+console.log(averageNumbers(numbersAvg));
+//=============================================================================================
 // Level 2: Array of strings
+
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arrayOfWords) {
+  let currentSum = 0;
 
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    currentSum = currentSum + arrayOfWords[i].length;
+  }
+
+  return currentSum / arrayOfWords.length;
+}
+console.log(averageWordLength(wordsArr));
+//=============================================================================================
 // Bonus - Iteration #4.1
-function avg() {}
 
+function avg(array) {
+  return sum(array) / array.length;
+}
+
+console.log(avg(mixedArr));
+
+//=============================================================================================
 // Iteration #5: Unique arrays
+
 const wordsUnique = [
   'crab',
   'poison',
@@ -52,18 +139,38 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(array) {
+  const newArray = [];
 
+  for (let i = 0; i < array.length; i++) {
+    const currentWord = array[i];
+    const firstIndexOfWord = array.indexOf(currentWord);
 
-
+    if (i === firstIndexOfWord) {
+      newArray.push(currentWord);
+    }
+  }
+  return newArray;
+}
+console.log(uniquifyArray(wordsUnique));
+//=============================================================================================
 // Iteration #6: Find elements
+
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
-
-
-
+function doesWordExist(words, wordToSearch) {
+  for (let i = 0; i < words.length; i++) {
+    const currentWord = words[i];
+    if (wordToSearch === currentWord) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(doesWordExist(wordsFind, 'machine'));
+//=============================================================================================
 // Iteration #7: Count repetition
+
 const wordsCount = [
   'machine',
   'matter',
@@ -78,38 +185,81 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(words, wordToCount) {
+  let occurences = 0;
 
-
-
+  for (let i = 0; i < words.length; i++) {
+    const currentWord = words[i];
+    if (currentWord === wordToCount) {
+      occurences++;
+    }
+  }
+  return occurences;
+}
+console.log(howManyTimes(wordsCount, 'matter'));
+//=============================================================================================
 // Iteration #8: Bonus
+
+// const matrix = [
+//   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
+//   [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
+//   [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
+//   [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
+//   [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+//   [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+//   [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+//   [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
+//   [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+//   [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
+//   [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
+//   [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
+//   [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+//   [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
+//   [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+//   [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+//   [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
+//   [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
+//   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
+//   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+// ];
+
 const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+  [1, 2, 3, 4, 5],
+  [1, 20, 3, 4, 5],
+  [1, 20, 3, 4, 5],
+  [1, 20, 3, 4, 5],
+  [1, 4, 3, 4, 5]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let currentGreatestProduct = 0;
+  for (let x = 0; x < matrix.length; x++) {
+    let currentXAxisProduct = 0;
+    let currentYAxisProduct = 0;
+    for (let y = 0; y < matrix[x].length; y++) {
+      if (matrix[x + 3][y]) {
+        currentXAxisProduct = matrix[x][y] * matrix[x + 1][y] * matrix[x + 2][y] * matrix[x + 3][y];
+      }
+      if (matrix[x][y + 3]) {
+        currentYAxisProduct = matrix[x][y] * matrix[x][y + 1] * matrix[x][y + 2] * matrix[x][y + 3];
+      }
+      if (currentXAxisProduct > currentGreatestProduct) {
+        currentGreatestProduct = currentXAxisProduct;
+      }
+      if (currentYAxisProduct > currentGreatestProduct) {
+        currentGreatestProduct = currentYAxisProduct;
+      }
+      console.log(x + ',' + y);
+      console.log(currentXAxisProduct);
+      console.log(currentYAxisProduct);
+    }
+  }
+  return currentGreatestProduct;
+}
 
+console.log(greatestProduct(matrix));
 
-
+//=============================================================================================
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
