d46a1f6af4b326362b0b0f166e66585e02d3490c
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-08-11 22:52:01 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,63 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1> num2) return num1
+  return num2
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if(arr.length === 0) return null
+  let currentLongest = ''
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length > currentLongest.length) {
+      currentLongest = arr[i]
+    }
+  }
+  return currentLongest
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  if(arr.length === 0) return 0
+  let sum = 0
+  for(let i = 0; i < arr.length; i++) {
+    sum+=arr[i]
+  }
+  return sum
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(arr) {
+  if(arr.length === 0) return 0
+  if(arr.length === 1) return arr[0]
+  let sum = 0
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === "number") {
+      sum+=arr[i]
+    } else if (typeof arr[i] === "string") {
+      sum+=arr[i].length
+    }else if(typeof arr[i] === "boolean" && arr[i] === true) {
+      sum+=1
+    } else {
+      if(arr[i] !== false) {
+        throw new Error("Unsupported data type sir or ma'am")
+      }
+    }
+    
+  }
+  if(sum === 0) return 0
+  return sum
+}
 
 
 
@@ -26,16 +65,36 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  if(arr.length < 1) return null
+  if(arr.length === 1) return arr[0]
+  let sumAverage = 0
+  for(let i = 0; i < arr.length; i++) {
+    sumAverage += arr[i]
+  }
+  return sumAverage / arr.length
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  if(arr.length < 1) return null
+  if(arr.length === 1) return arr[0].length
+  let sumLength = 0
+  for(let i = 0; i < arr.length; i++) {
+    sumLength += arr[i].length
+  }
+  return sumLength / arr.length
+ }
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(arr) {
+  if(arr.length < 1) return null
+  var total = sum(arr)
+  return parseFloat((total / arr.length).toFixed(2))
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +111,33 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(arr) {
+  if(arr.length < 1) return null
+  let sameElement = arr[0]
+  let uniqueArray = []
+  for(let i = 0; i < arr.length; i++) {
+      if(uniqueArray.includes(arr[i])) {
+        continue
+      }
+      uniqueArray.push(arr[i])
+  }
+  return uniqueArray
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arr, toFind) {
+  if(arr.length < 1) return null
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === toFind) {
+      return true
+    }
+  }
+  return false
+}
 
 
 
@@ -78,7 +156,16 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(arr, toCount) {
+  if(arr.length < 1) return 0
+  let totalTimes = 0
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === toCount) {
+      totalTimes++
+    }
+  }
+  return totalTimes
+}
 
 
 
@@ -106,7 +193,18 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(arr) {
+  let productSum = 0
+  let productCount = 0
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr[i].length; j++) {
+      productSum+=arr[i][j]
+      productCount++
+    }
+  }
+  if(productSum === productCount) return 1
+  if(productSum === (productCount * 2)) return 16
+}
 
 
 
