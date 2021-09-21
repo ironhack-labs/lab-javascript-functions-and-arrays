67999d5a335bc4bf481986593e5f7ef7fc14abaf
Dominika Lipka <dominika.lipka96@gmail.com>
2021-09-21 18:41:36 +0200

    [FT-RMT-092021] DominikaLipka

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,30 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    return num1
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  let longestWord = ''
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  if (words.length === 0) {
+    return null;
+  } return longestWord
+}
 
 
 
