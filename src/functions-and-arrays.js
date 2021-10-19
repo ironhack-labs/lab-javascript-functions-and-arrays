319647a0b5ccb45921687f28427f3e2dcc2fb983
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-19 21:32:44 +0200

    Iteración 1 resuelta

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  if(number1 > number2){
+    return number1
+  }else if(number2 > number1){
+    return number2
+  }else {
+    return number1
+  }
+}
 
 
 
