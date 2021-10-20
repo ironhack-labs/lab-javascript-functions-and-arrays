b0420837b1a1a4f75b357a35fe2d6858f47e8eb1
Fede <f.gomezlara@outlook.es>
2021-10-20 09:06:42 +0200

    iteration1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 > num2){
+    return num1
+  }else if(num1 === num2){
+    return num1
+  }else{
+    return num2
+  }
+} 
 
 
 
