07765db6da4af3e6fd59f28904c1304b07e6147c
Arturo Vivar <arthas1237@hotmail.com>
2021-11-08 20:48:52 +0100

    iteration12

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,45 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers() {
+
+  if  (num1>num2)
+
+  return num1;
+  
+  else 
+  
+  return num2;
+
+
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+
+  let longestWord = ""; 
+  
+  if (words.length === 0) {
+    return null
+  } else {
+  
+   for (word of words ){
+  
+   if (word.length > longestWord.length) {
+  
+  
+     longestWord=word
+   }
+  
+  
+  }
+  
+  }
+  
+  return longestWord;
+  }
 
 
 
