89c32812ef5f8f533e641b47122891bed3b33a35
Lucas Salomao <lucaas.afonso@hotmail.com>
2021-11-14 14:32:01 -0300

    iteration 3.1 Bonus entregue com teste falhando - não tem descrição de como deve ser retornada a ultima condição

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -32,7 +32,6 @@ function findLongestWord(arrayOfWords) {
   
 }
 
-
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
@@ -47,14 +46,27 @@ function sumNumbers(arrayOfNumbers) {
   return totalSum
 }
 
-console.log(sumNumbers(numbers))
-
-
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(arr) {
 
+  let totalSum = 0
 
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (typeof arr[i] === "string") {
+      totalSum += arr[i].length
+    } else if (typeof arr[i] === "number") {
+      totalSum += arr[i]
+    } else if (arr[i] === true) {
+      totalSum += 1
+    } else if ((typeof arr[i] === "object") || (typeof arr[i] === "array")){
+      const message = "Unsupported data type sir or ma'am"
+      return message
+    }
+    }
+  return totalSum
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
