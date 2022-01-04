f8ab3d4fb613379179466140c840c5a473ece9a0
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-04 20:39:34 +0100

    changes to iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,15 +1,16 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(num1, num2) {
   if(num1 > num2){
-    console.log(num1)
+    return num1
   }
-  else if(num1  <num2){
-    console.log(num2)
+  else if(num1 < num2){
+    return num2;
+  }
+  else if
+    (num1 === num2){
+    return num1;
   }
 }
-maxOfTwoNumbers (1, 2)
-
-
 
 
 
