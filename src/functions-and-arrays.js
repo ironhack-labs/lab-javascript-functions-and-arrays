81774976b12bdae79e0bbed4ca33227a55c07eb5
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-01-05 08:17:30 +0100

    Iteration 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -58,9 +58,17 @@ function sum() {}
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
-function averageNumbers() {}
-
+function averageNumbers(numbers) {
+  if (numbers.length === 0) {
+    return null;
+  }
+  let numberOfElements = numbers.length;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++){
+     sum += numbers[i];
+  }
+    return sum / numberOfElements;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
