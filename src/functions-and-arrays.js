c65256477e73a08c28b9145f2b4f08ea749df5d8
David Vekony <dave.vekony@gmail.com>
2022-01-05 01:02:48 +0100

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,41 +1,109 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(a, b) {
+  return Math.max(a, b);
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
-
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
+  let longestWord = '';
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
 
 // Iteration #3.1 Bonus:
-function sum() {}
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
-
+function sum(mixedArr) {
+  if (mixedArr.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  mixedArr.forEach((element) => {
+    switch (typeof element) {
+      case 'number':
+        sum += element;
+        break;
+      case 'string':
+        sum += element.length;
+        break;
+      case 'boolean':
+        if (element === true) {
+          sum++;
+        }
+        break;
+      default:
+        throw new Error("Unsupported data type sir or ma'am");
+    }
+  });
+  return sum;
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(numbersAvg) {
+  if (numbersAvg.length === 0) {
+    return null;
+  }
+  return sumNumbers(numbersAvg) / numbersAvg.length;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) {
+  if (wordsArr.length === 0) {
+    return null;
+  }
+  return sum(wordsArr) / wordsArr.length;
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(mixedArr) {
+  if (mixedArr.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  mixedArr.forEach((element) => {
+    switch (typeof element) {
+      case 'number':
+        sum += element;
+        break;
+      case 'string':
+        sum += element.length;
+        break;
+      case 'boolean':
+        if (element === true) {
+          sum++;
+        }
+        break;
+      default:
+        throw new Error("Unsupported data type sir or ma'am");
+    }
+  });
+  return parseFloat((sum / mixedArr.length).toFixed(2));
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,16 +120,37 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
-
-
+function uniquifyArray(wordsUnique) {
+  if (wordsUnique.length === 0) {
+    return null;
+  }
+  let dedupedWords = [];
+  for (let i = 0; i < wordsUnique.length; i++) {
+    if (dedupedWords.indexOf(wordsUnique[i]) === -1) {
+      dedupedWords.push(wordsUnique[i]);
+    }
+  }
+  return dedupedWords;
+}
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
-
-
+function doesWordExist(wordsFind, searchWord) {
+  if (wordsFind.length === 0) {
+    return null;
+  }
+  let wordExists;
+  for (let i = 0; i < wordsFind.length; i++) {
+    if (wordsFind[i] === searchWord) {
+      wordExists = true;
+      break;
+    } else {
+      wordExists = false;
+    }
+  }
+  return wordExists;
+}
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -78,9 +167,18 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
-
-
+function howManyTimes(wordsCount, countWord) {
+  if (wordsCount.length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  for (let i = 0; i < wordsCount.length; i++) {
+    if (wordsCount[i] === countWord) {
+      wordCount++;
+    }
+  }
+  return wordCount;
+}
 
 // Iteration #8: Bonus
 const matrix = [
@@ -107,9 +205,6 @@ const matrix = [
 ];
 
 function greatestProduct() {}
-
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
