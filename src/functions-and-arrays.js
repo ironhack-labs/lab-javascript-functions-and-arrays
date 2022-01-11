4e3ca3e9afa243ed76cdf9f278e0e1c05e5996bd
Jacob Kelly <bananacreamkelly@gmail.com>
2022-01-11 18:36:39 +0100

    finished lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,20 +1,39 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a,b) {
+  if (a > b){
+    return a
+  } else {
+    return b
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  const longestFirst = arr.sort ((a,b) => b.length - a.length); 
+   return longestFirst[0]
+  if(arr.length == 0) {
+    return 'null'
+  }
+} 
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(arr) {
+  let sum = 0
+  let sumArr = []
+  for (let i = 0; i < arr.length; i++){
+ sum += arr[i]
+ }
+  sumArr.push(sum)
+  return sumArr
+}
 
 
 // Iteration #3.1 Bonus:
@@ -26,13 +45,28 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  let avg = 0
+  for (let i = 0; i < arr.length; i++){
+ avg += arr[i] 
+ }
+  avg = avg/arr.length
+  return avg
+}
+
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  let avg = 0
+  for (let i = 0; i < arr.length;i++){
+   avg += arr[i].length;
+  }
+  avg = avg/arr.length
+  return avg
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
