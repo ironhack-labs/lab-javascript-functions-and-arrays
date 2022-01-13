01429b8b31fe445c63d3f5800088e4cd97b3b605
Saleh Ramadan <saleh.ramadan1989@gmail.com>
2022-01-13 17:23:00 +0200

    initial commit

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,11 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1
+  } else {
+    return num2
+  }
+}
 
 
 // Iteration #2: Find longest word
