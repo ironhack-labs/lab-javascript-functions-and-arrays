ebe72e66ac82f29b719401ab0a8697eedb4d292c
“Natalia <natalia.rudiger@gmail.com>
2022-01-15 23:14:21 -0200

    Iteration 2 incomplete

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,38 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2){
+    return num1;
+    }else if (num2 > num1){
+    return num2;
+    }
+    return num1;
+    }
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+let longest = '';
 
-function findLongestWord() {}
-
+function findLongestWord(arr) {
+  if (!arr.length) {
+    return null;
+  }
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > longest.length) {
+      longest = arr[i]; 
+      } else if (arr[i].length === longest.length) {
+        longest = arr.push(arr[i]);
+      }
+    }
+    return longest;
+   }
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers() {}
+
 
 
 
