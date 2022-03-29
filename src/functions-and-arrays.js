e4f287960e50977a7342d479d005bd007f04637f
Ivo Silva <silvaivosoares@gmail.com>
2022-03-29 22:38:20 +0100

    Completed all the questions in the assignement

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,86 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 > number2) {
+    return number1;
+  } else if (number2 > number1) {
+    return number2;
+  } else if (number1 === number2) {
+    return number1;
+  }
+}
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arrayWords) {
+  const numOfLetters = [];
+  let indexOfMax;
+  let maxNumOfLetters;
+
+  if (arrayWords.length === 0) {
+    return null;
+  } else if (arrayWords.length === 1) {
+    return arrayWords[0];
+  } else {
+    // Created a new array with the word sizes in the same order of the original words array
+    arrayWords.forEach(function (word) {
+      numOfLetters.push(word.length);
+    });
+
+    // I used the Math.max method fo find the max number of the new array, corresponding to the size of the biggest word
+    maxNumOfLetters = Math.max(...numOfLetters);
+
+    // I used then the indexOf method to find the first occurence of the biggest size word
+    indexOfMax = numOfLetters.indexOf(maxNumOfLetters);
+
+    return arrayWords[indexOfMax];
+  }
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrayToSum) {
+  let sum = 0;
+
+  if (arrayToSum.length === 0) {
+    return 0;
+  } else {
+    arrayToSum.forEach(function(number){
+        sum += number;
+    });
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(polyArray) {
+  let sum = 0;
+
+  if (polyArray.length === 0) {
+    return 0;
+  } else {
+    polyArray.forEach(function (element) {
+      if (typeof element === 'object') {
+        throw new Error("Unsupported data type sir or ma'am");
+      } else if (typeof element === 'string') {
+        sum += element.length;
+      } else if (typeof element === 'number') {
+        sum += element;
+      } else if (element === true) {
+        sum += 1;
+      } else if (element === false) {
+        sum += 0;
+      }
+    });
+  }
+  return sum;
+}
 
 
 
@@ -26,16 +88,54 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrayToAverage) {
+  if (arrayToAverage.length === 0) {
+    return null;
+  } else if (arrayToAverage.length === 1){
+    const average = sumNumbers(arrayToAverage);
+    return average;
+  } else {
+    const average = sumNumbers(arrayToAverage) / arrayToAverage.length;
+    return average;
+  }
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arrayWithWords) {
+  let numLettersPerWord = [];
+  let averageLength;
+
+  if (arrayWithWords.length === 0) {
+    return null;
+  } else {
+    arrayWithWords.forEach(function (word) {
+      numLettersPerWord.push(word.length);
+    });
+  }
+  
+averageLength = sumNumbers(numLettersPerWord)/numLettersPerWord.length;
+
+return averageLength;
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(arr) {
+  let numOfElements;
+  let average;
+
+  if (arr.length === 0) {
+    return null;
+  } else {
+  numOfElements = arr.length;
+  
+  // I had to find this workaround to round the result to 2 decimal numbers so it would pass the test, since the result expected was 5.11 and my result was 5.11111111111
+  average = Math.round((sum(arr) / numOfElements)*100) / 100;
+  }
+  return average;
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,15 +152,43 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(arrayToUniquify) {
+  let uniquifiedArray = [];
+
+  if (arrayToUniquify.length === 0) {
+    return null;
+  } else {
+    // Here I used a for loop to iterate through the array so I could have an easier access to each index for comparisons
+    for (let i = 0; i < arrayToUniquify.length; i++) {
+      if (i === arrayToUniquify.indexOf(arrayToUniquify[i])) {
+        uniquifiedArray.push(arrayToUniquify[i]);
+      }
+    }
+  }
+  return uniquifiedArray;
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(sourceArray, targetWord) {
+  let foundWord = false;
 
+  if (sourceArray.length === 0) {
+    return null;
+  } else if (sourceArray.length === 1 && sourceArray[0] === targetWord) {
+    return true;
+  } else {
+    sourceArray.forEach(function (word) {
+      if (word === targetWord) {
+        foundWord = true;
+      }
+    });
+  }
+  return foundWord;
+}
 
 
 // Iteration #7: Count repetition
@@ -78,7 +206,20 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(arrayToSearch, wordToFind) {
+  let timesFound = 0;
+
+  if (arrayToSearch.length === 0) {
+    return timesFound;
+  } else {
+    arrayToSearch.forEach(function(word){
+      if (word === wordToFind){
+        timesFound += 1;
+      }
+    });
+  }
+  return timesFound;
+}
 
 
 
@@ -106,7 +247,58 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let maxProduct = 0;
+  let result;
+
+  // Checking for horizontal products. Each cycle on the first for loop will check an element of the array and the nested for loop will iterate the correspodent child array
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      // Making sure that the code doesn't try to find numbers outside the range of the array
+      // This implies that for each index of the matrix array, it will check the groups of four numbers inside the array located in that index. This means it will check for horizontal products
+      if ((j-3) >= 0) {
+        result = matrix[i][j]*matrix[i][j-1]*matrix[i][j-2]*matrix[i][j-3];
+        if (maxProduct < result) {
+          maxProduct = result;
+        }
+      }
+      // This implies that for each specific index of the nested arrays it will check the corresponding index of the adjacent nested arrays inside the matrix array. This means it will check for vertical products.
+      if ((i-3)>=0) {
+        result = matrix[i][j]*matrix[i-1][j]*matrix[i-2][j]*matrix[i-3][j];
+        if (maxProduct < result) {
+          maxProduct = result;
+        }
+      }
+    }
+  }
+  return maxProduct
+}
+
+
+function greatestProductOfDiagonals(matrix){
+  let maxProduct = 0;
+  let result;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      // Making sure that the code doesn't try to find numbers outside the range of the array. Now starting at the top left, and searching in the down -> right diagonal
+      if ((j-3) >= 0 && (i-3) >= 0) {
+        result = matrix[i][j]*matrix[i-1][j-1]*matrix[i-2][j-2]*matrix[i-3][j-3];
+        if (maxProduct < result) {
+          maxProduct = result;
+        }
+      }
+      // Now checking for the up -> right diagonal
+      if ((i-3)>=0) {
+        result = matrix[i][j]*matrix[i-1][j+1]*matrix[i-2][j+2]*matrix[i-3][j+3];
+        if (maxProduct < result) {
+          maxProduct = result;
+        }
+      }
+    }
+  }
+  return maxProduct
+}
 
 
 
