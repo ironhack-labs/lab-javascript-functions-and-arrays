9e2c4fe62da119e3f7c9317d5fb31e68690f2bdc
Pablo <olbapdow@hotmail.com>
2022-04-30 18:09:42 +0200

    Iteration 1 - completed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 > num2){
+    return(num1)
+  } else {
+    return(num2)
+  }
+}
+maxOfTwoNumbers(200, 100)
 
 
 
