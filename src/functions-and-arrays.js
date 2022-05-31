2622ac999e076a536baf813588e4eddcb034745b
Alexander Farmakis <alexander@movact.de>
2022-05-31 18:06:31 +0200

    not done yet

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,41 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 > num2) {
+    return num1
+  } else {
+    return num2
+  }
+}
 
 
 
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot']
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+  if(words.length === 0) {
+    return null
+  }
+  let longestWord = ''
+  for(let i = 0; i < words.length; i++) {
+    if(words[i].length > longestWord.length) {
+      longestWord = words[i]
+    }
+  }
+  return longestWord
+}
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0
+  for(number of numbers) {
+    sum += number;
+  }
+  return sum
+}
 
 
 
@@ -26,7 +48,13 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+  if(!numbers.length) {
+    return 0;
+  }
+  let sum = sumNumbers(numbers)
+  return sum / numbers.length
+}
 
 
 // Level 2: Array of strings
