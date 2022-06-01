3e89a1107c816d6b9f60681366bbbaaf73a7b483
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-01 10:11:07 +0200

    Ironhack week 1 day 2 JS functions & arrays tasks

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,41 +1,125 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(x, y) {
+  if (x > y) {
+    return x 
+  }
+  else if (x < y) {
+    return y 
+  } 
+  else if (x === y) {
+    return x || y
+  }
+  else { 
+    return "Error" 
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  let longest = '';
+  if (words.length === 0) {
+    return null;
+  }
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(numbers) {
+  let total = 0; 
+  if (numbers.length === 0) {
+    return 0;
+  } 
+  for (let i in numbers) {
+    total += numbers[i];
+  }
+  return total;
+}
+  
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+// how much is the sum of the length of all of the words in an array
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+function sum(mixedArr) {
+  let res = 0; 
+  if (mixedArr.length === 0) {
+    return 0; 
+  }
+  for (let i in mixedArr) {
+  // for (i = 0; i < mixedArr.length; i++) {
+    res += mixedArr[i];
+  }
+  return res;
+} 
 
 
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9]; 
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+  let total = 0; 
+  if (numbersAvg.length === 0) { 
+    return null;
+  }
+  for (let i in numbersAvg) {
+    total += numbersAvg[i];
+  } 
+  average = total / numbersAvg.length; 
+  return average;
+}
+
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) { 
+  if (wordsArr.length === 0) {
+    return null;
+  }
+  arrLength = wordsArr.length; 
+  joining = wordsArr.join(''); 
+  avg = joining.length / arrLength; 
+  return avg;
+}
 
-// Bonus - Iteration #4.1
-function avg() {}
+
+
+// Bonus - Iteration #4.1 
+const arr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
+
+function avg(arr) {
+  let number = 0; 
+  if (arr.length === 0) { 
+    return null;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      numbers += arr[i].length;
+    } else {
+      numbers += arr[i];
+    }
+  } 
+  return Number((numbers / arr.length).toFixed(2));
+}
+
+
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +136,28 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(wordsUnique) {
+  let unique = []; 
+  if (wordsUnique.length === 0) { 
+    return null;
+  }
+  for (let i in wordsUnique) {
+    if (unique.indexOf(i) === -1 && i !== '') 
+      unique.push(i);
+  }
+  return unique;
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(wordsFind) {
+  if (wordsFind.length === 0) { 
+    return null;
+  }
+}
 
 
 
@@ -78,7 +176,11 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(wordsCount) {
+  if (wordsCount.length === 0) { 
+    return null;
+  }
+}
 
 
 
@@ -106,7 +208,11 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  if (matrix.length === 0) { 
+    return null;
+  }
+}
 
 
 
