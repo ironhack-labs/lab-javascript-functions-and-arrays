4ec9a6b3505efd669293c810b2d09c606799e12f
Walison Abram <abramwhas@gmail.com>
2022-06-19 21:00:28 -0300

    add iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,21 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) return num1;
+  if (num2 > num1) return num2;
+  if (num1 === num2) return num1;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(wordsArray) {
+  let longestWord = ''
+    for (let i = 0; i < wordsArray.length; i++) {
+
+  }
+}
 
 
 
