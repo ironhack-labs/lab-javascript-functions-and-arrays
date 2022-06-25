b601c62b265f6f139ce22cfb00346d3b2d7d0d7c
Samuel Taboas <droidsam@gmail.com>
2022-06-25 13:35:39 +0200

    add solution 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -25,7 +25,13 @@ function findLongestWord(input) {
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(input) {
+  let sum = 0;
+  for (let number of input) {
+    sum += number;
+  }
+  return sum;
+}
 
 // Iteration #3.1 Bonus:
 function sum() {}
