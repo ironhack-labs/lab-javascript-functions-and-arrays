cf89f5fb9c11963fb7ef1f09655f708f652b3ea8
Samuel Taboas <droidsam@gmail.com>
2022-06-25 13:19:25 +0200

    fix styles

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,42 +1,34 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers(aNumber, anotherNumber) {
-  if(aNumber > anotherNumber){
+  if (aNumber > anotherNumber) {
     return aNumber;
   }
   return anotherNumber;
 }
 
-
-
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
 function findLongestWord(input) {
-  if(!input || input.length == 0){
+  if (!input || input.length == 0) {
     return null;
   }
-  let longestWord = "";
-  for(word of input){
-    if(word.length > longestWord.length){
+  let longestWord = '';
+  for (word of input) {
+    if (word.length > longestWord.length) {
       longestWord = word;
     }
   }
   return longestWord;
 }
 
-
-
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers() {}
 
-
-
 // Iteration #3.1 Bonus:
 function sum() {}
-
-
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
@@ -44,11 +36,10 @@ const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function averageNumbers() {}
 
-
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength() {}
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -70,14 +61,10 @@ const wordsUnique = [
 
 function uniquifyArray() {}
 
-
-
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
 function doesWordExist() {}
-
-
 
 // Iteration #7: Count repetition
 const wordsCount = [
@@ -95,8 +82,6 @@ const wordsCount = [
 ];
 
 function howManyTimes() {}
-
-
 
 // Iteration #8: Bonus
 const matrix = [
@@ -123,9 +108,6 @@ const matrix = [
 ];
 
 function greatestProduct() {}
-
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
