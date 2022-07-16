f32d0bffb35bd51f00b47e045ddce9768cb70c61
MacDantas <marcelodecarvalhodantas@hotmail.com>
2022-07-16 14:43:51 -0300

    Interation 1 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,14 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2){
+    return num1;
+  } else if (num2 > num1){
+    return num2;
+  } else {
+    return num1;
+  }
+}
+  
 
 
 // Iteration #2: Find longest word
