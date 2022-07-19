cf60f23389f01e18d9f72ab9f6f1409d6e57c748
Romina Polo <rominalpolo@gmail.com>
2022-07-19 15:51:26 -0400

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,12 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(a,b) {
+if (a > b) {
+  return a;
+}
+else {
+  return b;
+}
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
