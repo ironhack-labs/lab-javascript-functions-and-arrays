d8ab7d043d88f72d273170b0ba5a611d5e02bdfb
JDLMS <lamorey.joshua@gmail.com>
2022-07-23 16:59:17 +0200

    first commit

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,37 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  return Math.max(num1, num2)
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  let largestWordSoFar = ""
+  if (arr.length < 1) {
+  return null
+ } 
+ arr.forEach (word => {
+  if (word.length > largestWordSoFar.length) {
+    largestWordSoFar = word
+  }
+ })
+  return largestWordSoFar
+}
+
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers() {
+  for (let i = 0; i < numbers.length; i ++) {
+    
+  }
+}
 
 
 
