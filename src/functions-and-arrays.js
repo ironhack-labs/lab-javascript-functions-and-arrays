c885bf8033f1b3951b300f411df257c4be469c1b
CescVila <vr.cesc@gmail.com>
2022-07-25 20:37:35 +0200

    try to fix

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -37,7 +37,7 @@ console.log(findLongestWord(wordList));
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 var total = 0;
-function sumNumbers() {
+function sumNumbers(numbers) {
   for (let i = 0; i < numbers.length; i++){
     total += numbers[i];
 
@@ -57,15 +57,16 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {
+function averageNumbers(averageNumbers) {
   let sum = 0
   let average = sum/(numbersAvg.length)
   for(let units = 0; units < numbersAvg.length; units++) {
     sum += numbersAvg[units]
-    return
+    
   }
 }
 console.log(averageNumbers(numbersAvg));
+console.log(average)
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
