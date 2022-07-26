9e35a571e84721e3d99d4d4e233195c9ac278d93
Agash Jeyakumar <agash.jey.it@gmail.com>
2022-07-26 18:23:41 +0200

    La almost done, I have two bonus exercises  to finish

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,41 +1,123 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1>num2){
+   return num1;
+  } else if (num1<num2){
+    return num2;
+  } else {
+   return num1;
+   return num2;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+  let longestWord='';
+  if (words.length===0){
+    return null;
+  } else {
+    for (let word of words){
+      if (word.length>longestWord.length){
+        longestWord = word;
+      }
+    }
+    return longestWord;
+  }
+}
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+  numbers.forEach(num => {
+    sum+=num;
+  });
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+const list=[6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, false, []];
 
+function sum(listAllTypes) {
+  let sum = 0;
+  if (listAllTypes.length===0){
+    return 0;
+  }else if (listAllTypes.length===1){
+    sum = listAllTypes[0];
+  }
 
+  let newList=[];
+
+  for(let i=0;i<listAllTypes.length;i++){
+    console.log(listAllTypes[i]);
+    if(listAllTypes[i]===true){
+      newList.push(1);
+    } else if(listAllTypes[i]===false){
+      newList.push(0);
+    } else if (typeof listAllTypes[i]==='string'){
+      newList.push(listAllTypes[i].length);
+    } else if (typeof listAllTypes[i]==='number') {
+      newList.push(listAllTypes[i]);
+    } else {
+      console.log("hello");
+      throw new Error("Unsupported data type sir or ma'am");
+    }
+  }
+  console.log(newList);
+  return sumNumbers(newList);
+}
+
+console.log(sum(list));
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+  let avg;
+  if(numbersAvg.length===0){
+    return null;
+  }else if (numbersAvg.length===1) {
+    return numbersAvg[0];
+  }
+  avg=sumNumbers(numbersAvg)/numbersAvg.length;
+
+  return avg;
+}
+
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+const mot = ['ironhack'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) {
+  let avgLength;
+  if(wordsArr.length===0){
+    return null;
+  }else if (wordsArr.length===1) {
+   return wordsArr[0].length;
+  }else{
+    const wordsLength=wordsArr.map(function(item){
+      return item.length;
+    });
+    avgLength=sumNumbers(wordsLength)/wordsLength.length;
+    return avgLength;
+  }
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg() {
+
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +134,54 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+const rien = [];
+const iphone = ['iPhone', 'Samsung', 'Android', 'iOS', 'iPhone', 'Samsung', 'Nokia', 'Blackberry', 'Android']
+
+function uniquifyArray(wordsUnique) {
+  if(wordsUnique.length===0){
+    return null;
+  }
+
+  let newList=[];
+  let item;
+
+//2nd method because I didn't saw that we had to use "includes"
+ /* for (let i=0; i<wordsUnique.length; i++){
+      item=wordsUnique[i];
+     
+      //Because "wordsUnique.indexOf(item)" will show 
+      //the index of the first occurence
+      if (i===wordsUnique.indexOf(item)){
+        newList.push(item);
+      }
+  }*/
+
+//1st method: with "includes" which is more simple
+  for (let i=0; i<wordsUnique.length; i++){
+    if (!newList.includes(wordsUnique[i])){
+      newList.push(wordsUnique[i]);
+    }
+  }
+
+  return newList;
+}
+
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(wordsFind, word) {
+  if(wordsFind.length===0){
+    return null;
+  }
+  if (wordsFind.includes(word)){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 
@@ -78,7 +200,15 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(wordsCount, word) {
+  let count =0;
+  for (let item of wordsCount){
+    if (item===word){
+      count += 1;
+    }
+  }
+  return count;
+}
 
 
 
