fb7513ce166cd4ce3d2c3c837fd67a25aee29d3d
Joanna Zielinska <joannazielinskam@gmail.com>
2022-08-10 16:01:40 +0200

    Solutions adjusted

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -4,46 +4,14 @@ function maxOfTwoNumbers( a, b ) {
     return a
   } else if ( a < b) {  
     return b
-  } else {
-    return a
   }
+    return a
+  
 }
-
-// no need to 'else' see below
-
-// function maxOfTwoNumbers( a, b ) {
-//   if ( a > b) {
-//     return a
-//   }   
-//     return b   
-// }
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
-// function findLongestWord(words)
-//   {
-//     if (words.length > 0) {
-//       let wordLength = words[0].length // change this part is too long -- see coment below
-//       let longestWord = words[0]
-//           for (let i = 1; i < words.length; i++) {
-//           if (words[i].length > wordLength) {
-//               longestWord = words[i]
-//               wordLength = maxi
-//           }
-//       }
-//       return longestWord;
-//     } else {
-//       return null
-//     }
-// } 
-
-
-
-// again else is not necessery 
-// let longestWord = ''
-// then for of loop
 
 function findLongestWord(words)
   {
@@ -77,30 +45,19 @@ function sumNumbers(numbers) {
 // Iteration #3.1 Bonus:   !!! need to check
 function sum(arr) {
   let argumentSum = 0
-      for (let argument of arr) {    
-      if (typeof argument  === 'string') {
-        argumentSum += argument.length
-      } else if (typeof argument  === "number"){
-        argumentSum += argument
-      } else if (typeof argument  === "boolean"){
-          if (argument === true) {
-            argumentSum += 1
-          } else {
-            argumentSum += 0
-          } 
-      } else {
-        throw 'Parameter is wrong!'
-      }
-    }
-    return argumentSum
+  for (let argument of arr) {    
+  if (typeof argument  === 'string') {
+    argumentSum += argument.length
+  } else if (typeof argument  === "number" || typeof argument  === "boolean"){
+    argumentSum += argument
+  }  else {
+    throw  'Error!'
+  }
+}
+return argumentSum
 }
 
 console.log(sum([6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10]))
-
-// can mbe done with case switch ( typeOf argument)
-// throw
-// js automaticaly add 0 or 1 for boolean --> no need to give them value
-
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
@@ -133,10 +90,12 @@ function averageWordLength(words) {
 
 // Bonus - Iteration #4.1 
 function avg(arr) {
+  if (arr.length === 0) {
+    return null
+  }
   return sum(arr) / arr.length
 }
 
-// return null
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -173,16 +132,6 @@ function uniquifyArray(wordsList) {
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-// function doesWordExist(wordsList,word) {
-//   if (wordsList.length > 0) {
-//       if (wordsList.includes(word)) {
-//         return true
-//       } else
-//     return false
-//     } else {
-//     return null
-//   }
-// }
 
 function doesWordExist(wordsList,word) {
   if (wordsList.length > 0) {
