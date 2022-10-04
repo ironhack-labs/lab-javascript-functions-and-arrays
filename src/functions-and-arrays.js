79d9b8dd1adfcf5b7f4d0b7d8d926522f631a18d
Andrea Leonetti <ciroandrealeonetti@gmail.com>
2022-10-04 19:43:46 +0100

    done2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,5 @@
 // Iteration #1: Find the maximum
-/*
+
 function maxOfTwoNumbers(num1, num2) {
   if (num1 > num2) {
     return num1;
@@ -7,11 +7,11 @@ function maxOfTwoNumbers(num1, num2) {
     return num2;
   }
 }
-*/
+
 
 
 // Iteration #2: Find longest word
-/*
+
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
 function findLongestWord(array) {
@@ -22,13 +22,13 @@ function findLongestWord(array) {
   
     return longestWord;
 }
-*/
+
 
 
 
 
 // Iteration #3: Calculate the sum
-/*
+
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 function sumNumbers(array) {
@@ -38,7 +38,7 @@ function sumNumbers(array) {
   }
   return sum;
 }  
-*/
+
 
 
 
@@ -49,6 +49,7 @@ function sum() {}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
+
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
 function averageNumbers(array) {
@@ -62,12 +63,20 @@ function averageNumbers(array) {
 
 
 // Level 2: Array of strings
+
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength(array) { 
-  let words = words.split(" ");
-  console.log(words);
+function averageWordLength(wordsArr) { 
+  let total = 0;
+  for (let i = 0; i < wordsArr.length; i++) {
+    total+=wordsArr[i].length;
+    console.log(total);
+  }
+  let average = total/wordsArr.length;
+
+return average;
 }
+
 
 // Bonus - Iteration #4.1
 function avg() {}
