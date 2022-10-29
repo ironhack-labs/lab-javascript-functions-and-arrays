817b6514e13dc2943fa9642e70995721a000366d
Secil <secilegen@gmail.com>
2022-10-29 17:13:07 +0200

    first 3 iterations done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,46 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  if (number1>number2) {
+    return number1
+  }
+  else if (number2>number1) {
+    return number2
+  }
+  else return number1 || number2;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length===0) {
+    return null;
+  }
+  let longestWord = '';
+  for (let i=0; i<words.length; i++){
+  if (words[i].length >  longestWord.length) {
+   longestWord = words[i];
+  }
+}
+return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
 
+  for (i=0; i<numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum;
 
+}
 
 // Iteration #3.1 Bonus:
 function sum() {}
