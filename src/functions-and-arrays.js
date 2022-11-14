df145e4a5b491ba1c7432e861b39223da7fd37d9
asm-dev <salamanca.m@outlook.com>
2022-11-14 16:05:30 +0100

    first iteration - maxOfTwoNumbers

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,22 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1,num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    return num1;
+  };
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord() {
+
+}
 
 
 
