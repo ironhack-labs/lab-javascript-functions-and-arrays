e67a2ab7aedfaaf528548a4f8e86cc82ec1f661d
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@iMac-de-Ana.home>
2022-11-17 21:24:51 +0100

    Iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,6 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(number1,number2) {
+  if (number1 > number2) {
+    return number1;
+  } else if (number1 < number2) {
+    return number2;
+  } else {
+    return number1;
+  }
+}
 
 
 // Iteration #2: Find longest word
