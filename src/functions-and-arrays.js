ba12d365c9b7a788b7a10e13ba000701c0ac0757
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2022-11-18 15:26:15 +0100

    Finished

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -298,7 +298,22 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let greatestNumber=0;
+
+  for (let i=0; i<matrix.length-3; i++){
+    for (let j=0; j<matrix[i].length-3; j++){
+      if(greatestNumber<(matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3])){
+        greatestNumer = matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3];
+      }
+      if (greatestNumber < (matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j])){
+        greatestNumber = matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j];
+      }
+    }
+  }
+
+  return greatestNumber;
+}
 
 
 
