e8ae05edc461e85e0e5854f79d0ac136eaada5d2
Noa Leon <n.leon.carrasco@gmail.com>
2022-12-13 14:49:12 +0100

    oopsie

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -234,7 +234,7 @@ function greatestProduct(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       let check = [];
-      if (matrix[j] === j) {
+      if (matrix.indexOf(matrix[j]) === j) {
         check.push(matrix[j]);
       }
     }
