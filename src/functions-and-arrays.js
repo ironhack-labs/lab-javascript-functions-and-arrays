d9eb228d9b7596ba6798bfc14d898f49cb16c899
rachelpytse <rachelpytse@gmail.com>
2023-02-19 13:22:34 +0100

    Modify iteration2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -21,6 +21,8 @@ function findLongestWord(words) {
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > longestWord.length) {
       longestWord = words[i];
+    } else {
+      continue
     }
   }
   return longestWord;
