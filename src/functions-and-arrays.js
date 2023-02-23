e35651ab56d51161945211b83cd7dfd1ce67b76a
Hugo Silva <dasilvapapis@protonmail.com>
2023-02-23 17:14:55 +0000

    still have 24 failures

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,20 +1,49 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }else if (num1 < num2) {
+    return num2;
+  }else return num1,num2;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
+function findLongestWord(longest) {
+  if (longest.length === 0) {
+    return null;
+  }
+  else {
+    let word = "";
+    for (let i = 0; i < longest.length; i++) {
+      if (word.length < longest[i].length) {
+        word = longest[i];
+      }
+    }
+    return word;
+  }
+}
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(nums) {
+  let sum = 0; 
 
+  if (nums.length === 0) {
+    return 0;
+  }
+  else {
+    for (let i = 0; i < nums.length; i++)
+      sum += nums[i];
+  }
+  console.log(sum);
+  return sum;
+}
 
 
 // Iteration #3.1 Bonus:
@@ -26,13 +55,36 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(nums) {
+  let aveNum = 0;
+    
+  if (nums.length === 0) {
+    return null;
+  }
+  else {
+    for (let i = 0; i < nums.length; i++) {
+      aveNum += nums[i];
+    }
+    return aveNum/nums.length;
+  }
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(words) {
+  let aveWords = 0;
+  if (words.length === 0) {
+    return null;
+  }
+  else {
+    for (let i = 0; i < words.length; i++) {
+      aveWords += words[i].length;
+    }
+    return aveWords/words.length;
+  }
+ }
 
 // Bonus - Iteration #4.1
 function avg() {}
