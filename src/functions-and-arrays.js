d3df788580a2b6bc28ec3afa290b6eac5064846f
Bartosz Dubinski <bartas.de@gmail.com>
2023-02-23 18:07:45 +0100

    first four

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,49 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1
+  } else if 
+    (num1 < num2) {
+      return num2
+    } else {
+      return num1=num2
+    }
+}
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if (arr.length === 0){
+    return null
+  } else { 
+  let word = ""
+  for ( let i = 0; i < arr.length; i++) {
+    if (word.length < arr[i].length) {
+      word = arr[i];
+    }
+  }
+  return word  
+}
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  let sum = 0;
+ if (arr.length === 0) {
+  return 0;
+ } else 
+ for (let i = 0; i <arr.length; i++) {
+  sum += arr[i];
+ }
+ console.log(sum);
+ return sum;
+}
 
 
 
@@ -26,7 +56,17 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  let sum = 0;
+  if (arr.length === 0) { 
+    return null
+  } else {
+    for ( i = 0; i < arr.length; i++)
+    sum += arr[i];
+    return sum/arr.length
+  }
+  
+}
 
 
 // Level 2: Array of strings
