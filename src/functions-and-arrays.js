f2d91f5f98e3f0450471bc977aafa558dac62459
Raquel Barrio <r.barrio.f@gmail.com>
2023-04-16 23:02:12 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -10,7 +10,6 @@ function maxOfTwoNumbers(num1, num2) {
 }
 
 
-
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
@@ -86,10 +85,10 @@ sumNumbers(numbers);
   })
 
   if (errorResult) {
-    console.log("error");
+/*     console.log("error"); */
     return "error";
   } else {
-    console.log(sumArray);
+/*     console.log(sumArray); */
     return sumArray;
   }
 }
