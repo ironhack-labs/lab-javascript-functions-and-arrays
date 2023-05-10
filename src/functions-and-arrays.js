6ceead3642f7819c06db1855315629eecd8d37cd
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-05-10 03:30:06 +0200

    another iteration

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -45,13 +45,21 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(avg) {
+  avgArray = 0;
+   avg.forEach(function(number){
+    avgArray += number;
+  });
+  return avgArray / avg.length;
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength() {
+  
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
