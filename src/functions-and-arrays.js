be8180a0c6342557a48c06a6ae82c7e1ae25ae61
Naiim Taefy <naiim.tj@gmail.com>
2023-06-04 14:30:25 +0200

    feat: Iteration 3 & Bonus 3.1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -32,10 +32,28 @@ function findLongestWord(words) {
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  return sum(numbers);
+}
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(numbers) {
+  let sum = 0;
+  for (const number of numbers) {
+    switch (typeof number) {
+      case "string":
+        sum += Number.isNaN(number) ? Number(number) : number.length;
+        break;
+      case "object":
+      case "array":
+        throw new Error("Unsupported data type sir or ma'am");
+      default:
+        sum += Number(number);
+        break;
+    }
+  }
+  return sum;
+}
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
