789f22c5f090cdc3860918a6b3eb0c866d8d8559
Andrews <andreeews2k12@gmail.com>
2023-06-06 18:01:43 +0200

    finished, some attempts at bonus logic, nothing of value

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -258,7 +258,15 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct() {
+
+
+
+
+
+
+  
+}
 
 
 
