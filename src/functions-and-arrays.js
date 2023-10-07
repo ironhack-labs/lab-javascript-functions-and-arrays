1957623795febe0d60f000d8859f792dc984cb88
iamchrisas <chrisas.agb@gmail.com>
2023-10-07 15:14:50 +0200

    Update functions-and-arrays.js
    Iteration 3 done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -31,7 +31,14 @@ function findLongestWord(words) {
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+ let sum = 0;
+  for (let j = 0; j < numbers.length; j++ ) {
+   sum += numbers[j]
+    }
+    return sum
+  }
+  
 
 
 
