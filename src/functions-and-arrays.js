a194a281bc139c0caecf5fbf3de67db64039a8b7
José Ramón Mosquera Farré <136368059+Theindicud@users.noreply.github.com>
2023-12-07 21:30:05 +0100

    feat: iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,14 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else if (num1 === num2) {
+    return num1;
+  }
+}
+
 
 
 
