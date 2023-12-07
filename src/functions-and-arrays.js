9c5a84f39212804d38a87885ea0160c9bcb8aa27
IvanZS <sayonbra@gmail.com>
2023-12-07 21:19:46 +0100

    feat: iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 < number2) {
+    return number2;
+  } else if (number1 > number2) {
+    return number1;
+  } else {
+    return number1, number2;
+  }
+}
 
 
 
