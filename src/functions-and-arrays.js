84146187e25d85f38d43ac56f40265fd72212137
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-11 15:31:46 +0100

    add functions and arrays exercises

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,84 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(number1, number2) {
+  return Math.max(number1, number2);
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let longestWord = '';
 
+    if(!words.length) {
+        return 0;
+    }
 
+    for(let i = 0; i < words.length; i ++) {
+      if (words[i].length > longestWord.length) {
+          longestWord = words[i];
+      }
+    }
+    return longestWord;
+}
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i ++) {
+        sum += numbers[i];
+    }
 
-
-
+    return sum;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    if(!numbers.length) {
+        return 0;
+    }
 
+    const sum = sumNumbers(numbers);
 
-
+    return sum / numbers.length;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(words, word) {
+    let hasWord = false;
+
+    if(!words.length) {
+        return null;
+    }
+
+    words.forEach(element => {
+        if(element === word) {
+            hasWord = true;
+        }
+    });
+
+    return hasWord;
+}
