375ea7d4475d1335a35e43f740f08729f2d4489c
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-11 15:08:14 +0100

    solve all iterations

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,47 +1,93 @@
 // Iteration 1 | Find the Maximum
+
 function maxOfTwoNumbers(num1, num2) {
-    if (num1 > num2) {
-        return num1;
-    } else if (num1 < num2) {
-        return num2;
-    } else if (num1 === num2) {
-        return "They are equal";
-    } else {
-        return "Something went wrong";
-    }
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    return "The numbers are equal";
+  }
 }
 
-
+console.log(maxOfTwoNumbers(8, 9));
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord(word) {
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
+function findLongestWord(arr) {
+  let longestWord = "";
+  let maxLength = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].length > maxLength) {
+      maxLength = arr[i].length;
+      longestWord = arr[i];
+    }
+  }
+  console.log(`The longest word is: ${longestWord}`);
 }
 
-
-
+findLongestWord(words);
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  console.log(sum);
+}
 
-
+sumNumbers(numbers);
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers(number) {
+function averageNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
+  let averageNumber = sum / arr.length;
+  console.log(averageNumber);
 }
 
-
-
+averageNumbers(numbers2);
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, wordToSearchFor) {
+  let index = arrayOfWords.indexOf(wordToSearchFor);
+
+  if (index !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(doesWordExist(words2, "Natalia"));
