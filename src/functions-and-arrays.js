e7f1ae9c8626d6413805aedb5494efbb3ae91e5d
Frederico Rose <fredericoa.rose@gmail.com>
2024-01-11 15:23:13 +0100

    Solved Lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,73 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
-
+function maxOfTwoNumbers(number1,number2) {
+    return Math.max(number1,number2);
+    }
+    const result = maxOfTwoNumbers(1,9);
+    console.log(result);
+    
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
-
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null;
+    }
+     let longestWord = words[0];
+     for (let i = 1; i < words.length; i++) {
+         if (words[i].length > longestWord.length) {
+         longestWord = words[i];
+     }
+ }
+ return longestWord;
+ } 
+ const wordsArray = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+ const longestWordResult = findLongestWord(wordsArray); 
+ console.log(longestWordResult);
 
 
 
 
 // Iteration 3 | Sum Numbers
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
-function sumNumbers() {}
-
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+    const numbersArray = [1, 2, 3, 4, 5];
+    const result = sumNumbers(numbersArray);
+    console.log(result);
 
 
 
 // Iteration 4 | Numbers Average
-const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
-
-function averageNumbers() {}
-
+function averageNumbers(numbers) {
+    const sum = sumNumbers(numbers);
+    if (numbers.length === 0) {
+        return null;
+    }
+    const average = sum / numbers.length;
+    return average;
+}
+    const arrayofNumbers = [2, 6, 9, 10, 7, 4, 1, 9];
+    const averageResult = averageNumbers(arrayofNumbers);
+    console.log(averageResult);
 
 
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
-
-function doesWordExist() {}
+function doesWordExist(wordArray, findWord) {
+    if (wordArray.length === 0) {
+        return null;
+ }
+     return wordArray.includes(findWord);
+ } 
+     const wordsPuzzle = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+     const searchWord = "disobedience";
+     const exists = doesWordExist(wordsPuzzle,searchWord);
+     if (exists === null) {
+         console.log("The array is empty.");
+     } else if (exists) {
+         console.log(`The word "${searchWord}" exists in the array.`);
+     } else {
+         console.log(`The word "${searchWord}" does not exist in the array.`);
+     }
